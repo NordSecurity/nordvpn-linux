785009b40e9d5de8ca55c647f4b86a025fe73151
ggediminass <gediminas.alsauskas@nordsec.com>
2023-05-09 12:31:00 +0000

    Refactor connect tests. Added few new also.

--- test/qa/test_connect.py
+++ test/qa/test_connect.py
@@ -1,4 +1,3 @@
-import random
 from lib import (
     daemon,
     info,
@@ -33,21 +32,28 @@ def teardown_function(function):
     logging.log()
 
 
+def connect_base_test(group=[], name="", hostname=""):
+    output = sh.nordvpn.connect(group)
+    print(output)
+    assert lib.is_connect_successful(output, name, hostname)
+    assert network.is_connected()
+
+
+def disconnect_base_test():
+    output = sh.nordvpn.disconnect()
+    print(output)
+    assert lib.is_disconnect_successful(output)
+    assert network.is_disconnected()
+
+
 @pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
 @pytest.mark.flaky(reruns=2, reruns_delay=90)
 @timeout_decorator.timeout(20)
 def test_quick_connect(tech, proto, obfuscated):
     lib.set_technology_and_protocol(tech, proto, obfuscated)
 
-    output = sh.nordvpn.connect()
-    print(output)
-    assert lib.is_connect_successful(output)
-    assert network.is_connected()
-
-    output = sh.nordvpn.disconnect()
-    print(output)
-    assert lib.is_disconnect_successful(output)
-    assert network.is_disconnected()
+    connect_base_test()
+    disconnect_base_test()
 
 
 @pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
@@ -56,20 +62,10 @@ def test_quick_connect(tech, proto, obfuscated):
 def test_double_quick_connect_only(tech, proto, obfuscated):
     lib.set_technology_and_protocol(tech, proto, obfuscated)
 
-    output = sh.nordvpn.connect()
-    print(output)
-    assert lib.is_connect_successful(output)
-    assert network.is_connected()
+    for n in range(2):
+        connect_base_test()
 
-    output = sh.nordvpn.connect()
-    print(output)
-    assert lib.is_connect_successful(output)
-    assert network.is_connected()
-
-    output = sh.nordvpn.disconnect()
-    print(output)
-    assert lib.is_disconnect_successful(output)
-    assert network.is_disconnected()
+    disconnect_base_test()
 
 
 @pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
@@ -103,15 +99,8 @@ def test_connect_to_random_server_by_name(tech, proto, obfuscated):
     lib.set_technology_and_protocol(tech, proto, obfuscated)
 
     name, hostname = server.get_hostname_by(tech, proto, obfuscated)
-    output = sh.nordvpn.connect(hostname.split(".")[0])
-    print(output)
-    assert lib.is_connect_successful(output, name, hostname)
-    assert network.is_connected()
-
-    output = sh.nordvpn.disconnect()
-    print(output)
-    assert lib.is_disconnect_successful(output)
-    assert network.is_disconnected()
+    connect_base_test(hostname.split(".")[0], name, hostname)
+    disconnect_base_test()
 
 
 @pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
@@ -120,10 +109,7 @@ def test_connect_to_random_server_by_name(tech, proto, obfuscated):
 def test_connection_recovers_from_network_restart(tech, proto, obfuscated):
     lib.set_technology_and_protocol(tech, proto, obfuscated)
 
-    output = sh.nordvpn.connect()
-    print(output)
-    assert lib.is_connect_successful(output)
-    assert network.is_connected()
+    connect_base_test()
 
     links = socket.if_nameindex()
     logging.log(links)
@@ -134,10 +120,7 @@ def test_connection_recovers_from_network_restart(tech, proto, obfuscated):
         assert network.is_connected()
     logging.log(info.collect())
 
-    output = sh.nordvpn.disconnect()
-    print(output)
-    assert lib.is_disconnect_successful(output)
-    assert network.is_disconnected()
+    disconnect_base_test()
 
 
 @pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
@@ -146,25 +129,9 @@ def test_connection_recovers_from_network_restart(tech, proto, obfuscated):
 def test_double_quick_connect_disconnect(tech, proto, obfuscated):
     lib.set_technology_and_protocol(tech, proto, obfuscated)
 
-    output = sh.nordvpn.connect()
-    print(output)
-    assert lib.is_connect_successful(output)
-    assert network.is_connected()
-
-    output = sh.nordvpn.disconnect()
-    print(output)
-    assert lib.is_disconnect_successful(output)
-    assert network.is_disconnected()
-
-    output = sh.nordvpn.connect()
-    print(output)
-    assert lib.is_connect_successful(output)
-    assert network.is_connected()
-
-    output = sh.nordvpn.disconnect()
-    print(output)
-    assert lib.is_disconnect_successful(output)
-    assert network.is_disconnected()
+    for n in range(2):
+        connect_base_test()
+        disconnect_base_test()
 
 
 @pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
@@ -180,25 +147,54 @@ def test_connect_without_internet_access(tech, proto, obfuscated):
     network.start()
 
 
+@pytest.mark.parametrize("group", lib.STANDARD_GROUPS)
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
 @pytest.mark.flaky(reruns=2, reruns_delay=90)
 @timeout_decorator.timeout(20)
-def test_connect_to_group():
-    group = random.choice(lib.GROUPS)
+def test_connect_to_group_standard(tech, proto, obfuscated, group):
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
 
-    output = sh.nordvpn.connect(group)
-    print(output)
-    assert lib.is_connect_successful(output)
-    assert network.is_connected()
+    connect_base_test(group)
+    disconnect_base_test()
 
-    output = sh.nordvpn.connect("--group", group)
-    print(output)
-    assert lib.is_connect_successful(output)
-    assert network.is_connected()
 
-    output = sh.nordvpn.disconnect()
-    print(output)
-    assert lib.is_disconnect_successful(output)
-    assert network.is_disconnected()
+@pytest.mark.parametrize("group", lib.ADDITIONAL_GROUPS)
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.STANDARD_TECHNOLOGIES)
+@pytest.mark.flaky(reruns=2, reruns_delay=90)
+@timeout_decorator.timeout(20)
+def test_connect_to_group_additional(tech, proto, obfuscated, group):
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    connect_base_test(group)
+    disconnect_base_test()
+
+
+@pytest.mark.parametrize("group", lib.STANDARD_GROUPS)
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
+@pytest.mark.flaky(reruns=2, reruns_delay=90)
+@timeout_decorator.timeout(20)
+def test_connect_to_group_flag_standard(tech, proto, obfuscated, group):
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    connect_base_test(["--group", group])
+    disconnect_base_test()
+
+    with pytest.raises(sh.ErrorReturnCode_1) as ex:
+        sh.nordvpn.connect("--group", group, group)
+
+    print(ex.value)
+    assert lib.is_connect_unsuccessful(ex)
+
+
+@pytest.mark.parametrize("group", lib.ADDITIONAL_GROUPS)
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.STANDARD_TECHNOLOGIES)
+@pytest.mark.flaky(reruns=2, reruns_delay=90)
+@timeout_decorator.timeout(20)
+def test_connect_to_group_flag_additional(tech, proto, obfuscated, group):
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    connect_base_test(["--group", group])
+    disconnect_base_test()
 
     with pytest.raises(sh.ErrorReturnCode_1) as ex:
         sh.nordvpn.connect("--group", group, group)
@@ -211,7 +207,41 @@ def test_connect_to_invalid_group():
     try:
         with pytest.raises(sh.ErrorReturnCode_1) as ex:
             sh.nordvpn.connect("--group", "nonexisting_group")
+
         # We want to check for this exact message
         assert "The specified group does not exist." in str(ex.value)
     finally:
         sh.nordvpn.disconnect()
+
+
+@pytest.mark.parametrize("country", lib.COUNTRIES)
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.STANDARD_TECHNOLOGIES) # TODO: Once LVPN-2668 is solved, use lib.TECHNOLOGIES instead
+@pytest.mark.flaky(reruns=2, reruns_delay=90)
+@timeout_decorator.timeout(20)
+def test_connect_to_country(tech, proto, obfuscated, country):
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    connect_base_test(country)
+    disconnect_base_test()
+
+
+@pytest.mark.parametrize("country", lib.COUNTRY_CODES)
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
+@pytest.mark.flaky(reruns=2, reruns_delay=90)
+@timeout_decorator.timeout(20)
+def test_connect_to_code_country(tech, proto, obfuscated, country):
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    connect_base_test(country)
+    disconnect_base_test()
+
+
+@pytest.mark.parametrize("city", lib.CITIES)
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.STANDARD_TECHNOLOGIES) # TODO: Once LVPN-2668 is solved, use lib.TECHNOLOGIES instead
+@pytest.mark.flaky(reruns=2, reruns_delay=90)
+@timeout_decorator.timeout(20)
+def test_connect_to_city(tech, proto, obfuscated, city):
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    connect_base_test(city)
+    disconnect_base_test()
