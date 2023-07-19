5a636a612dc25d935c1feff264d5bc54f8d31dc0
Emilijus Stankus <emilijus.stankus@nordsec.com>
2023-07-19 09:40:51 +0000

    Remove fileshare peer cache

--- fileshare/pubkey_test.go
+++ fileshare/pubkey_test.go
@@ -22,20 +22,3 @@ func TestPubkeyFunc(t *testing.T) {
 	assert.Equal(t, pubkey, base64.StdEncoding.EncodeToString(pubkeyActual))
 	assert.True(t, meshClient.getPeersCalled)
 }
-
-func TestPubkeyFunc_Cache(t *testing.T) {
-	meshClient := mockMeshClient{}
-	meshClient.externalPeers = []*meshpb.Peer{
-		{
-			Ip:     "1.2.3.4",
-			Pubkey: "PWOUuWuCLIj6xCbPOCBaMA2ez29g8aTMuYkCQx9kfj4=",
-		},
-	}
-	pubkeyProvider := NewPubkeyProvider(&meshClient)
-	fakePubkeyBytes := []byte{0x01, 0x02}
-	pubkeyProvider.pubkeyCache["1.2.3.4"] = fakePubkeyBytes
-
-	pubkeyActual := pubkeyProvider.PubkeyFunc("1.2.3.4")
-	assert.Equal(t, fakePubkeyBytes, pubkeyActual)
-	assert.False(t, meshClient.getPeersCalled)
-}
