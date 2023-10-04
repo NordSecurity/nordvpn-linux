ba3bd5758a1ea20db2c0ae6235a631720a017aff
Savolro <me@savolro.com>
2023-10-04 11:01:03 +0300

    Explain how to enable meshnet on fileshare help
    Signed-off-by: Savolro <me@savolro.com>

--- cli/messages.go
+++ cli/messages.go
@@ -245,7 +245,7 @@ Learn more:
 	MsgFileshareInvalidPeer               = "Peer name is invalid."
 	MsgFileshareDisconnectedPeer          = "Peer is disconnected."
 	MsgFileshareFileNotFound              = "File not found."
-	MsgFileshareSocketNotFound            = "Enable Meshnet to share files. If Meshnet is already enabled, try disabling and enabling it again."
+	MsgFileshareSocketNotFound            = "Enable Meshnet to share files. If Meshnet is already enabled, try disabling and enabling it again. Use \"nordvpn set meshnet on\" to enable it."
 
 	MsgFileshareAcceptHomeError      = "Cannot determine default download path. Please provide download path explicitly via --" + flagFilesharePath
 	MsgFileshareAcceptAllError       = "Download couldn't start."
