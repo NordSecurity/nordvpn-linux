7cd0f8f28fa4a6193e9d1a52aab276ad92b68531
bartoszWojciechO <bartosz.oleaczek@nordsec.com>
2023-10-31 13:45:32 +0100

    update command executor

--- daemon/firewall/firewall_manager.go
+++ daemon/firewall/firewall_manager.go
@@ -15,11 +15,13 @@ type CommandExecutor interface {
 }
 
 type ExecCommandExecutor struct {
+	command string
 }
 
-func (ExecCommandExecutor) ExecuteCommand(command string) error {
+func (e ExecCommandExecutor) ExecuteCommand(command string) error {
 	commandArgs := strings.Split(command, "")
-	_, err := exec.Command(commandArgs[0], commandArgs[1:]...).CombinedOutput()
+
+	_, err := exec.Command(e.command, commandArgs...).CombinedOutput()
 	return err
 }
 
@@ -61,7 +63,7 @@ func (f *FirewallManager) FileshareAllow(peer meshnet.UniqueAddress) error {
 	}
 
 	rule := fmt.Sprintf("INPUT -s %s/32 -p tcp -m tcp --dport 49111 -m comment --comment nordvpn -j ACCEPT", peer.Address.String())
-	if err := f.commandExecutor.ExecuteCommand("iptables -I " + rule); err != nil {
+	if err := f.commandExecutor.ExecuteCommand("-I " + rule); err != nil {
 		return fmt.Errorf("adding fileshare allow rule: %w", err)
 	}
 
@@ -76,7 +78,7 @@ func (f *FirewallManager) FileshareDeny(peer meshnet.UniqueAddress) error {
 		return fmt.Errorf("fileshare allow rule for the given peer doesn't exist")
 	}
 
-	if err := f.commandExecutor.ExecuteCommand("iptables -D " + rule); err != nil {
+	if err := f.commandExecutor.ExecuteCommand("-D " + rule); err != nil {
 		return fmt.Errorf("removing fileshare allow rule: %w", err)
 	}
 
@@ -94,12 +96,12 @@ func (f *FirewallManager) BlockTraffic() error {
 	// -I INPUT -i <iface> -m comment --comment nordvpn -j DROP
 	// -I OUTPUT -o <iface> -m comment --comment nordvpn -j DROP
 	for _, iface := range interfaces {
-		inputCommand := fmt.Sprintf("iptables -I INPUT -i %s -m comment --comment nordvpn -j DROP", iface.Name)
+		inputCommand := fmt.Sprintf("-I INPUT -i %s -m comment --comment nordvpn -j DROP", iface.Name)
 		if err := f.commandExecutor.ExecuteCommand(inputCommand); err != nil {
 			return fmt.Errorf("blocking input traffic: %w", err)
 		}
 
-		outputCommand := fmt.Sprintf("iptables -I OUTPUT -o %s -m comment --comment nordvpn -j DROP", iface.Name)
+		outputCommand := fmt.Sprintf("-I OUTPUT -o %s -m comment --comment nordvpn -j DROP", iface.Name)
 		if err := f.commandExecutor.ExecuteCommand(outputCommand); err != nil {
 			return fmt.Errorf("blocking output traffic: %w", err)
 		}
@@ -118,12 +120,12 @@ func (f *FirewallManager) UnblockTraffic() error {
 	// -D INPUT -i <iface> -m comment --comment nordvpn -j DROP
 	// -D OUTPUT -o <iface> -m comment --comment nordvpn -j DROP
 	for _, iface := range interfaces {
-		inputCommand := fmt.Sprintf("iptables -D INPUT -i %s -m comment --comment nordvpn -j DROP", iface.Name)
+		inputCommand := fmt.Sprintf("-D INPUT -i %s -m comment --comment nordvpn -j DROP", iface.Name)
 		if err := f.commandExecutor.ExecuteCommand(inputCommand); err != nil {
 			return fmt.Errorf("blocking input traffic: %w", err)
 		}
 
-		outputCommand := fmt.Sprintf("iptables -D OUTPUT -o %s -m comment --comment nordvpn -j DROP", iface.Name)
+		outputCommand := fmt.Sprintf("-D OUTPUT -o %s -m comment --comment nordvpn -j DROP", iface.Name)
 		if err := f.commandExecutor.ExecuteCommand(outputCommand); err != nil {
 			return fmt.Errorf("blocking output traffic: %w", err)
 		}
@@ -138,7 +140,7 @@ func (f *FirewallManager) AllowIncoming(peer meshnet.UniqueAddress, allowLocal b
 	}
 
 	rule := fmt.Sprintf("INPUT -s %s/32 -m comment --comment nordvpn -j ACCEPT", peer.Address)
-	if err := f.commandExecutor.ExecuteCommand("iptables -I " + rule); err != nil {
+	if err := f.commandExecutor.ExecuteCommand("-I " + rule); err != nil {
 		return fmt.Errorf("adding allow incoming rule: %w", err)
 	}
 
@@ -153,7 +155,7 @@ func (f *FirewallManager) AllowIncoming(peer meshnet.UniqueAddress, allowLocal b
 
 		for _, lan := range lans {
 			blockLANRule := fmt.Sprintf("INPUT -s %s/32 -d %s -m comment --comment nordvpn -j DROP", peer, lan)
-			if err := f.commandExecutor.ExecuteCommand("iptables -I " + blockLANRule); err != nil {
+			if err := f.commandExecutor.ExecuteCommand("-I " + blockLANRule); err != nil {
 				return fmt.Errorf("adding block peer lan rule: %w", err)
 			}
 			blockLANRules = append(blockLANRules, blockLANRule)
@@ -175,12 +177,12 @@ func (f *FirewallManager) DenyIncoming(peer meshnet.UniqueAddress) error {
 		return fmt.Errorf("allow incoming rule for the given peer not found")
 	}
 
-	if err := f.commandExecutor.ExecuteCommand("iptables -D " + rule.allowIncomingRule); err != nil {
+	if err := f.commandExecutor.ExecuteCommand("-D " + rule.allowIncomingRule); err != nil {
 		return fmt.Errorf("adding allow incoming rule: %w", err)
 	}
 
 	for _, blockLANCommand := range rule.blockLANRules {
-		if err := f.commandExecutor.ExecuteCommand("iptables -D " + blockLANCommand); err != nil {
+		if err := f.commandExecutor.ExecuteCommand("-D " + blockLANCommand); err != nil {
 			return fmt.Errorf("deleting block peer lan rule: %w", err)
 		}
 	}
@@ -193,12 +195,12 @@ func (f *FirewallManager) DenyIncoming(peer meshnet.UniqueAddress) error {
 func (f *FirewallManager) UnblockMeshnet(deviceAddress string) error {
 	// -A INPUT -s 100.64.0.0/10 -m conntrack --ctstate RELATED,ESTABLISHED --ctorigsrc {device address} -m comment --comment nordvpn -j ACCEPT
 	// -A INPUT -s 100.64.0.0/10 -m comment --comment nordvpn -j DROP
-	command := fmt.Sprintf("iptables -I INPUT -s 100.64.0.0/10 -m conntrack --ctstate RELATED,ESTABLISHED --ctorigsrc %s -m comment --comment nordvpn -j ACCEPT", deviceAddress)
+	command := fmt.Sprintf("-I INPUT -s 100.64.0.0/10 -m conntrack --ctstate RELATED,ESTABLISHED --ctorigsrc %s -m comment --comment nordvpn -j ACCEPT", deviceAddress)
 	if err := f.commandExecutor.ExecuteCommand(command); err != nil {
 		return fmt.Errorf("blocking unrelated mesh traffic: %w", err)
 	}
 
-	err := f.commandExecutor.ExecuteCommand("iptables -I INPUT -s 100.64.0.0/10 -m comment --comment nordvpn -j DROP")
+	err := f.commandExecutor.ExecuteCommand("-I INPUT -s 100.64.0.0/10 -m comment --comment nordvpn -j DROP")
 	if err != nil {
 		return fmt.Errorf("blocking mesh traffic: %w", err)
 	}
@@ -209,12 +211,12 @@ func (f *FirewallManager) UnblockMeshnet(deviceAddress string) error {
 func (f *FirewallManager) BlockMeshnet(deviceAddress string) error {
 	// -A INPUT -s 100.64.0.0/10 -m conntrack --ctstate RELATED,ESTABLISHED --ctorigsrc {device address} -m comment --comment nordvpn -j ACCEPT
 	// -A INPUT -s 100.64.0.0/10 -m comment --comment nordvpn -j DROP
-	command := fmt.Sprintf("iptables -I INPUT -s 100.64.0.0/10 -m conntrack --ctstate RELATED,ESTABLISHED --ctorigsrc %s -m comment --comment nordvpn -j ACCEPT", deviceAddress)
+	command := fmt.Sprintf("-I INPUT -s 100.64.0.0/10 -m conntrack --ctstate RELATED,ESTABLISHED --ctorigsrc %s -m comment --comment nordvpn -j ACCEPT", deviceAddress)
 	if err := f.commandExecutor.ExecuteCommand(command); err != nil {
 		return fmt.Errorf("blocking unrelated mesh traffic: %w", err)
 	}
 
-	err := f.commandExecutor.ExecuteCommand("iptables -I INPUT -s 100.64.0.0/10 -m comment --comment nordvpn -j DROP")
+	err := f.commandExecutor.ExecuteCommand("-I INPUT -s 100.64.0.0/10 -m comment --comment nordvpn -j DROP")
 	if err != nil {
 		return fmt.Errorf("blocking mesh traffic: %w", err)
 	}
@@ -251,25 +253,25 @@ func (f *FirewallManager) allowlistPort(iface string, protocol string, portRange
 	// -A OUTPUT -o {interface} -p {protocol} -m {protocol} --sport {port} -m comment --comment nordvpn -j ACCEPT
 	// -A OUTPUT -o {interface} -p {protocol} -m {protocol} --dport {port} -m comment --comment nordvpn -j ACCEPT
 	inputDportRule := fmt.Sprintf("INPUT -i %s -p %s -m %s --dport %d:%d -m comment --comment nordvpn -j ACCEPT", iface, protocol, protocol, portRange.min, portRange.max)
-	if err := f.commandExecutor.ExecuteCommand("iptables -I " + inputDportRule); err != nil {
+	if err := f.commandExecutor.ExecuteCommand("-I " + inputDportRule); err != nil {
 		return fmt.Errorf("allowlisting input dport: %w", err)
 	}
 	f.allowlistRules = append(f.allowlistRules, inputDportRule)
 
 	inputSportRule := fmt.Sprintf("INPUT -i %s -p %s -m %s --sport %d:%d -m comment --comment nordvpn -j ACCEPT", iface, protocol, protocol, portRange.min, portRange.max)
-	if err := f.commandExecutor.ExecuteCommand("iptables -I " + inputSportRule); err != nil {
+	if err := f.commandExecutor.ExecuteCommand("-I " + inputSportRule); err != nil {
 		return fmt.Errorf("allowlisting input sport: %w", err)
 	}
 	f.allowlistRules = append(f.allowlistRules, inputSportRule)
 
 	outputDportRule := fmt.Sprintf("OUTPUT -i %s -p %s -m %s --dport %d:%d -m comment --comment nordvpn -j ACCEPT", iface, protocol, protocol, portRange.min, portRange.max)
-	if err := f.commandExecutor.ExecuteCommand("iptables -I " + outputDportRule); err != nil {
+	if err := f.commandExecutor.ExecuteCommand("-I " + outputDportRule); err != nil {
 		return fmt.Errorf("allowlisting output dport: %w", err)
 	}
 	f.allowlistRules = append(f.allowlistRules, outputDportRule)
 
 	outputSportRule := fmt.Sprintf("OUTPUT -i %s -p %s -m %s --sport %d:%d -m comment --comment nordvpn -j ACCEPT", iface, protocol, protocol, portRange.min, portRange.max)
-	if err := f.commandExecutor.ExecuteCommand("iptables -I " + outputSportRule); err != nil {
+	if err := f.commandExecutor.ExecuteCommand("-I " + outputSportRule); err != nil {
 		return fmt.Errorf("allowlisting input dport: %w", err)
 	}
 	f.allowlistRules = append(f.allowlistRules, outputSportRule)
@@ -286,13 +288,13 @@ func (f *FirewallManager) SetAllowlist(udpPorts []int, tcpPorts []int, subnets [
 	for _, subnet := range subnets {
 		for _, iface := range ifaces {
 			inputRule := fmt.Sprintf("INPUT -s %s -i %s -m comment --comment nordvpn -j ACCEPT", subnet, iface.Name)
-			if err := f.commandExecutor.ExecuteCommand("iptables -I " + inputRule); err != nil {
+			if err := f.commandExecutor.ExecuteCommand("-I " + inputRule); err != nil {
 				return fmt.Errorf("adding input accept rule for subnet: %w", err)
 			}
 			f.allowlistRules = append(f.allowlistRules, inputRule)
 
 			outputRule := fmt.Sprintf("OUTPUT -d %s -o %s -m comment --comment nordvpn -j ACCEPT", subnet, iface.Name)
-			if err := f.commandExecutor.ExecuteCommand("iptables -I " + outputRule); err != nil {
+			if err := f.commandExecutor.ExecuteCommand("-I " + outputRule); err != nil {
 				return fmt.Errorf("adding output accept rule for subnet: %w", err)
 			}
 			f.allowlistRules = append(f.allowlistRules, outputRule)
@@ -322,7 +324,7 @@ func (f *FirewallManager) SetAllowlist(udpPorts []int, tcpPorts []int, subnets [
 
 func (f *FirewallManager) UnsetAllowlist() error {
 	for _, rule := range f.allowlistRules {
-		if err := f.commandExecutor.ExecuteCommand("iptables -D " + rule); err != nil {
+		if err := f.commandExecutor.ExecuteCommand("-D " + rule); err != nil {
 			return fmt.Errorf("removing allowlist rule: %w", err)
 		}
 	}
@@ -331,9 +333,9 @@ func (f *FirewallManager) UnsetAllowlist() error {
 }
 
 func (f *FirewallManager) manageApiAllowlist(allow bool) error {
-	iptablesMode := "iptables -I "
+	iptablesMode := "-I "
 	if !allow {
-		iptablesMode = "iptables -D "
+		iptablesMode = "-D "
 	}
 
 	ifaces, err := f.devices()
