9b8584f9d6235a57abf7d5b7086fb31574eb8796
ggediminass <gediminas.alsauskas@nordsec.com>
2023-12-04 16:33:25 +0200

    Add missing allowlist port tests
    Signed-off-by: ggediminass <gediminas.alsauskas@nordsec.com>

--- test/qa/test_allowlist_port.py
+++ test/qa/test_allowlist_port.py
@@ -1,3 +1,4 @@
+import random
 from lib import (
     allowlist,
     daemon,
@@ -105,3 +106,286 @@ def test_allowlist_port_requires_connection(allowlist_alias, tech, proto, obfusc
             allowlist.add_ports_to_allowlist([port], allowlist_alias)
             assert firewall.is_active([port])
         assert not firewall.is_active([port])
+
+
+#ADDED
+        
+
+
+@pytest.mark.parametrize("allowlist_alias", lib.ALLOWLIST_ALIAS)
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
+@pytest.mark.parametrize("port", lib.PORTS, ids=[f"{port.protocol}-{port.value}" for port in lib.PORTS])
+@pytest.mark.flaky(reruns=2, reruns_delay=90)
+@timeout_decorator.timeout(40)
+def test_allowlist_port_twice_connect(allowlist_alias, tech, proto, obfuscated, port):
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    with lib.Defer(sh.nordvpn.allowlist.remove.all):
+        with lib.Defer(sh.nordvpn.disconnect):
+            allowlist.add_ports_to_allowlist([port], allowlist_alias)
+
+            with pytest.raises(sh.ErrorReturnCode_1) as ex:
+                if port.protocol == lib.Protocol.ALL:
+                    sh.nordvpn(allowlist_alias, "add", "port", port.value)
+                else:
+                    sh.nordvpn(allowlist_alias, "add", "port", port.value, "protocol", port.protocol)
+
+            expected_message = allowlist.MSG_ALLOWLIST_PORT_ADD_ERROR % (port.value, port.protocol)
+            assert expected_message in str(ex)
+            assert str(sh.nordvpn.settings()).count(port.value) == 1
+            assert not firewall.is_active([port])
+
+            sh.nordvpn.connect()
+            assert network.is_connected()
+            assert firewall.is_active([port])
+        assert network.is_disconnected()
+    assert not firewall.is_active([port])
+
+
+@pytest.mark.parametrize("allowlist_alias", lib.ALLOWLIST_ALIAS)
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
+@pytest.mark.parametrize("port", lib.PORTS, ids=[f"{port.protocol}-{port.value}" for port in lib.PORTS])
+@pytest.mark.flaky(reruns=2, reruns_delay=90)
+@timeout_decorator.timeout(40)
+def test_connect_allowlist_port_twice(allowlist_alias, tech, proto, obfuscated, port):
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    with lib.Defer(sh.nordvpn.allowlist.remove.all):
+        with lib.Defer(sh.nordvpn.disconnect):
+            sh.nordvpn.connect()
+            assert network.is_connected()
+            assert firewall.is_active()
+
+            allowlist.add_ports_to_allowlist([port], allowlist_alias)
+
+            with pytest.raises(sh.ErrorReturnCode_1) as ex:
+                if port.protocol == lib.Protocol.ALL:
+                    sh.nordvpn(allowlist_alias, "add", "port", port.value)
+                else:
+                    sh.nordvpn(allowlist_alias, "add", "port", port.value, "protocol", port.protocol)
+
+            expected_message = allowlist.MSG_ALLOWLIST_PORT_ADD_ERROR % (port.value, port.protocol)
+            assert expected_message in str(ex)
+            assert str(sh.nordvpn.settings()).count(port.value) == 1
+            assert firewall.is_active([port])
+        assert network.is_disconnected()
+    assert not firewall.is_active([port])
+
+
+@pytest.mark.parametrize("allowlist_alias", lib.ALLOWLIST_ALIAS)
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
+@pytest.mark.parametrize("port", lib.PORTS + lib.PORTS_RANGE, ids=[f"{port.protocol}-{port.value}" for port in lib.PORTS + lib.PORTS_RANGE])
+@pytest.mark.flaky(reruns=2, reruns_delay=90)
+@timeout_decorator.timeout(40)
+def test_allowlist_port_and_remove_connect(allowlist_alias, tech, proto, obfuscated, port):
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    with lib.Defer(sh.nordvpn.allowlist.remove.all):
+        with lib.Defer(sh.nordvpn.disconnect):
+            allowlist.add_ports_to_allowlist([port], allowlist_alias)
+            assert not firewall.is_active([port])
+
+            allowlist.remove_ports_from_allowlist([port], allowlist_alias)
+            assert not firewall.is_active([port])
+
+            sh.nordvpn.connect()
+            assert network.is_connected()
+            assert firewall.is_active() and not firewall.is_active([port])
+        assert network.is_disconnected()
+    assert not firewall.is_active()
+
+
+@pytest.mark.parametrize("allowlist_alias", lib.ALLOWLIST_ALIAS)
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
+@pytest.mark.parametrize("port", lib.PORTS + lib.PORTS_RANGE, ids=[f"{port.protocol}-{port.value}" for port in lib.PORTS + lib.PORTS_RANGE])
+@pytest.mark.flaky(reruns=2, reruns_delay=90)
+@timeout_decorator.timeout(40)
+def test_connect_allowlist_port_and_remove(allowlist_alias, tech, proto, obfuscated, port):
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    with lib.Defer(sh.nordvpn.allowlist.remove.all):
+        with lib.Defer(sh.nordvpn.disconnect):
+            sh.nordvpn.connect()
+            assert network.is_connected()
+            assert firewall.is_active()
+
+            allowlist.add_ports_to_allowlist([port], allowlist_alias)
+            assert firewall.is_active([port])
+
+            allowlist.remove_ports_from_allowlist([port], allowlist_alias)
+            assert not firewall.is_active([port])
+
+            assert firewall.is_active()
+        assert network.is_disconnected()
+    assert not firewall.is_active()
+
+
+@pytest.mark.parametrize("allowlist_alias", lib.ALLOWLIST_ALIAS)
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
+@pytest.mark.parametrize("port", lib.PORTS, ids=[f"{port.protocol}-{port.value}" for port in lib.PORTS])
+def test_allowlist_port_remove_nonexistant_disconnected(allowlist_alias, tech, proto, obfuscated, port):
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    with pytest.raises(sh.ErrorReturnCode_1) as ex:
+        if port.protocol == lib.Protocol.ALL:
+            sh.nordvpn(allowlist_alias, "remove", "port", port.value)
+        else:
+            sh.nordvpn(allowlist_alias, "remove", "port", port.value, "protocol", port.protocol)
+
+    expected_message = allowlist.MSG_ALLOWLIST_PORT_REMOVE_ERROR % (port.value, port.protocol)
+    assert expected_message in str(ex)
+
+
+@pytest.mark.parametrize("allowlist_alias", lib.ALLOWLIST_ALIAS)
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
+@pytest.mark.parametrize("port", lib.PORTS, ids=[f"{port.protocol}-{port.value}" for port in lib.PORTS])
+@pytest.mark.flaky(reruns=2, reruns_delay=90)
+@timeout_decorator.timeout(40)
+def test_allowlist_port_remove_nonexistant_connected(allowlist_alias, tech, proto, obfuscated, port):
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    with lib.Defer(sh.nordvpn.disconnect):
+        sh.nordvpn.connect()
+        assert network.is_connected()
+
+        with pytest.raises(sh.ErrorReturnCode_1) as ex:
+            if port.protocol == lib.Protocol.ALL:
+                sh.nordvpn(allowlist_alias, "remove", "port", port.value)
+            else:
+                sh.nordvpn(allowlist_alias, "remove", "port", port.value, "protocol", port.protocol)
+
+        expected_message = allowlist.MSG_ALLOWLIST_PORT_REMOVE_ERROR % (port.value, port.protocol)
+        assert expected_message in str(ex)
+
+
+@pytest.mark.parametrize("allowlist_alias", lib.ALLOWLIST_ALIAS)
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
+@pytest.mark.parametrize("port", lib.PORTS_RANGE, ids=[f"{port.protocol}-{port.value}" for port in lib.PORTS_RANGE])
+def test_allowlist_port_range_remove_nonexistant_disconnected(allowlist_alias, tech, proto, obfuscated, port):
+    port_range = port.value.split(":")
+
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    with pytest.raises(sh.ErrorReturnCode_1) as ex:
+        if port.protocol == lib.Protocol.ALL:
+            sh.nordvpn(allowlist_alias, "remove", "ports", port_range[0], port_range[1])
+        else:
+            sh.nordvpn(allowlist_alias, "remove", "ports", port_range[0], port_range[1], "protocol", port.protocol)
+
+    expected_message = allowlist.MSG_ALLOWLIST_PORT_RANGE_REMOVE_ERROR % (port.value.replace(":", " - "), port.protocol)
+    assert expected_message in str(ex)
+
+
+@pytest.mark.parametrize("allowlist_alias", lib.ALLOWLIST_ALIAS)
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
+@pytest.mark.parametrize("port", lib.PORTS_RANGE, ids=[f"{port.protocol}-{port.value}" for port in lib.PORTS_RANGE])
+@pytest.mark.flaky(reruns=2, reruns_delay=90)
+@timeout_decorator.timeout(40)
+def test_allowlist_port_range_remove_nonexistant_connected(allowlist_alias, tech, proto, obfuscated, port):
+    port_range = port.value.split(":")
+
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    with lib.Defer(sh.nordvpn.disconnect):
+        sh.nordvpn.connect()
+        assert network.is_connected()
+
+        with pytest.raises(sh.ErrorReturnCode_1) as ex:
+            if port.protocol == lib.Protocol.ALL:
+                sh.nordvpn(allowlist_alias, "remove", "ports", port_range[0], port_range[1])
+            else:
+                sh.nordvpn(allowlist_alias, "remove", "ports", port_range[0], port_range[1], "protocol", port.protocol)
+
+        expected_message = allowlist.MSG_ALLOWLIST_PORT_RANGE_REMOVE_ERROR % (port.value.replace(":", " - "), port.protocol)
+        assert expected_message in str(ex)
+
+
+@pytest.mark.parametrize("allowlist_alias", lib.ALLOWLIST_ALIAS)
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
+@pytest.mark.parametrize("port", lib.PORTS_RANGE, ids=[f"{port.protocol}-{port.value}" for port in lib.PORTS_RANGE])
+@pytest.mark.flaky(reruns=2, reruns_delay=90)
+@timeout_decorator.timeout(40)
+def test_allowlist_port_range_twice_connect(allowlist_alias, tech, proto, obfuscated, port):
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    with lib.Defer(sh.nordvpn.allowlist.remove.all):
+        with lib.Defer(sh.nordvpn.disconnect):
+            for x in range(2):
+                allowlist.add_ports_to_allowlist([port], allowlist_alias)
+
+            sh.nordvpn.connect()
+            assert network.is_connected()
+            assert firewall.is_active([port])
+        assert network.is_disconnected()
+
+
+@pytest.mark.parametrize("allowlist_alias", lib.ALLOWLIST_ALIAS)
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
+@pytest.mark.parametrize("port", lib.PORTS_RANGE, ids=[f"{port.protocol}-{port.value}" for port in lib.PORTS_RANGE])
+@pytest.mark.flaky(reruns=2, reruns_delay=90)
+@timeout_decorator.timeout(40)
+def test_connect_allowlist_port_range_twice(allowlist_alias, tech, proto, obfuscated, port):
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    with lib.Defer(sh.nordvpn.allowlist.remove.all):
+        with lib.Defer(sh.nordvpn.disconnect):
+            sh.nordvpn.connect()
+            assert network.is_connected()
+            assert firewall.is_active()
+
+            for x in range(2):
+                allowlist.add_ports_to_allowlist([port], allowlist_alias)
+
+            assert firewall.is_active([port])
+        assert network.is_disconnected()
+
+
+@pytest.mark.parametrize("allowlist_alias", lib.ALLOWLIST_ALIAS)
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
+@pytest.mark.parametrize("port", lib.PORTS_RANGE, ids=[f"{port.protocol}-{port.value}" for port in lib.PORTS_RANGE])
+@pytest.mark.flaky(reruns=2, reruns_delay=90)
+@timeout_decorator.timeout(40)
+def test_allowlist_port_range_when_port_from_range_already_allowlisted_connect(allowlist_alias, tech, proto, obfuscated, port):
+    port_range = port.value.split(":")
+    random_port_from_port_range = str(random.randint(int(port_range[0]), int(port_range[1])))
+
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    with lib.Defer(sh.nordvpn.allowlist.remove.all):
+        with lib.Defer(sh.nordvpn.disconnect):
+            already_allowlisted_port = lib.Port(random_port_from_port_range, port.protocol)
+            allowlist.add_ports_to_allowlist([already_allowlisted_port], allowlist_alias)
+            assert not firewall.is_active([already_allowlisted_port])
+
+            allowlist.add_ports_to_allowlist([port], allowlist_alias)
+            assert not firewall.is_active([port]) and not firewall.is_active([already_allowlisted_port])
+
+            sh.nordvpn.connect()
+            assert network.is_connected()
+            assert firewall.is_active([port]) and not firewall.is_active([already_allowlisted_port])
+
+
+@pytest.mark.parametrize("allowlist_alias", lib.ALLOWLIST_ALIAS)
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
+@pytest.mark.parametrize("port", lib.PORTS_RANGE, ids=[f"{port.protocol}-{port.value}" for port in lib.PORTS_RANGE])
+@pytest.mark.flaky(reruns=2, reruns_delay=90)
+@timeout_decorator.timeout(40)
+def test_connect_allowlist_port_range_when_port_from_range_already_allowlisted(allowlist_alias, tech, proto, obfuscated, port):
+    port_range = port.value.split(":")
+    random_port_from_port_range = str(random.randint(int(port_range[0]), int(port_range[1])))
+
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    with lib.Defer(sh.nordvpn.allowlist.remove.all):
+        with lib.Defer(sh.nordvpn.disconnect):
+            sh.nordvpn.connect()
+            assert network.is_connected()
+            assert firewall.is_active()
+
+            already_allowlisted_port = lib.Port(random_port_from_port_range, port.protocol)
+            allowlist.add_ports_to_allowlist([already_allowlisted_port], allowlist_alias)
+            assert firewall.is_active([already_allowlisted_port])
+
+            allowlist.add_ports_to_allowlist([port], allowlist_alias)
+            assert firewall.is_active([port]) and not firewall.is_active([already_allowlisted_port])
+        assert network.is_disconnected()
