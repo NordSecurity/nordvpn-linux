f693284c1ce41d94de947afd4dc4cab51521a0b1
Gediminas Alšauskas <gediminas.alsauskas@nordsec.com>
2023-12-22 16:03:37 +0200

    Add missing routing tests

--- test/qa/test_routing.py
+++ test/qa/test_routing.py
@@ -36,6 +36,10 @@ SUBNET_2 = "3.3.3.3"
 SUBNET_3 = "4.4.4.4"
 
 MSG_ROUTING_OFF =  "Routing is set to 'disabled' successfully."
+MSG_ROUTING_ON = "Routing is set to 'enabled' successfully."
+MSG_ROUTING_OFF_ALREADY =  "Routing is already set to 'disabled'."
+MSG_ROUTING_ON_ALREADY = "Routing is already set to 'enabled'."
+MSG_ROUTING_USED_BY_MESH = "Routing is currently used by Meshnet. Disable it first."
 
 
 @pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
@@ -84,6 +88,92 @@ def test_routing_disabled_connect(tech, proto, obfuscated):
     assert not network_interface in sh.ip.route.show.table(firewall.IP_ROUTE_TABLE)
 
 
+    assert MSG_ROUTING_ON_ALREADY in sh.nordvpn.set.routing.on()
+    assert settings.get_is_routing_enabled()
+
+
+@pytest.mark.skip("LVPN-3273")
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
+@pytest.mark.flaky(reruns=2, reruns_delay=90)
+@timeout_decorator.timeout(40)
+def test_connected_routing_disable_enable(tech, proto, obfuscated):
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    network_interface = "nordtun" if tech == "openvpn" else "nordlynx"
+
+    print(sh.nordvpn.connect())
+    assert network.is_available()
+
+    assert MSG_ROUTING_OFF in sh.nordvpn.set.routing.off()
+    assert not settings.get_is_routing_enabled()
+    assert not network_interface in sh.ip.route.show.table(firewall.IP_ROUTE_TABLE)
+    assert not "mark" in sh.ip.rule()
+    assert network.is_not_available()
+
+    assert MSG_ROUTING_ON in sh.nordvpn.set.routing.on()
+    assert settings.get_is_routing_enabled()
+    assert network_interface in sh.ip.route.show.table(firewall.IP_ROUTE_TABLE)
+    assert "mark" in sh.ip.rule()
+    assert network.is_available()
+
+
+@pytest.mark.skip("LVPN-3273; LVPN-1574")
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
+@pytest.mark.flaky(reruns=2, reruns_delay=90)
+@timeout_decorator.timeout(40)
+def test_connected_routing_enable_disable(tech, proto, obfuscated):
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    network_interface = "nordtun" if tech == "openvpn" else "nordlynx"
+
+    assert MSG_ROUTING_OFF in sh.nordvpn.set.routing.off()
+    assert not settings.get_is_routing_enabled()
+
+    print(sh.nordvpn.connect())
+    assert network.is_not_available()
+
+    assert MSG_ROUTING_ON in sh.nordvpn.set.routing.on()
+    assert settings.get_is_routing_enabled()
+    assert network_interface in sh.ip.route.show.table(firewall.IP_ROUTE_TABLE)
+    assert "mark" in sh.ip.rule()
+    assert network.is_available()
+
+    assert MSG_ROUTING_OFF in sh.nordvpn.set.routing.off()
+    assert not settings.get_is_routing_enabled()
+    assert not network_interface in sh.ip.route.show.table(firewall.IP_ROUTE_TABLE)
+    assert not "mark" in sh.ip.rule()
+    assert network.is_not_available()
+
+
+@pytest.mark.skip("LVPN-4360")
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES_BASIC1)
+def test_meshnet_on_routing_disable(tech, proto, obfuscated):
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+
+    sh.nordvpn.set.mesh.on()
+    assert MSG_ROUTING_USED_BY_MESH in sh.nordvpn.set.routing.off()
+    assert settings.get_is_routing_enabled()
+
+
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
+def test_routing_already_enabled(tech, proto, obfuscated):
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+    lib.set_routing("on")
+
+    assert MSG_ROUTING_ON_ALREADY in sh.nordvpn.set.routing.on()
+    assert settings.get_is_routing_enabled()
+
+
+@pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
+def test_routing_already_disabled(tech, proto, obfuscated):
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+    lib.set_routing("off")
+
+    assert MSG_ROUTING_OFF_ALREADY in sh.nordvpn.set.routing.off()
+    assert not settings.get_is_routing_enabled()
+
+
+@pytest.mark.skip("LVPN-3273")
 @pytest.mark.parametrize("tech,proto,obfuscated", lib.TECHNOLOGIES)
 @pytest.mark.flaky(reruns=2, reruns_delay=90)
 @timeout_decorator.timeout(40)
@@ -153,3 +243,4 @@ def test_routing_when_iprule_already_exists(tech, proto, obfuscated):
 
         routes = sh.ip.route.show.table("main")
         assert f"default dev {network_interface}" not in routes
+
