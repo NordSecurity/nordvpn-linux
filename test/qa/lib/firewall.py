352e7bfc4dcc64d966e25cb5172e2bd207fcfab3
ggediminass <gediminas.alsauskas@nordsec.com>
2023-12-27 13:38:21 +0200

    Add try/except block

--- test/qa/lib/firewall.py
+++ test/qa/lib/firewall.py
@@ -120,11 +120,16 @@ def __rules_allowlist_subnet_chain_input(interface: str, subnets: list[str]):
     for subnet in subnets:
         result += f"-A INPUT -s {subnet} -i {interface} -m comment --comment nordvpn -j ACCEPT",
 
-    current_subnet_rules_input_chain = sh.grep("\-s ", _in=sh.grep("INPUT", _in=sh.sudo.iptables("-S"))).split("\n")
+    try:
+        current_subnet_rules_input_chain = sh.grep(r"\-s ", _in=sh.grep("INPUT", _in=sh.sudo.iptables("-S"))).split("\n")
 
-    # We need to sort rules, since they appear in iptables in random fashion, unlike port rules
-    # otherwise rule lists in `is_active` wont be the same, and assert will fail
-    return sort_list_by_other_list(result, current_subnet_rules_input_chain)
+        # We need to sort rules, since they appear in iptables in random fashion, unlike port rules
+        # otherwise rule lists in `is_active` wont be the same, and assert will fail
+        return sort_list_by_other_list(result, current_subnet_rules_input_chain)
+    except:
+        # If "-s" is nowhere to be found in iptables, we get exception, and we do not sort
+        # `result`, since we do not have other list to sort `result` list by
+        return result
 
 
 def __rules_allowlist_subnet_chain_output(interface: str, subnets: list[str]):
@@ -133,11 +138,16 @@ def __rules_allowlist_subnet_chain_output(interface: str, subnets: list[str]):
     for subnet in subnets:
         result += f"-A OUTPUT -d {subnet} -o {interface} -m comment --comment nordvpn -j ACCEPT",
 
-    current_subnet_rules_output_chain = sh.grep("\-d ", _in=sh.grep("OUTPUT", _in=sh.sudo.iptables("-S"))).split("\n")
+    try:
+        current_subnet_rules_output_chain = sh.grep("\-d ", _in=sh.grep("OUTPUT", _in=sh.sudo.iptables("-S"))).split("\n")
 
-    # We need to sort rules, since they appear in iptables in random fashion, unlike port rules
-    # otherwise rule lists in `is_active` wont be the same, and assert will fail
-    return sort_list_by_other_list(result, current_subnet_rules_output_chain)
+        # We need to sort rules, since they appear in iptables in random fashion, unlike port rules
+        # otherwise rule lists in `is_active` wont be the same, and assert will fail
+        return sort_list_by_other_list(result, current_subnet_rules_output_chain)
+    except:
+        # If "-s" is nowhere to be found in iptables, we get exception, and we do not sort
+        # `result`, since we do not have other list to sort `result` list by
+        return result
 
 
 def __rules_allowlist_port_chain_input(interface:str, ports_udp: lib.Port, ports_tcp: lib.Port):
