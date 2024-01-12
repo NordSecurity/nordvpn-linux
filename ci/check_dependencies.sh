74ea66f8437e2bc721f194196487a6f755ebb23c
Savolro <me@savolro.com>
2024-01-12 14:04:17 +0200

    Validate checksums of OpenVPN source code
    In order to protect from supply chain attacks, SHA256 checksums of
    OpenVPN and its dependencies are now validated before compilation. In
    addition, original sources are used instead of relying on Tunnelblick.
    
    Signed-off-by: Savolro <me@savolro.com>

--- ci/check_dependencies.sh
+++ ci/check_dependencies.sh
@@ -3,30 +3,44 @@ set -euo pipefail
 
 source "${WORKDIR}/ci/env.sh"
 
-TUNNELBLICK_VERSION="v3.8.8"
-OPENVPN_TARBALL_DIR="${WORKDIR}/build/openvpn/tarballs"
-OPENVPN_URL="https://github.com/Tunnelblick/Tunnelblick/raw/${TUNNELBLICK_VERSION}/third_party/sources"
-mkdir -p "${OPENVPN_TARBALL_DIR}"
-pushd "${OPENVPN_TARBALL_DIR}"
-	wget --quiet -nc "${OPENVPN_URL}/openssl-${OPENSSL_VERSION}.tar.gz"
-	wget --quiet -nc "${OPENVPN_URL}/lzo-${LZO_VERSION}.tar.gz"
-	wget --quiet -nc "${OPENVPN_URL}/openvpn/openvpn-${OPENVPN_VERSION}/openvpn-${OPENVPN_VERSION}.tar.gz"
+openvpn_tarbal_dir="${WORKDIR}/build/openvpn/tarballs"
+
+
+tunnelblick_version="v3.8.8"
+tunnelblick_sha256sum="747aeed732f5303b408006d40736ef2ce276e0d99671b2110bb6b4f2ff7a52ca"
+
+tunnelblick_url="https://github.com/Tunnelblick/Tunnelblick/raw/${tunnelblick_version}/third_party/sources"
+
+mkdir -p "${openvpn_tarbal_dir}"
+pushd "${openvpn_tarbal_dir}"
+	openvpn_tarbal="openvpn-${OPENVPN_VERSION}.tar.gz"
+	openssl_tarbal="openssl-${OPENSSL_VERSION}.tar.gz"
+	lzo_tarbal="lzo-${LZO_VERSION}.tar.gz"
+
+	wget --quiet -nc "https://swupdate.openvpn.org/community/releases/${openvpn_tarbal}"
+	wget --quiet -nc "https://www.openssl.org/source/${openssl_tarbal}"
+	wget --quiet -nc "https://www.oberhumer.com/opensource/lzo/download/${lzo_tarbal}"
+
+	echo "${OPENVPN_SHA256SUM} ${openvpn_tarbal}" | sha256sum -c -
+	echo "${OPENSSL_SHA256SUM} ${openssl_tarbal}" | sha256sum -c -
+	echo "${LZO_SHA256SUM} ${lzo_tarbal}" | sha256sum -c -
 popd
 
-OPENVPN_PATCHES_DIR="${WORKDIR}/build/openvpn/patches"
-mkdir -p "${OPENVPN_PATCHES_DIR}"
-pushd "${OPENVPN_PATCHES_DIR}"
-	wget --quiet -nc "${OPENVPN_URL}/openvpn/openvpn-${OPENVPN_VERSION}/patches/02-tunnelblick-openvpn_xorpatch-a.diff"
-	wget --quiet -nc "${OPENVPN_URL}/openvpn/openvpn-${OPENVPN_VERSION}/patches/03-tunnelblick-openvpn_xorpatch-b.diff"
-	wget --quiet -nc "${OPENVPN_URL}/openvpn/openvpn-${OPENVPN_VERSION}/patches/04-tunnelblick-openvpn_xorpatch-c.diff"
-	wget --quiet -nc "${OPENVPN_URL}/openvpn/openvpn-${OPENVPN_VERSION}/patches/05-tunnelblick-openvpn_xorpatch-d.diff"
-	wget --quiet -nc "${OPENVPN_URL}/openvpn/openvpn-${OPENVPN_VERSION}/patches/06-tunnelblick-openvpn_xorpatch-e.diff"
+openvpn_patches_dir="${WORKDIR}/build/openvpn/patches"
+mkdir -p "${openvpn_patches_dir}"
+pushd "${openvpn_patches_dir}"
+	wget --quiet -nc "${tunnelblick_url}/openvpn/openvpn-${OPENVPN_VERSION}/patches/02-tunnelblick-openvpn_xorpatch-a.diff"
+	wget --quiet -nc "${tunnelblick_url}/openvpn/openvpn-${OPENVPN_VERSION}/patches/03-tunnelblick-openvpn_xorpatch-b.diff"
+	wget --quiet -nc "${tunnelblick_url}/openvpn/openvpn-${OPENVPN_VERSION}/patches/04-tunnelblick-openvpn_xorpatch-c.diff"
+	wget --quiet -nc "${tunnelblick_url}/openvpn/openvpn-${OPENVPN_VERSION}/patches/05-tunnelblick-openvpn_xorpatch-d.diff"
+	wget --quiet -nc "${tunnelblick_url}/openvpn/openvpn-${OPENVPN_VERSION}/patches/06-tunnelblick-openvpn_xorpatch-e.diff"
+	[[ "$(sha256sum <<< "$(cat ./*diff)" | awk $'{print $1}')" == "${tunnelblick_sha256sum}" ]] || exit 1
 popd
 
-LIBNORD_VERSION="0.5.1"
-LIBNORD_ID="6385"
+libnord_version="0.5.1"
+libnord_id="6385"
 
 if [[ "${FEATURES}" == *internal* ]]; then
 	"${WORKDIR}"/ci/download_from_remote.sh \
-		-O nord -p "${LIBNORD_ID}" -v "${LIBNORD_VERSION}" ${ARCH:+-a ${ARCH}} libnord.a
+		-O nord -p "${libnord_id}" -v "${libnord_version}" ${ARCH:+-a ${ARCH}} libnord.a
 fi
