baf8e6a912f4f4da9ba5471618a05fdc8bce18fc
Savolro <me@savolro.com>
2024-01-18 17:25:37 +0200

    Add moose logging configuration
    Moose logs will be printed to stdout now. Log level:
    * `debug` for development builds
    * `error` for production builds
    
    Signed-off-by: Savolro <me@savolro.com>

--- cmd/daemon/events_moose.go
+++ cmd/daemon/events_moose.go
@@ -3,8 +3,11 @@
 package main
 
 import (
+	"os"
+
 	"github.com/NordSecurity/nordvpn-linux/config"
 	"github.com/NordSecurity/nordvpn-linux/events/moose"
+	"github.com/NordSecurity/nordvpn-linux/internal"
 )
 
 var (
@@ -14,6 +17,12 @@ var (
 
 func newAnalytics(eventsDbPath string, fs *config.FilesystemConfigManager,
 	ver, env, id string) *moose.Subscriber {
+	_ = os.Setenv("MOOSE_LOG_FILE", "Stdout")
+	logLevel := "error"
+	if !internal.IsProdEnv(env) {
+		logLevel = "debug"
+	}
+	_ = os.Setenv("MOOSE_LOG", logLevel)
 	return &moose.Subscriber{
 		EventsDbPath: eventsDbPath,
 		Config:       fs,
