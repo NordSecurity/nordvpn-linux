55f7f790b4469e55d3b0808eeed55b235932ceda
Marius Sincovici <marius.sincovici@nordsec.com>
2024-02-13 09:02:49 +0100

    add linux build
    Signed-off-by: Marius Sincovici <marius.sincovici@nordsec.com>

---
+++ ui/linux/flutter/ephemeral/generated_config.cmake
@@ -0,0 +1,21 @@
+# Generated code do not commit.
+file(TO_CMAKE_PATH "/home/mariussincovici/snap/flutter/common/flutter" FLUTTER_ROOT)
+file(TO_CMAKE_PATH "/home/mariussincovici/projects/nordvpn-linux/ui" PROJECT_DIR)
+
+set(FLUTTER_VERSION "1.0.0+1" PARENT_SCOPE)
+set(FLUTTER_VERSION_MAJOR 1 PARENT_SCOPE)
+set(FLUTTER_VERSION_MINOR 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_PATCH 0 PARENT_SCOPE)
+set(FLUTTER_VERSION_BUILD 1 PARENT_SCOPE)
+
+# Environment variables to pass to tool_backend.sh
+list(APPEND FLUTTER_TOOL_ENVIRONMENT
+  "FLUTTER_ROOT=/home/mariussincovici/snap/flutter/common/flutter"
+  "PROJECT_DIR=/home/mariussincovici/projects/nordvpn-linux/ui"
+  "DART_DEFINES=RkxVVFRFUl9XRUJfQVVUT19ERVRFQ1Q9dHJ1ZQ==,RkxVVFRFUl9XRUJfQ0FOVkFTS0lUX1VSTD1odHRwczovL3d3dy5nc3RhdGljLmNvbS9mbHV0dGVyLWNhbnZhc2tpdC9mNDBlOTc2YmVkZmY1N2U2OWUxYjNkODlhN2MyYTNjNjE3YTAzZGFkLw=="
+  "DART_OBFUSCATION=false"
+  "TRACK_WIDGET_CREATION=true"
+  "TREE_SHAKE_ICONS=false"
+  "PACKAGE_CONFIG=/home/mariussincovici/projects/nordvpn-linux/ui/.dart_tool/package_config.json"
+  "FLUTTER_TARGET=/home/mariussincovici/projects/nordvpn-linux/ui/lib/main.dart"
+)
