d19477895e02b0c0de295cd90c69fbd78688af8f
keliramu <ramunas.keliuotis@nordsec.com>
2024-02-23 15:48:54 +0200

    Implement limitations to prevent daemon crashes under stress
    Signed-off-by: keliramu <ramunas.keliuotis@nordsec.com>

--- internal/socket.go
+++ internal/socket.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"net"
 	"os/user"
+	"reflect"
 	"strconv"
 	"strings"
 	"sync"
@@ -13,78 +14,19 @@ import (
 	"google.golang.org/grpc/credentials"
 )
 
-// getUnixCreds returns info from unix socket connection about the process on the other end.
-func getUnixCreds(conn net.Conn) (*unix.Ucred, error) {
-	unixConn, ok := conn.(*net.UnixConn)
-	if !ok {
-		return nil, fmt.Errorf("socket is not a unix socket")
-	}
-
-	rawConn, err := unixConn.SyscallConn()
-	if err != nil {
-		return nil, fmt.Errorf("getting raw connection: %w", err)
-	}
-
-	var ucred *unix.Ucred
-	var internalErr error
-	err = rawConn.Control(func(fd uintptr) {
-		ucred, internalErr = unix.GetsockoptUcred(int(fd), unix.SOL_SOCKET, unix.SO_PEERCRED)
-	})
-	if internalErr != nil {
-		return nil, fmt.Errorf("doing GetsockoptUcred: %w", internalErr)
-	}
-	if err != nil {
-		return nil, fmt.Errorf("doing rawConn Control: %w", err)
-	}
-
-	if err := authenticateUser(ucred, []string{"nordvpn"}); err != nil {
-		return nil, err
-	}
-
-	return ucred, nil
-}
-
-// serialize user information handling
-var here_lock sync.Mutex = sync.Mutex{}
-
-func authenticateUser(ucred *unix.Ucred, allowGroups []string) error {
-	here_lock.Lock()
-	defer here_lock.Unlock()
-
-	// root?
-	if ucred.Uid == 0 {
-		return nil
-	}
-	userInfo, err := user.LookupId(fmt.Sprintf("%d", ucred.Uid))
-	if err != nil {
-		return fmt.Errorf("authenticate user, lookup user info: %s", err)
-	}
-	// user belongs to the allowed group?
-	groups, err := userInfo.GroupIds()
-	if err != nil {
-		return fmt.Errorf("authenticate user, check user groups: %s", err)
-	}
-	for _, groupId := range groups {
-		groupInfo, err := user.LookupGroupId(groupId)
-		if err != nil {
-			return fmt.Errorf("authenticate user, check user group: %s", err)
-		}
-		for _, allowGroupName := range allowGroups {
-			if groupInfo.Name == allowGroupName {
-				return nil
-			}
-		}
-	}
-	return fmt.Errorf("requesting user does not have permissions")
-}
-
 // UnixSocketCredentials is used to retrieve linux user ID from unix socket connection between client and daemon
 // Implements credentials.TransportCredentials to be passed to gRPC server initialization
-type UnixSocketCredentials struct{}
+type UnixSocketCredentials struct {
+	mu sync.Mutex
+}
 
 // ServerHandshake is called when client connects to daemon.
 // We retrieve user ID which opened the client here.
-func (UnixSocketCredentials) ServerHandshake(c net.Conn) (net.Conn, credentials.AuthInfo, error) {
+func (cr *UnixSocketCredentials) ServerHandshake(c net.Conn) (net.Conn, credentials.AuthInfo, error) {
+	// under snap and under stress load, cgo calls cause crash or deadlock, need serialize
+	cr.mu.Lock()
+	defer cr.mu.Unlock()
+
 	creds, err := getUnixCreds(c)
 	if err != nil || creds == nil {
 		return c, UcredAuth{}, err
@@ -94,22 +36,22 @@ func (UnixSocketCredentials) ServerHandshake(c net.Conn) (net.Conn, credentials.
 }
 
 // ClientHandshake is a stub to implement credentials.TransportCredentials
-func (UnixSocketCredentials) ClientHandshake(_ context.Context, _ string, c net.Conn) (net.Conn, credentials.AuthInfo, error) {
+func (cr *UnixSocketCredentials) ClientHandshake(_ context.Context, _ string, c net.Conn) (net.Conn, credentials.AuthInfo, error) {
 	return c, nil, nil
 }
 
 // Info is a stub to implement credentials.TransportCredentials
-func (UnixSocketCredentials) Info() credentials.ProtocolInfo {
+func (cr *UnixSocketCredentials) Info() credentials.ProtocolInfo {
 	return credentials.ProtocolInfo{}
 }
 
 // Clone is a stub to implement credentials.TransportCredentials
-func (UnixSocketCredentials) Clone() credentials.TransportCredentials {
-	return UnixSocketCredentials{}
+func (cr *UnixSocketCredentials) Clone() credentials.TransportCredentials {
+	return cr
 }
 
 // OverrideServerName is a stub to implement credentials.TransportCredentials
-func (UnixSocketCredentials) OverrideServerName(string) error {
+func (cr *UnixSocketCredentials) OverrideServerName(string) error {
 	return nil
 }
 
@@ -146,4 +88,85 @@ func StringToUcred(ucredStr string) (unix.Ucred, error) {
 	}
 
 	return unix.Ucred{Pid: int32(pid), Uid: uint32(uid), Gid: uint32(gid)}, nil
+}
+
+// getUnixCreds returns info from unix socket connection about the process on the other end.
+func getUnixCreds(conn net.Conn) (*unix.Ucred, error) {
+	conn2 := extractConnection(conn)
+	if conn2 == nil {
+		return nil, fmt.Errorf("cannot extract connection of proper type, is netutil.LimitListener used?")
+	}
+
+	unixConn, ok := conn2.(*net.UnixConn)
+	if !ok {
+		return nil, fmt.Errorf("socket is not a unix socket")
+	}
+
+	rawConn, err := unixConn.SyscallConn()
+	if err != nil {
+		return nil, fmt.Errorf("getting raw connection: %w", err)
+	}
+
+	var ucred *unix.Ucred
+	var internalErr error
+	err = rawConn.Control(func(fd uintptr) {
+		ucred, internalErr = unix.GetsockoptUcred(int(fd), unix.SOL_SOCKET, unix.SO_PEERCRED)
+	})
+	if internalErr != nil {
+		return nil, fmt.Errorf("doing GetsockoptUcred: %w", internalErr)
+	}
+	if err != nil {
+		return nil, fmt.Errorf("doing rawConn Control: %w", err)
+	}
+
+	if err := authenticateUser(ucred, []string{"nordvpn"}); err != nil {
+		return nil, err
+	}
+
+	return ucred, nil
+}
+
+func authenticateUser(ucred *unix.Ucred, allowGroups []string) error {
+	// root?
+	if ucred.Uid == 0 {
+		return nil
+	}
+	userInfo, err := user.LookupId(fmt.Sprintf("%d", int(ucred.Uid)))
+	if err != nil {
+		return fmt.Errorf("authenticate user, lookup user info: %s", err)
+	}
+	// user belongs to the allowed group?
+	groups, err := userInfo.GroupIds()
+	if err != nil {
+		return fmt.Errorf("authenticate user, check user groups: %s", err)
+	}
+	for _, groupId := range groups {
+		groupInfo, err := user.LookupGroupId(groupId)
+		if err != nil {
+			return fmt.Errorf("authenticate user, check user group: %s", err)
+		}
+		for _, allowGroupName := range allowGroups {
+			if groupInfo.Name == allowGroupName {
+				return nil
+			}
+		}
+	}
+	return fmt.Errorf("requesting user does not have permissions")
+}
+
+// using `netutil.LimitListener` but it has internal unexported type `limitListenerConn`
+// which wraps original `net.UnixConn` value inside by embeding abstract interface `net.Conn`
+// this way we cannot access `net.UnixConn` value, because of that we use `go reflection`
+// to extract original wrapped value.
+func extractConnection(c interface{}) net.Conn {
+	v := reflect.ValueOf(c)
+	if v.Kind() == reflect.Ptr {
+		v = reflect.Indirect(v)
+	}
+	if v.Kind() == reflect.Struct {
+		y := v.FieldByName("Conn")
+		cc := y.Interface().(net.Conn)
+		return cc
+	}
+	return nil
 }
