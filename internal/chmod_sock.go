63447cd4dfdbbd779898e7b179e8092a010e0d93
Savolro <me@savolro.com>
2024-03-06 10:09:23 +0200

    Add mode parameter to `UpdateFilePermissions`
    Signed-off-by: Savolro <me@savolro.com>

--- internal/chmod_sock.go
+++ internal/chmod_sock.go
@@ -5,13 +5,13 @@ import (
 	"os"
 )
 
-// UpdateSocketFilePermissions set socket file permissions
-func UpdateSocketFilePermissions(sockFileName string) {
-	if !FileExists(sockFileName) {
+// UpdateFilePermissions sets permissions of a given file if it exists and logs the error to stdout
+func UpdateFilePermissions(name string, mode os.FileMode) {
+	if !FileExists(name) {
 		return
 	}
 	// #nosec G302 -- need world writable permissions
-	if err := os.Chmod(sockFileName, PermUserRWGroupRWOthersRW); err != nil {
+	if err := os.Chmod(name, mode); err != nil {
 		log.Println(ErrorPrefix, err)
 	}
 }
