4c818d68fd441d5c8f7ab2c84acf9ab8cba1ce81
keliramu <ramunas.keliuotis@nordsec.com>
2024-03-18 13:37:35 +0200

    Improve local builds - do not build openvpn if it exists
    Signed-off-by: keliramu <ramunas.keliuotis@nordsec.com>

--- magefiles/mage.go
+++ magefiles/mage.go
@@ -208,8 +208,12 @@ func (Build) Notices() error {
 func buildPackage(packageType string, buildFlags string) error {
 	mg.Deps(Build.Data)
 	mg.Deps(mg.F(buildBinaries, buildFlags))
-	mg.Deps(Build.Openvpn)
 	mg.Deps(Build.Notices)
+
+	// do not build openvpn dependency if it already exists
+	if !internal.FileExists(fmt.Sprintf("./bin/deps/openvpn/%s/latest/openvpn", build.Default.GOARCH)) {
+		mg.Deps(Build.Openvpn)
+	}
 
 	env, err := getEnv()
 	if err != nil {
@@ -246,8 +250,12 @@ func (Build) Snap() error {
 func buildPackageDocker(ctx context.Context, packageType string, buildFlags string) error {
 	mg.Deps(Build.Data)
 	mg.Deps(mg.F(buildBinariesDocker, buildFlags))
-	mg.Deps(Build.OpenvpnDocker)
 	mg.Deps(Build.Notices)
+
+	// do not build openvpn dependency if it already exists
+	if !internal.FileExists(fmt.Sprintf("./bin/deps/openvpn/%s/latest/openvpn", build.Default.GOARCH)) {
+		mg.Deps(Build.OpenvpnDocker)
+	}
 
 	env, err := getEnv()
 	if err != nil {
