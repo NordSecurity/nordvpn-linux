b6612042880ac5abb6b1a499d0980568faf8c469
Piotr Jurkiewicz <piotr.jurkiewicz@nordsec.com>
2024-03-21 00:47:37 +0100

    Add tray implementation
    Signed-off-by: Piotr Jurkiewicz <piotr.jurkiewicz@nordsec.com>

---
+++ tray/menu.go
@@ -0,0 +1,173 @@
+package tray
+
+import (
+	"os/exec"
+	"strings"
+
+	"github.com/NordSecurity/systray"
+)
+
+func addDebugSection() {
+	systray.AddSeparator()
+	mRedraw := systray.AddMenuItem("Redraw", "Redraw")
+	go func() {
+		for {
+			<-mRedraw.ClickedCh
+			redrawChan <- struct{}{}
+		}
+	}()
+	mUpdate := systray.AddMenuItem("Update", "Update")
+	go func() {
+		for {
+			<-mUpdate.ClickedCh
+			updateChan <- false
+		}
+	}()
+	mUpdateFull := systray.AddMenuItem("Full update", "Full update")
+	go func() {
+		for {
+			<-mUpdateFull.ClickedCh
+			updateChan <- true
+		}
+	}()
+}
+
+func addQuitItem() {
+	systray.AddSeparator()
+	m := systray.AddMenuItem("Quit", "Quit")
+	m.Enable()
+	go func() {
+		<-m.ClickedCh
+		systray.Quit()
+	}()
+}
+
+func addAppSection() {
+	m := systray.AddMenuItem("NordVPN", "NordVPN")
+	go func() {
+		for {
+			<-m.ClickedCh
+			cmd := exec.Command("zenity", "--info", "--text", "This will be the NordVPN GUI", "--no-wrap")
+			err := cmd.Start()
+			if err != nil {
+				notification("error", "Failed to start NordVPN GUI: %s", err)
+			} else {
+				err = cmd.Wait()
+			}
+		}
+	}()
+}
+
+func addDaemonSection() {
+	systray.AddSeparator()
+	m := systray.AddMenuItem("Daemon not available", "Daemon not available")
+	m.Disable()
+
+	mError := systray.AddMenuItem(state.daemonError, state.daemonError)
+	mError.Disable()
+}
+
+func addVpnSection() {
+	systray.AddSeparator()
+
+	mStatus := systray.AddMenuItem("VPN "+strings.ToLower(state.vpnStatus), "VPN "+strings.ToLower(state.vpnStatus))
+	mStatus.Disable()
+
+	if state.vpnStatus == "Connected" {
+		mHostname := systray.AddMenuItem("Server: "+state.vpnHostname, "Server: "+state.vpnHostname)
+		mHostname.Disable()
+		mCity := systray.AddMenuItem("City: "+state.vpnCity, "City: "+state.vpnCity)
+		mCity.Disable()
+		mCountry := systray.AddMenuItem("Country: "+state.vpnCountry, "Country: "+state.vpnCountry)
+		mCountry.Disable()
+		mDisconnect := systray.AddMenuItem("Disconnect", "Disconnect")
+		go func() {
+			success := false
+			for !success {
+				<-mDisconnect.ClickedCh
+				success = disconnect(client)
+			}
+			updateChan <- true
+		}()
+	} else {
+		mConnect := systray.AddMenuItem("Quick Connect", "Quick Connect")
+		go func() {
+			success := false
+			for !success {
+				<-mConnect.ClickedCh
+				success = connect(client, "", "")
+			}
+			updateChan <- true
+		}()
+	}
+}
+
+func addMeshnetSection() {
+	systray.AddSeparator()
+
+	status := ""
+	if state.meshnetEnabled {
+		status = "enabled"
+	} else {
+		status = "disabled"
+	}
+	mStatus := systray.AddMenuItem("Meshnet "+status, "Meshnet "+status)
+	mStatus.Disable()
+
+	if state.meshnetEnabled {
+		mDisconnect := systray.AddMenuItem("Disable", "Disable")
+		go func() {
+			success := false
+			for !success {
+				<-mDisconnect.ClickedCh
+				success = disableMeshnet(meshClient)
+			}
+			updateChan <- true
+		}()
+	} else {
+		mConnect := systray.AddMenuItem("Enable", "Enable")
+		go func() {
+			success := false
+			for !success {
+				<-mConnect.ClickedCh
+				success = enableMeshnet(meshClient)
+			}
+			updateChan <- true
+		}()
+	}
+}
+
+func addAccountSection() {
+	systray.AddSeparator()
+
+	if state.loggedIn {
+		m := systray.AddMenuItem("Logged in as:", "Logged in as:")
+		m.Disable()
+
+		mName := systray.AddMenuItem(state.accountName, state.accountName)
+		mName.Disable()
+
+		mLogout := systray.AddMenuItem("Log out", "Log out")
+
+		go func() {
+			success := false
+			for !success {
+				<-mLogout.ClickedCh
+				success = logout(client, false)
+			}
+			updateChan <- true
+		}()
+	} else {
+		m := systray.AddMenuItem("Not logged in", "Not logged in")
+		m.Disable()
+
+		mLogin := systray.AddMenuItem("Log in", "Log in")
+
+		go func() {
+			for {
+				<-mLogin.ClickedCh
+				login(client)
+			}
+		}()
+	}
+}
---
+++ tray/monitor.go
@@ -0,0 +1,294 @@
+package tray
+
+import (
+	"context"
+	"fmt"
+	"strings"
+	"sync"
+	"time"
+
+	"github.com/NordSecurity/nordvpn-linux/cli"
+	"github.com/NordSecurity/nordvpn-linux/daemon/pb"
+	"github.com/NordSecurity/nordvpn-linux/internal"
+	meshpb "github.com/NordSecurity/nordvpn-linux/meshnet/pb"
+
+	"github.com/NordSecurity/systray"
+	"github.com/fatih/color"
+	"google.golang.org/grpc/status"
+)
+
+type stateType struct {
+	daemonAvailable bool
+	loggedIn        bool
+	vpnActive       bool
+	meshnetEnabled  bool
+	daemonError     string
+	accountName     string
+	vpnStatus       string
+	vpnHostname     string
+	vpnCity         string
+	vpnCountry      string
+	mu              sync.RWMutex
+}
+
+var state = stateType{vpnStatus: "Disconnected"}
+
+// The pattern is to return 'true' if something has changed and 'false' when no changes were detected
+
+func ping(client pb.DaemonClient) bool {
+	changed := false
+	daemonAvailable := false
+	daemonError := ""
+
+	resp, err := client.Ping(context.Background(), &pb.Empty{})
+	if err != nil {
+		daemonError = internal.ErrDaemonConnectionRefused.Error()
+		if strings.Contains(err.Error(), "no such file or directory") {
+			daemonError = "nordvpnd is not running"
+		}
+		if strings.Contains(err.Error(), "permission denied") {
+			daemonError = "add a user to the nordvpn group"
+		}
+		if snapErr := cli.RetrieveSnapConnsError(err); snapErr != nil {
+			daemonError = cli.FormatSnapMissingConnsErr(snapErr)
+		}
+	} else {
+		switch resp.Type {
+		case internal.CodeOffline:
+			daemonError = cli.ErrInternetConnection.Error()
+		case internal.CodeDaemonOffline:
+			daemonError = internal.ErrDaemonConnectionRefused.Error()
+		case internal.CodeOutdated:
+			daemonError = cli.ErrUpdateAvailable.Error()
+		}
+	}
+
+	if daemonError == "" {
+		daemonAvailable = true
+	}
+
+	state.mu.Lock()
+
+	if state.daemonAvailable == false && daemonAvailable == true {
+		state.daemonAvailable = true
+		changed = true
+		if notifyEnabled {
+			defer notification("info", "Connected to NordVPN daemon")
+		}
+	} else if state.daemonAvailable == true && daemonAvailable == false {
+		state.daemonAvailable = false
+		changed = true
+		if notifyEnabled {
+			defer notification("info", "Disconnected from NordVPN daemon")
+		}
+	}
+
+	if state.daemonError != daemonError {
+		state.daemonError = daemonError
+		changed = true
+	}
+
+	state.mu.Unlock()
+	return changed
+}
+
+func fetchLogged(client pb.DaemonClient) bool {
+	changed := false
+	resp, err := client.IsLoggedIn(context.Background(), &pb.Empty{})
+	loggedIn := err == nil && resp.GetValue()
+
+	state.mu.Lock()
+
+	if state.loggedIn == false && loggedIn == true {
+		state.loggedIn = true
+		changed = true
+		if notifyEnabled {
+			defer notification("info", "Logged in")
+		}
+	} else if state.loggedIn == true && loggedIn == false {
+		state.loggedIn = false
+		changed = true
+		if notifyEnabled {
+			defer notification("info", "Logged out")
+		}
+	}
+
+	state.mu.Unlock()
+	return changed
+}
+
+func fetchMeshnet(meshClient meshpb.MeshnetClient) bool {
+	changed := false
+	meshResp, err := meshClient.IsEnabled(context.Background(), &meshpb.Empty{})
+	meshnetEnabled := err == nil && meshResp.GetValue()
+
+	state.mu.Lock()
+
+	if state.meshnetEnabled == false && meshnetEnabled == true {
+		state.meshnetEnabled = true
+		changed = true
+		if notifyEnabled {
+			defer notification("info", "Meshnet enabled")
+		}
+	} else if state.meshnetEnabled == true && meshnetEnabled == false {
+		state.meshnetEnabled = false
+		changed = true
+		if notifyEnabled {
+			defer notification("info", "Meshnet disabled")
+		}
+	}
+
+	state.mu.Unlock()
+	return changed
+}
+
+func fetchStatus(client pb.DaemonClient) bool {
+	changed := false
+	vpnStatus := ""
+	vpnHostname := ""
+	vpnCity := ""
+	vpnCountry := ""
+	resp, err := client.Status(context.Background(), &pb.Empty{})
+	if err == nil {
+		vpnStatus = resp.State
+		vpnHostname = resp.Hostname
+		vpnCity = resp.City
+		vpnCountry = resp.Country
+	}
+
+	state.mu.Lock()
+
+	if state.vpnStatus != vpnStatus {
+		if vpnStatus == "Connected" {
+			systray.SetIconName(iconConnected)
+			if notifyEnabled {
+				defer notification("info", "Connected to VPN server: %s", vpnHostname)
+			}
+		} else {
+			systray.SetIconName(iconDisconnected)
+			if notifyEnabled {
+				defer notification("info", "Disconnected from VPN server")
+			}
+		}
+		state.vpnStatus = vpnStatus
+		changed = true
+	}
+
+	if state.vpnHostname != vpnHostname {
+		state.vpnHostname = vpnHostname
+		changed = true
+	}
+
+	state.vpnCity = vpnCity
+	state.vpnCountry = vpnCountry
+
+	state.mu.Unlock()
+	return changed
+}
+
+func accountInfo(client pb.DaemonClient) bool {
+	changed := false
+	loggedIn := false
+	vpnActive := false
+	accountName := ""
+
+	payload, err := client.AccountInfo(context.Background(), &pb.Empty{})
+	if err != nil {
+		if status.Convert(err).Message() != internal.ErrNotLoggedIn.Error() {
+			color.Red("Error retrieving account info: %s", err)
+		}
+	} else {
+		switch payload.Type {
+		case internal.CodeUnauthorized:
+			color.Red(cli.AccountTokenUnauthorizedError)
+		case internal.CodeExpiredRenewToken:
+			color.Red("CodeExpiredRenewToken")
+		case internal.CodeTokenRenewError:
+			color.Red("CodeTokenRenewError")
+		default:
+			loggedIn = true
+		}
+
+		if payload.Username != "" {
+			accountName = payload.Username
+		} else {
+			accountName = payload.Email
+		}
+
+		switch payload.Type {
+		case internal.CodeSuccess:
+			vpnActive = true
+		case internal.CodeNoVPNService:
+			vpnActive = false
+		}
+	}
+
+	state.mu.Lock()
+
+	if state.loggedIn != loggedIn {
+		state.loggedIn = loggedIn
+		changed = true
+	}
+
+	if state.vpnActive != vpnActive {
+		state.vpnActive = vpnActive
+		changed = true
+	}
+
+	if state.accountName != accountName {
+		state.accountName = accountName
+		changed = true
+	}
+
+	state.mu.Unlock()
+	return changed
+}
+
+func maybeRedraw(result bool, previous bool) bool {
+	if result {
+		redrawChan <- struct{}{}
+	}
+	return result || previous
+}
+
+func pollingMonitor(client pb.DaemonClient, update <-chan bool, ticker <-chan time.Time) {
+	fullUpdate := true
+	fullUpdateLast := time.Time{}
+	for {
+		changed := false
+		fullUpdate = maybeRedraw(ping(client), fullUpdate)
+		if state.daemonAvailable {
+			fullUpdate = maybeRedraw(fetchLogged(client), fullUpdate)
+			if state.loggedIn {
+				fullUpdate = maybeRedraw(fetchMeshnet(meshClient), fullUpdate)
+				fullUpdate = maybeRedraw(fetchStatus(client), fullUpdate)
+				if fullUpdate {
+					changed = accountInfo(client)
+					fullUpdateLast = time.Now()
+				}
+			}
+		}
+
+		if changed {
+			redrawChan <- struct{}{}
+		}
+		select {
+		case fullUpdate = <-update:
+		case <-systray.TrayOpenedCh:
+			fullUpdate = true
+		case ts := <-ticker:
+			if ts.Sub(fullUpdateLast) > PollingFullUpdateInterval {
+				fullUpdate = true
+			} else {
+				fullUpdate = false
+			}
+		}
+		if debugMode {
+			if fullUpdate {
+				fmt.Println(time.Now().String(), "Full update")
+			} else {
+				fmt.Println(time.Now().String(), "Update")
+			}
+		}
+	}
+}
