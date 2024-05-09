43ade03c759b7362e0b7f2a9ccfbad1184779dd8
Bartosz Oleaczek <122802123+bartoszWojciechO@users.noreply.github.com>
2024-05-09 07:12:01 +0200

    Add a delay before starting mesh peer list tests

--- test/qa/test_meshnet_peer_list.py
+++ test/qa/test_meshnet_peer_list.py
@@ -2,7 +2,7 @@ import pytest
 import sh
 import timeout_decorator
 
-from lib import meshnet, ssh
+from lib import meshnet, poll, ssh
 
 ssh_client = ssh.Ssh("qa-peer", "root", "root")
 
@@ -81,12 +81,17 @@ def test_meshnet_peer_list_permission_filters(allows_incoming_traffic, allows_ro
     base_test_peer_list(filter_list)
 
 
-@pytest.mark.skip("LVPN-4860")
 def test_meshnet_peer_list_peer_connected():
-    local_peer_list = sh.nordvpn.mesh.peer.list(_tty_out=False)
-    remote_peer_list = ssh_client.exec_command("nordvpn mesh peer list")
+    def is_peer_connected():
+        local_peer_list = sh.nordvpn.mesh.peer.list(_tty_out=False)
+        remote_peer_list = ssh_client.exec_command("nordvpn mesh peer list")
 
-    peer_lists = [local_peer_list, remote_peer_list]
+        peer_lists = [local_peer_list, remote_peer_list]
+        return ("Status: connected" in list for list in peer_lists)
 
-    for list in peer_lists:
-        assert "Status: connected" in list, "Status: connected not found in peer list"
+    # There is a delay between when peer comes online and when it affect its status when listing peers.
+    for result in poll(is_peer_connected):
+        if result:
+            break
+
+    assert result, "Status: connected not found in peer list"
