3a4effaf98d0df5152250ce16d1c971e496f9115
Marius-Mihai Sincovici <marius.sincovici@nordsec.com>
2024-05-30 14:48:54 +0200

    Display the tray after settings were fetched
    Signed-off-by: Marius-Mihai Sincovici <marius.sincovici@nordsec.com>

--- tray/monitor.go
+++ tray/monitor.go
@@ -241,6 +241,10 @@ func (ti *Instance) pollingMonitor() {
 	fullUpdate := true
 	fullUpdateLast := time.Time{}
 	for {
+		if ti.state.notificationsStatus == Invalid {
+			ti.updateSettings()
+		}
+
 		ti.redraw(ti.ping())
 		if ti.state.daemonAvailable {
 			ti.redraw(ti.updateLoginStatus())
@@ -258,12 +262,15 @@ func (ti *Instance) pollingMonitor() {
 			}
 		}
 
-		if initialChan != nil {
-			initialChan <- struct{}{}
-			close(initialChan)
-			initialChan = nil
-			if ti.debugMode {
-				log.Println(internal.DebugPrefix, "Initial retrieve")
+		// while the settings were not fetch don't unblock the tray loop
+		if ti.state.trayStatus != Invalid {
+			if initialChan != nil {
+				initialChan <- struct{}{}
+				close(initialChan)
+				initialChan = nil
+				if ti.debugMode {
+					log.Println(internal.DebugPrefix, "Initial retrieve")
+				}
 			}
 		}
 
