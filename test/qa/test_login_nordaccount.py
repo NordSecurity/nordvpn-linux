6d9a60ee5c2f0c654cf7f83468c8c52e435f24db
Piotr Jurkiewicz <piotr.jurkiewicz@nordsec.com>
2024-06-10 16:46:24 +0200

    Add separate tester-selenium Docker image
    Signed-off-by: Piotr Jurkiewicz <piotr.jurkiewicz@nordsec.com>

--- test/qa/test_login_nordaccount.py
+++ test/qa/test_login_nordaccount.py
@@ -8,6 +8,7 @@ from lib import (
     info,
     logging,
     login,
+    selenium,
 )
 
 
@@ -26,7 +27,7 @@ def teardown_function(function):  # noqa: ARG001
 
 
 @pytest.mark.skip("Does not work on Docker")
-@pytest.mark.parametrize("login_flag", login.LOGIN_FLAG)
+@pytest.mark.parametrize("login_flag", selenium.LOGIN_FLAG)
 @timeout_decorator.timeout(60)
 def test_selenium_login(login_flag):
     preferences = [
@@ -35,10 +36,10 @@ def test_selenium_login(login_flag):
         ["network.protocol-handler.app.nordvpn", "/usr/bin/nordvpn"]
         ]
 
-    selenium = login.SeleniumBrowser(preferences)
-    browser = selenium.browser_get()
+    sel = selenium.SeleniumBrowser(preferences)
+    browser = sel.browser_get()
 
-    with lib.Defer(selenium.browser_kill):
+    with lib.Defer(sel.browser_kill):
         # Get login link from NordVPN app, trim all spaces & chars after link itself
         login_link = sh.nordvpn.login(login_flag, _tty_out=False).strip().split(": ")[1]
         print(f"Login link: {login_link}\n")
@@ -50,31 +51,31 @@ def test_selenium_login(login_flag):
         try:
             credentials = login.get_credentials("default")
             # Username page
-            selenium.browser_element_interact(login.NA_USERNAME_PAGE_TEXTBOX_XPATH, credentials.email)
-            selenium.browser_element_interact(login.NA_USERNAME_PAGE_BUTTON_XPATH)
+            sel.browser_element_interact(selenium.NA_USERNAME_PAGE_TEXTBOX_XPATH, credentials.email)
+            sel.browser_element_interact(selenium.NA_USERNAME_PAGE_BUTTON_XPATH)
 
             # Password page
-            selenium.browser_element_interact(login.NA_PASSWORD_PAGE_TEXTBOX_XPATH, credentials.password)
-            selenium.browser_element_interact(login.NA_PASSWORD_PAGE_BUTTON_XPATH)
+            sel.browser_element_interact(selenium.NA_PASSWORD_PAGE_TEXTBOX_XPATH, credentials.password)
+            sel.browser_element_interact(selenium.NA_PASSWORD_PAGE_BUTTON_XPATH)
 
             # Continue to app page
-            selenium.browser_element_interact(login.NA_CONTINUE_PAGE_LINK_BUTTON)
+            sel.browser_element_interact(selenium.NA_CONTINUE_PAGE_LINK_BUTTON)
         except:  # noqa: E722
-            browser.save_screenshot(login.BROWSER_LOGS_PATH + "Screenshot.png")
+            browser.save_screenshot(selenium.BROWSER_LOGS_PATH + "Screenshot.png")
             pytest.fail()
 
         output = sh.nordvpn.logout(_tty_out=False)
         print(f"Logout action output: {output}\n")
-        assert login.LOGOUT_MSG_SUCCESS in output
+        assert selenium.LOGOUT_MSG_SUCCESS in output
 
 
-@pytest.mark.parametrize("login_flag", login.LOGIN_FLAG)
+@pytest.mark.parametrize("login_flag", selenium.LOGIN_FLAG)
 @timeout_decorator.timeout(60)
 def test_selenium_login_callback(login_flag):
-    selenium = login.SeleniumBrowser()
-    browser = selenium.browser_get()
+    sel = selenium.SeleniumBrowser()
+    browser = sel.browser_get()
 
-    with lib.Defer(selenium.browser_kill):
+    with lib.Defer(sel.browser_kill):
         # Get login link from NordVPN app, trim all spaces & chars after link itself
         login_link = sh.nordvpn.login(login_flag, _tty_out=False).strip().split(": ")[1]
         print(f"Login link: {login_link}\n")
@@ -86,21 +87,21 @@ def test_selenium_login_callback(login_flag):
         try:
             credentials = login.get_credentials("default")
             # Username page
-            selenium.browser_element_interact(login.NA_USERNAME_PAGE_TEXTBOX_XPATH, credentials.email)
-            selenium.browser_element_interact(login.NA_USERNAME_PAGE_BUTTON_XPATH)
+            sel.browser_element_interact(selenium.NA_USERNAME_PAGE_TEXTBOX_XPATH, credentials.email)
+            sel.browser_element_interact(selenium.NA_USERNAME_PAGE_BUTTON_XPATH)
 
             # Password page
-            selenium.browser_element_interact(login.NA_PASSWORD_PAGE_TEXTBOX_XPATH, credentials.password)
-            selenium.browser_element_interact(login.NA_PASSWORD_PAGE_BUTTON_XPATH)
+            sel.browser_element_interact(selenium.NA_PASSWORD_PAGE_TEXTBOX_XPATH, credentials.password)
+            sel.browser_element_interact(selenium.NA_PASSWORD_PAGE_BUTTON_XPATH)
 
             # Continue to app page
             # preferences not set in constructor, so when we click link it does not redirect us to app.
-            callback_link = selenium.browser_element_interact(login.NA_CONTINUE_PAGE_LINK_BUTTON, return_attribute='href')
+            callback_link = sel.browser_element_interact(selenium.NA_CONTINUE_PAGE_LINK_BUTTON, return_attribute='href')
             print(f"Callback URL: {callback_link}\n")
         except:  # noqa: E722
-            browser.save_screenshot(login.BROWSER_LOGS_PATH + "Screenshot.png")
+            browser.save_screenshot(selenium.BROWSER_LOGS_PATH + "Screenshot.png")
             pytest.fail()
 
         output = sh.nordvpn.login("--callback", callback_link, _tty_out=False)
         print(f"Callback login action output: {output}\n")
-        assert login.LOGIN_MSG_SUCCESS in output
+        assert selenium.LOGIN_MSG_SUCCESS in output
