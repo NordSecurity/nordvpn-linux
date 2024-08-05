91c7808d44592a1cb93d8ff297d88b26a7570dd2
Bartosz Zbytniewski <bartosz.zbytniewski@nordsec.com>
2024-08-05 08:24:21 +0200

    Fix issue with multiple telio instances

--- cmd/daemon/vpn_telio.go
+++ cmd/daemon/vpn_telio.go
@@ -4,27 +4,31 @@ package main
 
 import (
 	"errors"
-	"fmt"
+	"log"
 
 	"github.com/NordSecurity/nordvpn-linux/config"
 	"github.com/NordSecurity/nordvpn-linux/daemon"
 	"github.com/NordSecurity/nordvpn-linux/daemon/vpn"
 	"github.com/NordSecurity/nordvpn-linux/daemon/vpn/nordlynx/libtelio"
 	"github.com/NordSecurity/nordvpn-linux/daemon/vpn/openvpn"
+	"github.com/NordSecurity/nordvpn-linux/internal"
 	"github.com/NordSecurity/nordvpn-linux/meshnet"
 )
 
 func getVpnFactory(eventsDbPath string, fwmark uint32, envIsDev bool,
 	cfg vpn.LibConfigGetter, deviceID, appVersion string, eventsPublisher *vpn.Events,
 ) daemon.FactoryFunc {
+	//
+	telio, err := libtelio.New(!envIsDev, eventsDbPath, fwmark, cfg, deviceID, appVersion, eventsPublisher)
+	if err != nil {
+		// don't exit with `err` here in case the factory will be called with
+		// technology different than `config.Technology_NORDLYNX`
+		log.Println(internal.WarningPrefix, "failed to create libtelio instance:", err)
+	}
 	return func(tech config.Technology) (vpn.VPN, error) {
 		switch tech {
 		case config.Technology_NORDLYNX:
-			telio, err := libtelio.New(!envIsDev, eventsDbPath, fwmark, cfg, deviceID, appVersion, eventsPublisher)
-			if err != nil {
-				return nil, fmt.Errorf("libtelio creation failed: %w", err)
-			}
-			return telio, nil
+			return telio, err
 		case config.Technology_OPENVPN:
 			return openvpn.New(fwmark, eventsPublisher), nil
 		default:
