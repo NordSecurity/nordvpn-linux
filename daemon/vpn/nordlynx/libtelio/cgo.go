29ca24e3473b34d0bf948b14efc0fcfd91f52287
Bartosz Zbytniewski <bartosz.zbytniewski@nordsec.com>
2024-08-05 10:18:33 +0200

    Test

--- daemon/vpn/nordlynx/libtelio/cgo.go
+++ daemon/vpn/nordlynx/libtelio/cgo.go
@@ -1,5 +1,7 @@
 package libtelio
 
+// test comment
+
 // #cgo amd64 LDFLAGS: -L${SRCDIR}/../../../../bin/deps/lib/amd64/latest -ltelio -lsqlite3
 // #cgo 386 LDFLAGS: -L${SRCDIR}/../../../../bin/deps/lib/i386/latest -ltelio -lsqlite3
 // #cgo arm LDFLAGS: -L${SRCDIR}/../../../../bin/deps/lib/armel/latest -ltelio -lsqlite3
