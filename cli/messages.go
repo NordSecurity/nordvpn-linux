2686ee701a77d9c4f8d70422de015f199d4a3bbf
Bartosz Oleaczek <bartosz.oleaczek@nordsec.com>
2024-08-05 08:34:56 +0200

    Update error message when DIP server is not selected

--- cli/messages.go
+++ cli/messages.go
@@ -118,7 +118,7 @@ Example: nordvpn set %s on`
 	ExpiredAccountMessage                  = "Your account has expired. Renew your subscription now to continue enjoying the ultimate privacy and security with NordVPN. %s" // TODO: update once we get new error message.
 	NoDedicatedIPMessage                   = "You don’t have a dedicated IP subscription. To get a personal IP address that belongs only to you, continue in the browser: \n%s"
 	NoDedidcatedIPServerMessage            = "This server isn't currently included in your dedicated IP subscription."
-	NoPreferredDedicatedIPLocationSelected = "It looks like you haven’t selected the preferred location for your dedicated IP. Please head over to Nord Account and set up your dedicated IP."
+	NoPreferredDedicatedIPLocationSelected = "It looks like you haven’t selected the preferred server location for your dedicated IP. Please head over to Nord Account and set up your dedicated IP server."
 	NoSuchCommand                          = "Command '%s' doesn't exist."
 	MsgListIsEmpty                         = "We couldn’t load the list of %s. Please try again later."
 
