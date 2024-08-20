a1deb4d6ec89401f516d2b6e332fd5358ca43cd4
keliramu <ramunas.keliuotis@nordsec.com>
2024-08-20 09:26:21 +0300

    Builder docker file adjustments
    Signed-off-by: keliramu <ramunas.keliuotis@nordsec.com>

--- magefiles/mage.go
+++ magefiles/mage.go
@@ -19,7 +19,7 @@ import (
 
 const (
 	registryPrefix         = "ghcr.io/nordsecurity/nordvpn-linux/"
-	imageBuilder           = registryPrefix + "builder:1.3.0"
+	imageBuilder           = registryPrefix + "builder:1.3.1"
 	imagePackager          = registryPrefix + "packager:1.3.0"
 	imageSnapPackager      = registryPrefix + "snaper:0.0.2"
 	imageProtobufGenerator = registryPrefix + "generator:1.3.0"
