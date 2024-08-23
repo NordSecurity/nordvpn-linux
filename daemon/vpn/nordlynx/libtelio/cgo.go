057e58da126069196f8c713fb506788463883635
Bartosz Zbytniewski <bartosz.zbytniewski@nordsec.com>
2024-08-23 15:01:11 +0200

    Don't link sqlite for public builds

--- daemon/vpn/nordlynx/libtelio/cgo.go
+++ daemon/vpn/nordlynx/libtelio/cgo.go
@@ -1,9 +1,11 @@
+//go:build !internal
+
 package libtelio
 
-// #cgo amd64 LDFLAGS: -L${SRCDIR}/../../../../bin/deps/lib/amd64/latest -ltelio -lsqlite3
-// #cgo 386 LDFLAGS: -L${SRCDIR}/../../../../bin/deps/lib/i386/latest -ltelio -lsqlite3
-// #cgo arm LDFLAGS: -L${SRCDIR}/../../../../bin/deps/lib/armel/latest -ltelio -lsqlite3
-// #cgo arm LDFLAGS: -L${SRCDIR}/../../../../bin/deps/lib/armhf/latest -ltelio -lsqlite3
-// #cgo arm64 LDFLAGS: -L${SRCDIR}/../../../../bin/deps/lib/aarch64/latest -ltelio -lsqlite3
+// #cgo amd64 LDFLAGS: -L${SRCDIR}/../../../../bin/deps/lib/amd64/latest -ltelio
+// #cgo 386 LDFLAGS: -L${SRCDIR}/../../../../bin/deps/lib/i386/latest -ltelio
+// #cgo arm LDFLAGS: -L${SRCDIR}/../../../../bin/deps/lib/armel/latest -ltelio
+// #cgo arm LDFLAGS: -L${SRCDIR}/../../../../bin/deps/lib/armhf/latest -ltelio
+// #cgo arm64 LDFLAGS: -L${SRCDIR}/../../../../bin/deps/lib/aarch64/latest -ltelio
 // #cgo LDFLAGS: -ldl -lm
 import "C"
