13e0074b1ffe36718524c5fb38d4a5972641606c
keliramu <ramunas.keliuotis@nordsec.com>
2024-09-05 09:20:21 +0300

    Make binaries in snap package stripped same as deb/rpm
    Signed-off-by: keliramu <ramunas.keliuotis@nordsec.com>

--- ci/build_snap.sh
+++ ci/build_snap.sh
@@ -3,18 +3,17 @@ set -euxo pipefail
 
 source "${WORKDIR}/ci/env.sh"
 
-if [ "${ENVIRONMENT}" = "prod" ]; then
-    STRIP="$(which eu-strip 2>/dev/null)"
-    BASEDIR="bin/${ARCH}"
-    # shellcheck disable=SC2153
-    "${STRIP}" "${BASEDIR}"/nordvpnd
-    # shellcheck disable=SC2153
-    "${STRIP}" "${BASEDIR}"/nordvpn
-    # shellcheck disable=SC2153
-    "${STRIP}" "${BASEDIR}"/nordfileshare
-    # shellcheck disable=SC2153
-    "${STRIP}" "${BASEDIR}"/norduserd
-fi
+# snap package will have stripped binaries - same as deb/rpm
+STRIP="$(which eu-strip 2>/dev/null)"
+BASEDIR="bin/${ARCH}"
+# shellcheck disable=SC2153
+"${STRIP}" "${BASEDIR}"/nordvpnd
+# shellcheck disable=SC2153
+"${STRIP}" "${BASEDIR}"/nordvpn
+# shellcheck disable=SC2153
+"${STRIP}" "${BASEDIR}"/nordfileshare
+# shellcheck disable=SC2153
+"${STRIP}" "${BASEDIR}"/norduserd
 
 # build snap package
 snapcraft --destructive-mode
