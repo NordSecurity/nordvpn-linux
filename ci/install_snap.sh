4db6110138bf04ac728a6631d9435250b276cb0f
keliramu <ramunas.keliuotis@nordsec.com>
2024-09-27 15:15:50 +0300

    Automate snap tests
    Signed-off-by: keliramu <ramunas.keliuotis@nordsec.com>

--- ci/install_snap.sh
+++ ci/install_snap.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 set -euxo pipefail
 
+ARCH=$([ "${ARCH}" == "aarch64" ] && echo arm64 || echo "${ARCH}")
+
 # if host does not have ip6table modules loaded, we must loaded it the docker
 if [[ ! $(sudo ip6tables -S) ]]; then
     if [[ ! $(command -v modprobe) ]]; then
@@ -13,7 +15,7 @@ echo "~~~REMOVE previous SNAP package"
 sudo snap remove --purge nordvpn
 
 echo "~~~INSTALL new SNAP package"
-find "${WORKDIR}"/ -type f -name "*amd64.snap" \
+find "${WORKDIR}"/ -type f -name "*${ARCH}.snap" \
 	-exec sudo snap install --dangerous "{}" +
 
 echo "~~~GRANT permissions - connect snap interfaces"
