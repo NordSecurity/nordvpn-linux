2cc89c939f79f7d9b1bc83258a4bdcf60a9a1d28
Bartosz Oleaczek <bartosz.oleaczek@nordsec.com>
2024-10-09 10:18:57 +0200

    change <lib_name>_TAG_VERSION to <lib_name>_BINDINGS_VERSION

--- ci/set_bindings_version.sh
+++ ci/set_bindings_version.sh
@@ -12,9 +12,9 @@ declare -A LIB_NAME_TO_VERSION
 [[ -n "${LIBTELIO_VERSION:-}" ]] && LIB_NAME_TO_VERSION[libtelio]=$LIBTELIO_VERSION
 [[ -n "${LIBDROP_VERSION:-}" ]] && LIB_NAME_TO_VERSION[libdrop]=$LIBDROP_VERSION
 
-declare -A LIB_NAME_TO_TAG_VERSION
-[[ -n "${LIBTELIO_TAG_VERSION:-}" ]] && LIB_NAME_TO_TAG_VERSION[libtelio]=$LIBTELIO_TAG_VERSION
-[[ -n "${LIBDROP_TAG_VERSION:-}" ]] && LIB_NAME_TO_TAG_VERSION[libdrop]=$LIBDROP_TAG_VERSION
+declare -A LIB_NAME_TO_BINDINGS_VERSION
+[[ -n "${LIBTELIO_BINDINGS_VERSION:-}" ]] && LIB_NAME_TO_BINDINGS_VERSION[libtelio]=$LIBTELIO_BINDINGS_VERSION
+[[ -n "${LIBTELIO_BINDINGS_VERSION:-}" ]] && LIB_NAME_TO_BINDINGS_VERSION[libdrop]=$LIBTELIO_BINDINGS_VERSION
 
 lib_name=$1
 repo_path="${LIB_NAME_TO_PACKAGE[$lib_name]}"
@@ -23,13 +23,13 @@ if [[ -z "${lib_version}" ]]; then
   return 0
 fi
 
-tag_version="${LIB_NAME_TO_TAG_VERSION[$lib_name]:-}"
+bindings_version="${LIB_NAME_TO_BINDINGS_VERSION[$lib_name]:-}"
 
 major_version=$(echo "${lib_version}" | cut -d'.' -f1)
 
 full_package_path=$repo_path/$major_version@$lib_version
-if [[ -n "${tag_version}" ]]; then
-  full_package_path=$repo_path/$major_version@$tag_version
+if [[ -n "${bindings_version}" ]]; then
+  full_package_path=$repo_path/$major_version@$bindings_version
 fi
 
 go get "$full_package_path"
