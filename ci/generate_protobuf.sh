5dea1ac5b61e6ec7be2c4680c72215861808a8fd
Savolro <me@savolro.com>
2024-11-14 23:03:36 +0200

    Remove unneeded dependencies from protobuf files
    Signed-off-by: Savolro <me@savolro.com>

--- ci/generate_protobuf.sh
+++ ci/generate_protobuf.sh
@@ -5,10 +5,10 @@ protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf
 protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/daemon/config/technology.proto
 protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/daemon/config/group.proto
 protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/daemon/account.proto -I protobuf/daemon
-protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/daemon/cities.proto -I protobuf/daemon
+protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/daemon/cities.proto
 protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/daemon/common.proto -I protobuf/daemon
-protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/daemon/connect.proto -I protobuf/daemon
-protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/daemon/countries.proto -I protobuf/daemon
+protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/daemon/connect.proto
+protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/daemon/countries.proto
 protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/daemon/login.proto -I protobuf/daemon
 protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/daemon/logout.proto -I protobuf/daemon
 protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/daemon/login_with_token.proto -I protobuf/daemon
@@ -22,6 +22,7 @@ protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf
 protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/daemon/token.proto -I protobuf/daemon
 protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/daemon/purchase.proto -I protobuf/daemon
 protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/daemon/servers.proto -I protobuf/daemon
+protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/daemon/state.proto -I protobuf/daemon
 protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/meshnet/empty.proto -I protobuf/meshnet
 protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/meshnet/fsnotify.proto -I protobuf/meshnet
 protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/meshnet/service_response.proto -I protobuf/meshnet
@@ -31,7 +32,6 @@ protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf
 protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/fileshare/fileshare.proto -I protobuf/fileshare
 protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/snapconf/snapconf.proto -I protobuf/snapconf
 protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/norduser/norduser.proto -I protobuf/norduser
-protoc --go_opt=module=github.com/NordSecurity/nordvpn-linux --go_out=. protobuf/daemon/state.proto -I protobuf/daemon
 
 protoc --go_grpc_opt=module=github.com/NordSecurity/nordvpn-linux --go_grpc_out=. protobuf/daemon/service.proto -I protobuf/daemon
 protoc --go_grpc_opt=module=github.com/NordSecurity/nordvpn-linux --go_grpc_out=. protobuf/meshnet/service.proto -I protobuf/meshnet
--- daemon/pb/cities.pb.go
+++ daemon/pb/cities.pb.go
@@ -2,12 +2,11 @@
 // versions:
 // 	protoc-gen-go v1.35.1
 // 	protoc        v3.21.6
-// source: cities.proto
+// source: protobuf/daemon/cities.proto
 
 package pb
 
 import (
-	_ "github.com/NordSecurity/nordvpn-linux/config"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	reflect "reflect"
@@ -31,7 +30,7 @@ type CitiesRequest struct {
 
 func (x *CitiesRequest) Reset() {
 	*x = CitiesRequest{}
-	mi := &file_cities_proto_msgTypes[0]
+	mi := &file_protobuf_daemon_cities_proto_msgTypes[0]
 	ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 	ms.StoreMessageInfo(mi)
 }
@@ -43,7 +42,7 @@ func (x *CitiesRequest) String() string {
 func (*CitiesRequest) ProtoMessage() {}
 
 func (x *CitiesRequest) ProtoReflect() protoreflect.Message {
-	mi := &file_cities_proto_msgTypes[0]
+	mi := &file_protobuf_daemon_cities_proto_msgTypes[0]
 	if x != nil {
 		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
 		if ms.LoadMessageInfo() == nil {
@@ -56,7 +55,7 @@ func (x *CitiesRequest) ProtoReflect() protoreflect.Message {
 
 // Deprecated: Use CitiesRequest.ProtoReflect.Descriptor instead.
 func (*CitiesRequest) Descriptor() ([]byte, []int) {
-	return file_cities_proto_rawDescGZIP(), []int{0}
+	return file_protobuf_daemon_cities_proto_rawDescGZIP(), []int{0}
 }
 
 func (x *CitiesRequest) GetCountry() string {
@@ -66,37 +65,37 @@ func (x *CitiesRequest) GetCountry() string {
 	return ""
 }
 
-var File_cities_proto protoreflect.FileDescriptor
+var File_protobuf_daemon_cities_proto protoreflect.FileDescriptor
 
-var file_cities_proto_rawDesc = []byte{
-	0x0a, 0x0c, 0x63, 0x69, 0x74, 0x69, 0x65, 0x73, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x02,
-	0x70, 0x62, 0x1a, 0x15, 0x63, 0x6f, 0x6e, 0x66, 0x69, 0x67, 0x2f, 0x70, 0x72, 0x6f, 0x74, 0x6f,
-	0x63, 0x6f, 0x6c, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x22, 0x29, 0x0a, 0x0d, 0x43, 0x69, 0x74,
-	0x69, 0x65, 0x73, 0x52, 0x65, 0x71, 0x75, 0x65, 0x73, 0x74, 0x12, 0x18, 0x0a, 0x07, 0x63, 0x6f,
-	0x75, 0x6e, 0x74, 0x72, 0x79, 0x18, 0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x63, 0x6f, 0x75,
-	0x6e, 0x74, 0x72, 0x79, 0x42, 0x31, 0x5a, 0x2f, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63,
-	0x6f, 0x6d, 0x2f, 0x4e, 0x6f, 0x72, 0x64, 0x53, 0x65, 0x63, 0x75, 0x72, 0x69, 0x74, 0x79, 0x2f,
-	0x6e, 0x6f, 0x72, 0x64, 0x76, 0x70, 0x6e, 0x2d, 0x6c, 0x69, 0x6e, 0x75, 0x78, 0x2f, 0x64, 0x61,
-	0x65, 0x6d, 0x6f, 0x6e, 0x2f, 0x70, 0x62, 0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
+var file_protobuf_daemon_cities_proto_rawDesc = []byte{
+	0x0a, 0x1c, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x62, 0x75, 0x66, 0x2f, 0x64, 0x61, 0x65, 0x6d, 0x6f,
+	0x6e, 0x2f, 0x63, 0x69, 0x74, 0x69, 0x65, 0x73, 0x2e, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x12, 0x02,
+	0x70, 0x62, 0x22, 0x29, 0x0a, 0x0d, 0x43, 0x69, 0x74, 0x69, 0x65, 0x73, 0x52, 0x65, 0x71, 0x75,
+	0x65, 0x73, 0x74, 0x12, 0x18, 0x0a, 0x07, 0x63, 0x6f, 0x75, 0x6e, 0x74, 0x72, 0x79, 0x18, 0x01,
+	0x20, 0x01, 0x28, 0x09, 0x52, 0x07, 0x63, 0x6f, 0x75, 0x6e, 0x74, 0x72, 0x79, 0x42, 0x31, 0x5a,
+	0x2f, 0x67, 0x69, 0x74, 0x68, 0x75, 0x62, 0x2e, 0x63, 0x6f, 0x6d, 0x2f, 0x4e, 0x6f, 0x72, 0x64,
+	0x53, 0x65, 0x63, 0x75, 0x72, 0x69, 0x74, 0x79, 0x2f, 0x6e, 0x6f, 0x72, 0x64, 0x76, 0x70, 0x6e,
+	0x2d, 0x6c, 0x69, 0x6e, 0x75, 0x78, 0x2f, 0x64, 0x61, 0x65, 0x6d, 0x6f, 0x6e, 0x2f, 0x70, 0x62,
+	0x62, 0x06, 0x70, 0x72, 0x6f, 0x74, 0x6f, 0x33,
 }
 
 var (
-	file_cities_proto_rawDescOnce sync.Once
-	file_cities_proto_rawDescData = file_cities_proto_rawDesc
+	file_protobuf_daemon_cities_proto_rawDescOnce sync.Once
+	file_protobuf_daemon_cities_proto_rawDescData = file_protobuf_daemon_cities_proto_rawDesc
 )
 
-func file_cities_proto_rawDescGZIP() []byte {
-	file_cities_proto_rawDescOnce.Do(func() {
-		file_cities_proto_rawDescData = protoimpl.X.CompressGZIP(file_cities_proto_rawDescData)
+func file_protobuf_daemon_cities_proto_rawDescGZIP() []byte {
+	file_protobuf_daemon_cities_proto_rawDescOnce.Do(func() {
+		file_protobuf_daemon_cities_proto_rawDescData = protoimpl.X.CompressGZIP(file_protobuf_daemon_cities_proto_rawDescData)
 	})
-	return file_cities_proto_rawDescData
+	return file_protobuf_daemon_cities_proto_rawDescData
 }
 
-var file_cities_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
-var file_cities_proto_goTypes = []any{
+var file_protobuf_daemon_cities_proto_msgTypes = make([]protoimpl.MessageInfo, 1)
+var file_protobuf_daemon_cities_proto_goTypes = []any{
 	(*CitiesRequest)(nil), // 0: pb.CitiesRequest
 }
-var file_cities_proto_depIdxs = []int32{
+var file_protobuf_daemon_cities_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for method output_type
 	0, // [0:0] is the sub-list for method input_type
 	0, // [0:0] is the sub-list for extension type_name
@@ -104,27 +103,27 @@ var file_cities_proto_depIdxs = []int32{
 	0, // [0:0] is the sub-list for field type_name
 }
 
-func init() { file_cities_proto_init() }
-func file_cities_proto_init() {
-	if File_cities_proto != nil {
+func init() { file_protobuf_daemon_cities_proto_init() }
+func file_protobuf_daemon_cities_proto_init() {
+	if File_protobuf_daemon_cities_proto != nil {
 		return
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
 		File: protoimpl.DescBuilder{
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
-			RawDescriptor: file_cities_proto_rawDesc,
+			RawDescriptor: file_protobuf_daemon_cities_proto_rawDesc,
 			NumEnums:      0,
 			NumMessages:   1,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
-		GoTypes:           file_cities_proto_goTypes,
-		DependencyIndexes: file_cities_proto_depIdxs,
-		MessageInfos:      file_cities_proto_msgTypes,
+		GoTypes:           file_protobuf_daemon_cities_proto_goTypes,
+		DependencyIndexes: file_protobuf_daemon_cities_proto_depIdxs,
+		MessageInfos:      file_protobuf_daemon_cities_proto_msgTypes,
 	}.Build()
-	File_cities_proto = out.File
-	file_cities_proto_rawDesc = nil
-	file_cities_proto_goTypes = nil
-	file_cities_proto_depIdxs = nil
+	File_protobuf_daemon_cities_proto = out.File
+	file_protobuf_daemon_cities_proto_rawDesc = nil
+	file_protobuf_daemon_cities_proto_goTypes = nil
+	file_protobuf_daemon_cities_proto_depIdxs = nil
 }
