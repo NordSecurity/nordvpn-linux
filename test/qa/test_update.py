3ff15efd6582a319640a799e25a256a3afa78905
ggediminass <gediminas.alsauskas@nordsec.com>
2024-11-20 17:14:03 +0200

    Install latest prod app version

--- test/qa/test_update.py
+++ test/qa/test_update.py
@@ -3,3 +3,5 @@ import sh
 
 def setup_module(module):  # noqa: ARG001
     sh.sudo.apt.purge("-y", "nordvpn")
+
+    sh.sh(_in=sh.curl("-sSf", "https://downloads.nordcdn.com/apps/linux/install.sh"))
