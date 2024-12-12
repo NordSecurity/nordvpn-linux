be516ac0cf35577d553d09a037daadd61bab0a91
Bartosz Oleaczek <bartosz.oleaczek@nordsec.com>
2024-12-12 11:31:30 +0100

    remove old comment

--- ci/add_private_bindings.sh
+++ ci/add_private_bindings.sh
@@ -5,10 +5,3 @@ bindings_path=$2
 
 go mod edit -require="$bindings_name"@v0.0.0
 go mod edit -replace="$bindings_name"="$bindings_path"
-
-# function revert_private_bindings {
-#     go mod edit -droprequire="$bindings_name"
-#     go mod edit -dropreplace="$bindings_name"
-# }
-
-# trap revert_private_bindings EXIT
