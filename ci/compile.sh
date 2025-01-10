a70aae61ca2c60f465be35f5d39489ae5f904f02
Bartosz Oleaczek <122802123+bartoszWojciechO@users.noreply.github.com>
2025-01-10 13:09:05 +0100

    Remove private bindings when compile script finishes
    Signed-off-by: Grzegorz Baranski <me@gbaranski.com>
    Co-authored-by: Grzegorz Baranski <grzegorz.baranski@nordsec.com>

--- ci/compile.sh
+++ ci/compile.sh
@@ -62,6 +62,7 @@ tags="${FEATURES:-"telio drop"}"
 source "${WORKDIR}"/ci/set_bindings_version.sh libtelio
 source "${WORKDIR}"/ci/set_bindings_version.sh libdrop
 
+trap -- '${WORKDIR}/ci/remove_private_bindings.sh moose/events; ${WORKDIR}/ci/remove_private_bindings.sh moose/worker; ${WORKDIR}/ci/remove_private_bindings.sh quench' EXIT
 if [[ $tags == *"moose"* ]]; then
 	# Set correct events domain in case compiling with moose
 	if [[ "${ENVIRONMENT}" == "prod" ]]; then
