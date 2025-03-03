0b05cfc7839e194694a75659a4288bdf636d6ccd
Bartosz Oleaczek <bartosz.oleaczek@nordsec.com>
2025-03-03 11:27:12 +0100

    Report error on login attempt with no net

--- cli/cli_register.go
+++ cli/cli_register.go
@@ -1,12 +1,8 @@
 package cli
 
 import (
-	"context"
-	"io"
-
 	"github.com/NordSecurity/nordvpn-linux/daemon/pb"
 
-	"github.com/fatih/color"
 	"github.com/urfave/cli/v2"
 )
 
@@ -14,28 +10,5 @@ import (
 const RegisterUsageText = "Registers a new user account"
 
 func (c *cmd) Register(ctx *cli.Context) error {
-	cl, err := c.client.LoginOAuth2(
-		context.Background(),
-		&pb.LoginOAuth2Request{
-			Type: pb.LoginType_LoginType_SIGNUP,
-		},
-	)
-	if err != nil {
-		return formatError(err)
-	}
-
-	for {
-		resp, err := cl.Recv()
-		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			return formatError(err)
-		}
-		if url := resp.GetData(); url != "" {
-			color.Green("Continue in the browser: %s", url)
-		}
-	}
-
-	return nil
+	return c.login(pb.LoginType_LoginType_SIGNUP)
 }
