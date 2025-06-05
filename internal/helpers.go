9299ec26d82a5149b48cc84435cd456e9249b320
Bartosz Zbytniewski <bartosz.zbytniewski@nordsec.com>
2025-06-05 08:48:30 +0200

    LVPN-8135: Remove unnecessary code

--- internal/helpers.go
+++ internal/helpers.go
@@ -1,8 +1,6 @@
 package internal
 
 import (
-	"time"
-
 	"golang.org/x/exp/slices"
 )
 
@@ -19,21 +17,4 @@ func Find[T comparable](l []T, element T) *T {
 func Contains[T comparable](l []T, element T) bool {
 	e := Find(l, element)
 	return e != nil
-}
-
-// Retry calls fn up to attempts times, sleeping delay between calls.
-// It returns nil as soon as fn returns nil; otherwise it returns the last error.
-func Retry(attempts int, delay time.Duration, fn func() error) error {
-	var err error
-	for i := 0; i < attempts; i++ {
-		err = fn()
-		if err == nil {
-			return nil
-		}
-		// If this wasn’t the last attempt, wait before retrying
-		if i < attempts-1 {
-			time.Sleep(delay)
-		}
-	}
-	return err
 }
