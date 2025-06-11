4bb2812588db1c3f5908f3da55b796497fab19fd
Gražvidas Balčius <grazvidas.balcius@nordsec.com>
2025-06-11 13:41:48 +0300

    Refactore to reuse functionality
    Replacing manual in-code checks with utility functions.
    
    Signed-off-by: Gražvidas Balčius <grazvidas.balcius@nordsec.com>

---
+++ tray/tray_test.go
@@ -0,0 +1,27 @@
+package tray
+
+import "testing"
+
+func Test_selectIcon(t *testing.T) {
+	type args struct {
+		desktopEnv string
+	}
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{name: "KDE environment", args: args{desktopEnv: "kde"}, want: IconBlack},
+		{name: "MATE environment", args: args{desktopEnv: "mate"}, want: IconGray},
+		{name: "Unknown environment", args: args{desktopEnv: "gnome"}, want: IconWhite},
+		{name: "Empty environment", args: args{desktopEnv: ""}, want: IconWhite},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := selectIcon(tt.args.desktopEnv); got != tt.want {
+				t.Errorf("selectIcon() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
