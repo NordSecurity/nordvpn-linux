d7797d2cb60bf8b9f5c32c6ca4e2ac44bb5d04ec
Bartosz Oleaczek <122802123+bartoszWojciechO@users.noreply.github.com>
2025-06-12 16:14:14 +0200

    LVPN-8134: Terminal consent flow

--- events/moose/moose_test.go
+++ events/moose/moose_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/NordSecurity/nordvpn-linux/config"
 	"github.com/NordSecurity/nordvpn-linux/core"
 	"github.com/NordSecurity/nordvpn-linux/test/category"
 	"gotest.tools/v3/assert"
@@ -123,6 +124,147 @@ func TestFindOrder(t *testing.T) {
 			order, ok := findOrder(tt.payment, tt.orders)
 			assert.DeepEqual(t, tt.order, order)
 			assert.Equal(t, tt.ok, ok)
+		})
+	}
+}
+
+func TestChangeConsentState(t *testing.T) {
+	category.Set(t, category.Unit)
+
+	tests := []struct {
+		name                            string
+		currentConsentState             config.AnalyticsConsent
+		currentOptInState               bool
+		desiredConsentState             config.AnalyticsConsent
+		consentErrCode                  uint32
+		optInErrCode                    uint32
+		expectedConsentState            config.AnalyticsConsent
+		expectedEssentialAnalyticsState bool
+		expectedOptInState              bool
+		shouldFail                      bool
+	}{
+		{
+			name:                            "undefined to enabled success",
+			currentConsentState:             config.ConsentUndefined,
+			currentOptInState:               false,
+			desiredConsentState:             config.ConsentGranted,
+			expectedConsentState:            config.ConsentGranted,
+			expectedEssentialAnalyticsState: true,
+			expectedOptInState:              true,
+		},
+		{
+			name:                            "undefined to disabled success",
+			currentConsentState:             config.ConsentUndefined,
+			currentOptInState:               false,
+			desiredConsentState:             config.ConsentDenied,
+			expectedConsentState:            config.ConsentDenied,
+			expectedEssentialAnalyticsState: false,
+			expectedOptInState:              true,
+		},
+		{
+			name:                            "enabled to disabled success",
+			currentConsentState:             config.ConsentGranted,
+			currentOptInState:               true,
+			desiredConsentState:             config.ConsentDenied,
+			expectedConsentState:            config.ConsentDenied,
+			expectedEssentialAnalyticsState: false,
+			expectedOptInState:              true,
+		},
+		{
+			name:                            "disabled to enabled success",
+			currentConsentState:             config.ConsentDenied,
+			currentOptInState:               true,
+			desiredConsentState:             config.ConsentGranted,
+			expectedConsentState:            config.ConsentGranted,
+			expectedEssentialAnalyticsState: true,
+			expectedOptInState:              true,
+		},
+		{
+			name:                            "undefined to enabled failure to opt in",
+			currentConsentState:             config.ConsentUndefined,
+			currentOptInState:               false,
+			desiredConsentState:             config.ConsentGranted,
+			optInErrCode:                    1,
+			expectedConsentState:            config.ConsentUndefined,
+			expectedEssentialAnalyticsState: false,
+			expectedOptInState:              false,
+		},
+		{
+			name:                            "undefined to disabled failure to opt in",
+			currentConsentState:             config.ConsentUndefined,
+			currentOptInState:               false,
+			desiredConsentState:             config.ConsentDenied,
+			optInErrCode:                    1,
+			expectedConsentState:            config.ConsentUndefined,
+			expectedEssentialAnalyticsState: false,
+			expectedOptInState:              false,
+		},
+		{
+			name:                            "undefined to enabled failure to consent",
+			currentConsentState:             config.ConsentUndefined,
+			currentOptInState:               false,
+			desiredConsentState:             config.ConsentGranted,
+			consentErrCode:                  1,
+			expectedConsentState:            config.ConsentDenied,
+			expectedEssentialAnalyticsState: false,
+			expectedOptInState:              true,
+		},
+		{
+			name:                            "undefined to disabled failure to consent",
+			currentConsentState:             config.ConsentUndefined,
+			currentOptInState:               false,
+			desiredConsentState:             config.ConsentDenied,
+			consentErrCode:                  1,
+			expectedConsentState:            config.ConsentDenied,
+			expectedEssentialAnalyticsState: false,
+			expectedOptInState:              true,
+		},
+		{
+			name:                            "enabled to undefined fails",
+			currentConsentState:             config.ConsentGranted,
+			currentOptInState:               true,
+			desiredConsentState:             config.ConsentUndefined,
+			expectedConsentState:            config.ConsentGranted,
+			expectedEssentialAnalyticsState: true,
+			expectedOptInState:              true,
+			shouldFail:                      true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			optIn := test.currentOptInState
+			optInFunc := func(enable bool) uint32 {
+				if test.optInErrCode != 0 {
+					return test.optInErrCode
+				}
+				optIn = enable
+				return 0
+			}
+
+			consent := test.currentConsentState == config.ConsentGranted
+			consentFunc := func(enable bool) uint32 {
+				if test.consentErrCode != 0 {
+					return test.consentErrCode
+				}
+				consent = enable
+				return 0
+			}
+
+			s := &Subscriber{
+				consent:               test.currentConsentState,
+				mooseOptInFunc:        optInFunc,
+				mooseConsentLevelFunc: consentFunc,
+			}
+
+			err := s.changeConsentState(test.desiredConsentState)
+			if test.optInErrCode != 0 || test.consentErrCode != 0 || test.shouldFail {
+				assert.Assert(t, err != nil)
+			}
+
+			assert.Equal(t, test.expectedConsentState, s.consent, "Unexpected consent state saved.")
+			assert.Equal(t, test.expectedOptInState, optIn, "Unexpected opt in configuration.")
+			assert.Equal(t, test.expectedEssentialAnalyticsState, consent, "Unexpected consent configuration.")
 		})
 	}
 }
