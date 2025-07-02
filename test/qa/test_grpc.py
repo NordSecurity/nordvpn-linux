1e910b41b284274881a2a489f39c69ead11458b4
Bartosz Oleaczek <bartosz.oleaczek@nordsec.com>
2025-07-02 09:02:47 +0200

    LVPN-7891: fix grpc tests
    Added synchronization to grpc tests, added a wait for grpc server to
    become available and switched to using a single socket in case of
    multiple subscribers test.

--- test/qa/test_grpc.py
+++ test/qa/test_grpc.py
@@ -4,6 +4,7 @@ import sh
 import grpc
 from collections.abc import Sequence
 from lib.protobuf.daemon import (common_pb2, service_pb2_grpc, state_pb2, status_pb2)
+from threading import Barrier
 
 
 pytestmark = pytest.mark.usefixtures("nordvpnd_scope_function")
@@ -20,19 +21,22 @@ def test_multiple_state_subscribers():
     ]
 
     num_threads = 5
+    all_threads_subscribed = Barrier(num_threads+1) # number of child threads + parrent thread
     threads = []
     results = {}
 
-    threads = [threading.Thread(target=lambda i=i: results.update(
-        {i: collect_state_changes(len(expected_states), ['connection_status'])})) for i in range(num_threads)]
+    with grpc.insecure_channel(NORDVPND_SOCKET) as channel:
+        threads = [threading.Thread(target=lambda i=i: results.update(
+            {i: collect_state_changes(channel, len(expected_states), ['connection_status'], all_threads_subscribed)})) for i in range(num_threads)]
 
-    [thread.start() for thread in threads]
-    sh.nordvpn.connect()
-    [thread.join() for thread in threads]
+        [thread.start() for thread in threads]
+        all_threads_subscribed.wait(timeout=10)
+        sh.nordvpn.connect()
+        [thread.join() for thread in threads]
 
-    for i in range(num_threads):
-        assert all(a.connection_status.state == b for a, b in zip(
-            results[i], expected_states, strict=True))
+        for i in range(num_threads):
+            assert all(a.connection_status.state == b for a, b in zip(
+                results[i], expected_states, strict=True))
 
 
 def test_tunnel_update_notifications_before_and_after_connect():
@@ -43,10 +47,12 @@ def test_tunnel_update_notifications_before_and_after_connect():
         status_pb2.ConnectionState.DISCONNECTED,
     ]
 
+    subscribtion_barrier = Barrier(2) # parrent and child thread
     result = []
-    thread = threading.Thread(target=lambda: result.extend(collect_state_changes(
-        len(expected_states), ['connection_status'])))
+    thread = threading.Thread(target=lambda: result.extend(collect_state_changes_guard(
+        len(expected_states), ['connection_status'], subscribtion_barrier)))
     thread.start()
+    subscribtion_barrier.wait(timeout=10)
     sh.nordvpn.connect()
     sh.nordvpn.disconnect()
     thread.join()
@@ -54,9 +60,14 @@ def test_tunnel_update_notifications_before_and_after_connect():
                b in zip(result, expected_states, strict=True))
 
 
-def collect_state_changes(stop_at: int, tracked_states: Sequence[str], timeout: int = 10) -> Sequence[state_pb2.AppState]:
+def collect_state_changes_guard(stop_at: int, tracked_states: Sequence[str], subscribtion_barrier: Barrier, timeout: int = 10) -> Sequence[state_pb2.AppState]:
     with grpc.insecure_channel(NORDVPND_SOCKET) as channel:
+        return collect_state_changes(channel, stop_at, tracked_states, subscribtion_barrier, timeout)
+
+def collect_state_changes(channel: grpc.Channel, stop_at: int, tracked_states: Sequence[str], subscribtion_barrier: Barrier, timeout: int = 10) -> Sequence[state_pb2.AppState]:
+        grpc.channel_ready_future(channel).result(timeout=timeout)
         stub = service_pb2_grpc.DaemonStub(channel)
+        subscribtion_barrier.wait()
         response_stream = stub.SubscribeToStateChanges(
             common_pb2.Empty(), timeout=timeout)
         result = []
@@ -67,7 +78,6 @@ def collect_state_changes(stop_at: int, tracked_states: Sequence[str], timeout: 
                 if len(result) >= stop_at:
                     break
         return result
-
 
 def test_is_virtual_location_is_true_for_virtual_location():
     check_is_virtual_location_in_response("Algiers", True)
@@ -80,10 +90,12 @@ def check_is_virtual_location_in_response(loc: str, expected_is_virtual: bool):
         status_pb2.ConnectionState.CONNECTED
     ]
 
+    subscribtion_barrier = Barrier(2) # parrent and child thread
     result = []
-    thread = threading.Thread(target=lambda: result.extend(collect_state_changes(
-        len(expected_states), ['connection_status'])))
+    thread = threading.Thread(target=lambda: result.extend(collect_state_changes_guard(
+        len(expected_states), ['connection_status'], subscribtion_barrier)))
     thread.start()
+    subscribtion_barrier.wait(timeout=10)
     sh.nordvpn.connect(loc)
     sh.nordvpn.disconnect()
     thread.join()
@@ -101,10 +113,12 @@ def test_manual_connection_source_is_present_in_response():
         status_pb2.ConnectionState.CONNECTED
     ]
 
+    subscribtion_barrier = Barrier(2) # parrent and child thread
     result = []
-    thread = threading.Thread(target=lambda: result.extend(collect_state_changes(
-        len(expected_states), ['connection_status'])))
+    thread = threading.Thread(target=lambda: result.extend(collect_state_changes_guard(
+        len(expected_states), ['connection_status'], subscribtion_barrier)))
     thread.start()
+    subscribtion_barrier.wait(timeout=10)
     sh.nordvpn.connect()
     sh.nordvpn.disconnect()
     thread.join()
