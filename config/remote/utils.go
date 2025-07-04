f27e018e98fc1a75956c216c66d0a641de248205
Stanislaw Kubiak <stanislaw.kubiak@nordsec.com>
2025-07-04 11:31:32 +0200

    Modify rollout_group generator
    Rollout group changed to be just an integer value
    Remove parameters, values should be always in the range of [0,100]
    Make value actually accessible for consumers of the package

--- config/remote/utils.go
+++ config/remote/utils.go
@@ -3,41 +3,23 @@ package remote
 import (
 	"crypto/sha256"
 	"encoding/binary"
-	"fmt"
 
 	"github.com/google/uuid"
 )
 
-const DefaultMaxGroup = 100
+const defaultMaxGroup = 100
 
-type Group struct {
-	value int
-}
-
-// NewGroup creates a new Group instance based on a provided UUID and maximum group size.
-// It computes a group value by hashing the UUID and deriving a number between 1 and provided max (inclusive).
+// GenerateRolloutGroup creates a new RolloutGroup instance based on a provided UUID.
+// It computes a group value by hashing the UUID and deriving a number between 1 and defaultMaxGroup (inclusive).
 //
 // Parameters:
 //   - uuid: The UUID used as the basis for group assignment
-//   - max: The maximum possible group value (must be positive and not exceed DefaultMaxGroup)
 //
 // Returns:
-//   - *Group: A new Group instance with the computed value
-//   - error: An error if max is less than 1 or greater than DefaultMaxGroup
-func NewGroup(uuid uuid.UUID, max int) (*Group, error) {
-	if max < 1 {
-		return nil, fmt.Errorf("max value must be positive, got %d", max)
-	}
-
-	if max > DefaultMaxGroup {
-		return nil, fmt.Errorf("max value must not exceed %d, got %d", DefaultMaxGroup, max)
-	}
-
+//   - RolloutGroup: A new RolloutGroup instance with the computed value
+func GenerateRolloutGroup(uuid uuid.UUID) int {
 	hash := sha256.Sum256(uuid[:])
 	num := binary.BigEndian.Uint32(hash[:])
-	value := int(num)%max + 1
-
-	return &Group{
-		value: value,
-	}, nil
+	value := int(num%uint32(defaultMaxGroup)) + 1
+	return value
 }
--- config/remote/utils_test.go
+++ config/remote/utils_test.go
@@ -1,18 +1,16 @@
 package remote
 
 import (
-	"fmt"
 	"math"
 	"testing"
 
 	"github.com/NordSecurity/nordvpn-linux/test/category"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
-	"github.com/stretchr/testify/require"
 )
 
-// TestGroupDistribution verifies that the distribution of groups created by NewGroup is uniform.
-// The test generates an arbitrary number of 10kk random UUIDs and assigns them to groups using NewGroup,
+// TestGroupDistribution verifies that the distribution of groups created by GenerateRolloutGroup is uniform.
+// The test generates an arbitrary number of 10kk random UUIDs and assigns them to groups using GenerateRolloutGroup,
 // then checks that the distribution of groups across five equal-sized brackets is statistically
 // balanced. It validates:
 //  1. Each bracket (1-20, 21-40, 41-60, 61-80, 81-100) contains approximately 20% of the groups
@@ -24,7 +22,6 @@ func TestGroupDistribution(t *testing.T) {
 	const (
 		iterations  = 10_000_000
 		bracketSize = 20
-		maxGroup    = 100
 		tolerance   = 0.02 // 2% tolerance
 	)
 
@@ -34,9 +31,8 @@ func TestGroupDistribution(t *testing.T) {
 	groups := make([]int, iterations)
 	for i := 0; i < iterations; i++ {
 		id := uuid.New()
-		g, err := NewGroup(id, maxGroup)
-		require.NoError(t, err)
-		groups[i] = g.value
+		g := GenerateRolloutGroup(id)
+		groups[i] = g
 	}
 
 	// calculate specific brackets counts
@@ -82,82 +78,4 @@ func TestGroupDistribution(t *testing.T) {
 	// Assert total count matches iterations
 	assert.Equal(t, iterations, sum,
 		"Sum of bracket counts (%d) doesn't match total number of generated UUIDs (%d)", sum, iterations)
-}
-
-func TestNewGroupErrors(t *testing.T) {
-	category.Set(t, category.Unit)
-
-	tests := []struct {
-		name    string
-		max     int
-		wantErr string
-	}{
-		{
-			name:    "zero max value",
-			max:     0,
-			wantErr: "max value must be positive, got 0",
-		},
-		{
-			name:    "negative max value",
-			max:     -1,
-			wantErr: "max value must be positive, got -1",
-		},
-		{
-			name:    "exceeding max allowed value",
-			max:     DefaultMaxGroup + 1,
-			wantErr: fmt.Sprintf("max value must not exceed %d, got %d", DefaultMaxGroup, DefaultMaxGroup+1),
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			id := uuid.New()
-			group, err := NewGroup(id, tt.max)
-
-			assert.Nil(t, group)
-			assert.EqualError(t, err, tt.wantErr)
-		})
-	}
-}
-
-func TestNewGroupValue(t *testing.T) {
-	category.Set(t, category.Unit)
-
-	tests := []struct {
-		name    string
-		max     int
-		wantMin int
-		wantMax int
-	}{
-		{
-			name:    "max value 20",
-			max:     20,
-			wantMin: 1,
-			wantMax: 20,
-		},
-		{
-			name:    "max value 100",
-			max:     100,
-			wantMin: 1,
-			wantMax: 100,
-		},
-		{
-			name:    "max value 50",
-			max:     50,
-			wantMin: 1,
-			wantMax: 50,
-		},
-	}
-
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			id := uuid.New()
-			group, err := NewGroup(id, tt.max)
-
-			require.NoError(t, err)
-			require.NotNil(t, group)
-			assert.GreaterOrEqual(t, group.value, tt.wantMin)
-			assert.LessOrEqual(t, group.value, tt.wantMax)
-		})
-	}
 }
