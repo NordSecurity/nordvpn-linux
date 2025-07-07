e15039e260fc91e4cbc6de313d9988371e2a1597
Gražvidas Balčius <grazvidas.balcius@nordsec.com>
2025-07-07 15:32:34 +0300

    Extract 'Disconnect' function from RPC structure
    Signed-off-by: Gražvidas Balčius <grazvidas.balcius@nordsec.com>

--- daemon/rpc_disconnect.go
+++ daemon/rpc_disconnect.go
@@ -1,13 +1,10 @@
 package daemon
 
 import (
-	"fmt"
 	"log"
-	"time"
 
-	"github.com/NordSecurity/nordvpn-linux/config"
+	"github.com/NordSecurity/nordvpn-linux/daemon/access"
 	"github.com/NordSecurity/nordvpn-linux/daemon/pb"
-	"github.com/NordSecurity/nordvpn-linux/events"
 	"github.com/NordSecurity/nordvpn-linux/internal"
 )
 
@@ -25,40 +22,11 @@ func (r *RPC) Disconnect(_ *pb.Empty, srv pb.Daemon_DisconnectServer) error {
 	return srv.Send(&pb.Payload{Type: internal.CodeDisconnected})
 }
 
-// DoDisconnect is the non-gRPC function for Disconect to be used dirrectly.
+// DoDisconnect is the non-gRPC function for Disconect to be used directly.
 func (r *RPC) DoDisconnect() (bool, error) {
-	startTime := time.Now()
-	if !r.netw.IsVPNActive() {
-		if err := r.netw.UnsetFirewall(); err != nil {
-			log.Println(internal.WarningPrefix, "failed to force unset firewall on disconnect:", err)
-		}
-		return false, nil
-	}
-
-	var cfg config.Config
-	var err error
-	defer func() {
-		status := events.StatusSuccess
-		if err != nil {
-			status = events.StatusFailure
-		}
-		r.events.Service.Disconnect.Publish(events.DataDisconnect{
-			Protocol:             cfg.AutoConnectData.Protocol,
-			EventStatus:          status,
-			Technology:           cfg.Technology,
-			ThreatProtectionLite: cfg.AutoConnectData.ThreatProtectionLite,
-			Duration:             time.Since(startTime),
-			Error:                err,
-		})
-	}()
-	if err = r.netw.Stop(); err != nil {
-		err = fmt.Errorf("stopping networker: %w", err)
-		return true, err
-	}
-	if err = r.cm.Load(&cfg); err != nil {
-		err = fmt.Errorf("loading config: %w", err)
-		return true, err
-	}
-
-	return true, nil
+	return access.Disconnect(access.DisconnectInput{
+		Networker:     r.netw,
+		ConfigManager: r.cm,
+		Events:        r.events,
+	})
 }
