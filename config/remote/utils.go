c2bec5c860bc09052d74cdc18781f2031844cce7
Stanislaw Kubiak <stanislaw.kubiak@nordsec.com>
2025-07-07 14:50:36 +0200

    Initialise constant as uint32 to get rid of casting while using the max group variable

--- config/remote/utils.go
+++ config/remote/utils.go
@@ -9,7 +9,7 @@ import (
 
 // defaultMaxGroup represents the maximum value for a rollout group,
 // effectively making the value to be in range of 1-100 (inclusive) to reflect percentage-based groups.
-const defaultMaxGroup = 100
+const defaultMaxGroup uint32 = 100
 
 // GenerateRolloutGroup creates a new RolloutGroup instance based on a provided UUID.
 // It computes a group value by hashing the UUID and deriving a number between 1 and defaultMaxGroup (inclusive).
@@ -22,6 +22,6 @@ const defaultMaxGroup = 100
 func GenerateRolloutGroup(uuid uuid.UUID) int {
 	hash := sha256.Sum256(uuid[:])
 	num := binary.BigEndian.Uint32(hash[:])
-	value := int(num%uint32(defaultMaxGroup)) + 1
+	value := int(num%defaultMaxGroup) + 1
 	return value
 }
