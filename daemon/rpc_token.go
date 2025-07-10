7bfd22cb7240c04d0999c98db5c10fcd2ea3d92d
Gražvidas Balčius <grazvidas.balcius@nordsec.com>
2025-07-10 08:22:26 +0300

    Handle token-revoke errors
    Signed-off-by: Gražvidas Balčius <grazvidas.balcius@nordsec.com>

--- daemon/rpc_token.go
+++ daemon/rpc_token.go
@@ -13,21 +13,18 @@ import (
 // TokenInfo returns token information.
 func (r *RPC) TokenInfo(ctx context.Context, _ *pb.Empty) (*pb.TokenInfoResponse, error) {
 	if ok, err := r.ac.IsLoggedIn(); !ok {
-		if errors.Is(err, core.ErrLoginTokenExpired) {
-			return nil, err
+		if errors.Is(err, core.ErrLoginTokenRevoked) {
+			return &pb.TokenInfoResponse{Type: internal.CodeRevokedAccessToken}, nil
 		}
 		return nil, internal.ErrNotLoggedIn
 	}
 
 	var cfg config.Config
 	if err := r.cm.Load(&cfg); err != nil {
-		return &pb.TokenInfoResponse{
-			Type: internal.CodeConfigError,
-		}, nil
+		return &pb.TokenInfoResponse{Type: internal.CodeConfigError}, nil
 	}
 
 	tokenData := cfg.TokensData[cfg.AutoConnectData.ID]
-
 	tokenInfo := &pb.TokenInfoResponse{
 		Type:               internal.CodeSuccess,
 		Token:              tokenData.Token,
