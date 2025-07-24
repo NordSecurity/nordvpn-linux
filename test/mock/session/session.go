42cdc169d7f94bc68a87f895d57887bb292e9866
Gražvidas Balčius <grazvidas.balcius@nordsec.com>
2025-07-24 16:11:07 +0300

    Updating tests
    Signed-off-by: Gražvidas Balčius <grazvidas.balcius@nordsec.com>

--- test/mock/session/session.go
+++ test/mock/session/session.go
@@ -48,3 +48,28 @@ func (m *MockAccessTokenSessionStore) GetToken() string {
 	m.GetTokenCallCount++
 	return m.GetTokenFunc()
 }
+
+type MockAccessTokenSessionStore struct {
+	RenewFunc      func() error
+	InvalidateFunc func(reason error) error
+	GetTokenFunc   func() string
+
+	RenewCallCount      int
+	InvalidateCallCount int
+	GetTokenCallCount   int
+}
+
+func (m *MockAccessTokenSessionStore) Renew() error {
+	m.RenewCallCount++
+	return m.RenewFunc()
+}
+
+func (m *MockAccessTokenSessionStore) Invalidate(reason error) error {
+	m.InvalidateCallCount++
+	return m.InvalidateFunc(reason)
+}
+
+func (m *MockAccessTokenSessionStore) GetToken() string {
+	m.GetTokenCallCount++
+	return m.GetTokenFunc()
+}
