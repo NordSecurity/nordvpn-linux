0b3fdad137785820584a927f3dc99862a1c18d99
Daniel Fetti <39974476+dfetti@users.noreply.github.com>
2025-07-29 22:38:39 +0300

    Use the meshnet feature toggle from the remote config (#980)
    Signed-off-by: Daniel Fetti <daniel.fetti@nordsec.com>

--- daemon/rpc_feature_toggles.go
+++ daemon/rpc_feature_toggles.go
@@ -3,11 +3,12 @@ package daemon
 import (
 	"context"
 
+	"github.com/NordSecurity/nordvpn-linux/config/remote"
 	"github.com/NordSecurity/nordvpn-linux/daemon/pb"
 )
 
 // Returns the list of feature toggles fetched from the remote config
-// TODO (dfe): Use the actual info from the remote config
 func (r *RPC) GetFeatureToggles(ctx context.Context, in *pb.Empty) (*pb.FeatureToggles, error) {
-	return &pb.FeatureToggles{MeshnetEnabled: true}, nil
+	meshnetEnabled := r.remoteConfigGetter.IsFeatureEnabled(remote.FeatureMeshnet.String())
+	return &pb.FeatureToggles{MeshnetEnabled: meshnetEnabled}, nil
 }
