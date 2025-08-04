d4076c67cbb0e0d9d57319b45a7f1d3f5ca0b199
Gražvidas Balčius <grazvidas.balcius@nordsec.com>
2025-08-04 13:32:43 +0300

    Update invalidation and tests
    Signed-off-by: Gražvidas Balčius <grazvidas.balcius@nordsec.com>

--- cmd/daemon/main.go
+++ cmd/daemon/main.go
@@ -319,7 +319,7 @@ func main() {
 		machineIdGenerator.GetUsedInformationMask(),
 	)
 
-	invalidSessionErrHandlingReg := internal.NewErrorHandlingRegistry[int64]()
+	invalidSessionErrHandlingReg := internal.NewErrorHandlingRegistry[error]()
 
 	// encapsulating initialization logic
 	clientAPI, accessTokenSessionStore := func() (core.ClientAPI, session.SessionStore) {
@@ -544,7 +544,7 @@ func main() {
 	// on session invalidation (unauthorized access, missing server resources, invalid request)
 	// perform user log-out action
 	invalidSessionErrHandlingReg.Add(
-		func(uid int64) {
+		func(reason error) {
 			discArgs := access.DisconnectInput{
 				Networker:                  netw,
 				ConfigManager:              fsystem,
