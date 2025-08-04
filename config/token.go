decd826b01b605c2b8893ed1da5b2a1802ddfad2
Gražvidas Balčius <grazvidas.balcius@nordsec.com>
2025-08-04 16:33:39 +0300

    Reorder config params
    Signed-off-by: Gražvidas Balčius <grazvidas.balcius@nordsec.com>

--- config/token.go
+++ config/token.go
@@ -9,11 +9,11 @@ type TokenData struct {
 	IsOAuth                bool       `json:"is_oauth,omitempty"`
 	TrustedPassToken       string     `json:"trusted_pass_token,omitempty"`
 	TrustedPassOwnerID     string     `json:"trusted_pass_owner_id,omitempty"`
+	TrustedPassTokenExpiry string     `json:"trusted_pass_token_expiry,omitempty"`
 	ServiceExpiry          string     `json:"service_expiry,omitempty"`
 	NordLynxPrivateKey     string     `json:"nordlynx_private_key"`
 	OpenVPNUsername        string     `json:"openvpn_username"`
 	OpenVPNPassword        string     `json:"openvpn_password"`
 	NCData                 NCData     `json:"nc_data,omitempty"`
 	IdempotencyKey         *uuid.UUID `json:"idempotency_key,omitempty"`
-	TrustedPassTokenExpiry string     `json:"trusted_pass_token_expiry,omitempty"`
 }
