42a391fc80a39dfd0cb508f049affe20656d3331
Gražvidas Balčius <grazvidas.balcius@nordsec.com>
2025-08-04 17:11:17 +0300

    Restore missing code
    Signed-off-by: Gražvidas Balčius <grazvidas.balcius@nordsec.com>

--- cmd/daemon/init.go
+++ cmd/daemon/init.go
@@ -64,3 +64,51 @@ func buildTrustedPassSessionStoreAPIRenewalCall(clientAPI core.ClientAPI) sessio
 		}, nil
 	}
 }
+
+func buildVPNCredentialsSessionStore(
+	confman config.Manager,
+	errRegistry *internal.ErrorHandlingRegistry[error],
+	clientAPI core.ClientAPI,
+) session.SessionStore {
+	return session.NewVPNCredentialsSessionStore(
+		confman,
+		errRegistry,
+		session.NewCompositeValidator(
+			session.NewExpiryValidator(),
+			session.NewOpenVPNCredentialsValidator(),
+			session.NewPrivateKeyValidator(),
+		),
+		buildVPNCredentialsSessionStoreAPIRenewalCall(confman, clientAPI),
+	)
+}
+
+func buildVPNCredentialsSessionStoreAPIRenewalCall(
+	confman config.Manager,
+	clientAPI core.ClientAPI,
+) session.VPNCredentialsRenewalAPICall {
+	return func() (*session.VPNCredentialsResponse, error) {
+		var cfg config.Config
+		if err := confman.Load(&cfg); err != nil {
+			return nil, err
+		}
+
+		data, ok := cfg.TokensData[cfg.AutoConnectData.ID]
+		if !ok {
+			return nil, errors.New("there is not data")
+		}
+
+		// actual API call passed into Session Store object
+		return func(token string) (*session.VPNCredentialsResponse, error) {
+			resp, err := clientAPI.ServiceCredentials(token)
+			if err != nil {
+				return nil, fmt.Errorf("getting vpn credentials data: %w", err)
+			}
+
+			return &session.VPNCredentialsResponse{
+				Username:           resp.Username,
+				Password:           resp.Password,
+				NordLynxPrivateKey: resp.NordlynxPrivateKey,
+			}, nil
+		}(data.Token)
+	}
+}
