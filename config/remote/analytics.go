809b4a499ec201164d3a82cf4fa1ae78365d5ab3
Stanislaw Kubiak <stanislaw.kubiak@nordsec.com>
2025-08-06 09:55:25 +0200

    Make RC analytics build tag agnostic
    There's no need to check against moose build tag, as it's going to be
    handled when events are reported anyway.

--- config/remote/analytics.go
+++ config/remote/analytics.go
@@ -1,11 +1,19 @@
-//go:build moose
-
 package remote
 
 import (
 	"github.com/NordSecurity/nordvpn-linux/events"
 )
 
+// Analytics defines an interface for reporting various analytics events related to downloads,
+// local usage, JSON parsing, and partial rollouts. Implementations of this interface are
+// responsible for handling the notification logic for each event type.
+type Analytics interface {
+	NotifyDownload(string, string)
+	NotifyDownloadFailure(string, string, DownloadError)
+	NotifyLocalUse(string, string, error)
+	NotifyJsonParse(string, string, error)
+	NotifyPartialRollout(string, string, int, bool)
+}
 type MooseAnalytics struct {
 	publisher events.PublishSubcriber[events.DebuggerEvent]
 	ctx       Event
