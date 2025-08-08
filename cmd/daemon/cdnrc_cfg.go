791d1525cba1b054e7d18791d98d2faa9087d227
Stanislaw Kubiak <stanislaw.kubiak@nordsec.com>
2025-08-08 12:58:12 +0200

    Address review comments part 2

--- cmd/daemon/cdnrc_cfg.go
+++ cmd/daemon/cdnrc_cfg.go
@@ -10,6 +10,6 @@ import (
 )
 
 func getRemoteConfigGetter(buildTarget config.BuildTarget, rpath string,
-	cdn core.RemoteStorage, rcAna remote.Analytics, appRollout int) *remote.CdnRemoteConfig {
-	return remote.NewCdnRemoteConfig(buildTarget, rpath, internal.ConfigFilesPathCommon, cdn, rcAna, appRollout)
+	cdn core.RemoteStorage, analytics remote.Analytics, appRollout int) *remote.CdnRemoteConfig {
+	return remote.NewCdnRemoteConfig(buildTarget, rpath, internal.ConfigFilesPathCommon, cdn, analytics, appRollout)
 }
--- config/remote/config.go
+++ config/remote/config.go
@@ -32,6 +32,17 @@ func (e *DownloadError) Error() string {
 	return fmt.Sprintf("%s: %v", e.Kind, e.Cause)
 }
 
+func (e *DownloadError) Unwrap() error {
+	return e.Cause
+}
+
+func NewDownloadError(kind DownloadErrorKind, err error) *DownloadError {
+	return &DownloadError{
+		Kind:  kind,
+		Cause: err,
+	}
+}
+
 func hash(content []byte) string {
 	hash := sha256.Sum256(content)
 	return hex.EncodeToString(hash[:])
@@ -83,21 +94,21 @@ func (f *Feature) download(cdn fileReader, fw fileWriter, jv validator, cdnBaseP
 	}()
 
 	if f.name == "" {
-		return false, &DownloadError{Kind: DownloadErrorOther, Cause: fmt.Errorf("feature name is not set")}
+		return false, NewDownloadError(DownloadErrorOther, fmt.Errorf("feature name is not set"))
 	}
 
 	if err = internal.EnsureDirFull(targetPath); err != nil {
-		return false, &DownloadError{Kind: DownloadErrorLocalFS, Cause: fmt.Errorf("setting-up target dir: %w", err)}
+		return false, NewDownloadError(DownloadErrorLocalFS, fmt.Errorf("setting-up target dir: %w", err))
 	}
 
 	mainJsonHashStr, err := cdn.readFile(f.HashFilePath(cdnBasePath))
 	if err != nil {
-		return false, &DownloadError{Kind: DownloadErrorRemoteHashNotFound, Cause: fmt.Errorf("downloading main hash file: %w", err)}
+		return false, NewDownloadError(DownloadErrorRemoteHashNotFound, fmt.Errorf("downloading main hash file: %w", err))
 	}
 
 	var mainJsonHash jsonHash
 	if err = json.Unmarshal(mainJsonHashStr, &mainJsonHash); err != nil {
-		return false, &DownloadError{Kind: DownloadErrorHashParsing, Cause: fmt.Errorf("parsing main hash file: %w", err)}
+		return false, NewDownloadError(DownloadErrorHashParsing, fmt.Errorf("parsing main hash file: %w", err))
 	}
 
 	if f.hash == mainJsonHash.Hash {
@@ -107,41 +118,41 @@ func (f *Feature) download(cdn fileReader, fw fileWriter, jv validator, cdnBaseP
 	// main hash covers the include files as well
 	mainJsonStr, err := cdn.readFile(f.FilePath(cdnBasePath))
 	if err != nil {
-		return false, &DownloadError{Kind: DownloadErrorRemoteFileNotFound, Cause: fmt.Errorf("downloading main file: %w", err)}
+		return false, NewDownloadError(DownloadErrorRemoteFileNotFound, fmt.Errorf("downloading main file: %w", err))
 	}
 
 	// validate json against predefined schema
 	if err = jv.validate(mainJsonStr); err != nil {
-		return false, &DownloadError{Kind: DownloadErrorParsing, Cause: fmt.Errorf("validating main: %w", err)}
+		return false, NewDownloadError(DownloadErrorParsing, fmt.Errorf("validating main: %w", err))
 	}
 
 	incFiles, err := walkIncludeFiles(mainJsonStr, cdnBasePath, targetPath, cdn, fw)
 	if err != nil {
-		return false, &DownloadError{Kind: DownloadErrorIncludeFile, Cause: fmt.Errorf("downloading include files: %w", err)}
+		return false, NewDownloadError(DownloadErrorIncludeFile, fmt.Errorf("downloading include files: %w", err))
 	}
 
 	// verify content integrity
 	// if main json has include files - hash should cover whole content
 	if !isHashEqual(mainJsonHash.Hash, mainJsonStr, incFiles) {
-		return false, &DownloadError{Kind: DownloadErrorHashIntegrity, Cause: fmt.Errorf("main file integrity problem")}
+		return false, NewDownloadError(DownloadErrorHashIntegrity, fmt.Errorf("main file integrity problem"))
 	}
 
 	// write main json to file
 	localFileName := f.FilePath(targetPath) + tmpExt
 	if err = fw.writeFile(localFileName, mainJsonStr, internal.PermUserRW); err != nil {
-		return false, &DownloadError{Kind: DownloadErrorWriteJson, Cause: fmt.Errorf("writing main file: %w", err)}
+		return false, NewDownloadError(DownloadErrorWriteJson, fmt.Errorf("writing main file: %w", err))
 	}
 	// write main hash to file
 	localFileName = f.HashFilePath(targetPath) + tmpExt
 	if err = fw.writeFile(localFileName, mainJsonHashStr, internal.PermUserRW); err != nil {
-		return false, &DownloadError{Kind: DownloadErrorWriteHash, Cause: fmt.Errorf("writing main hash file: %w", err)}
+		return false, NewDownloadError(DownloadErrorWriteHash, fmt.Errorf("writing main hash file: %w", err))
 	}
 
 	// while processing, save files with special extension '*.bu'
 	// if download or handling would fail in the middle - previous files are left intact,
 	// also need to cleanup tmp files (see above)
 	if err = internal.RenameTmpFiles(targetPath, tmpExt); err != nil {
-		return false, &DownloadError{Kind: DownloadErrorFileRename, Cause: fmt.Errorf("writing/renaming files: %w", err)}
+		return false, NewDownloadError(DownloadErrorFileRename, fmt.Errorf("writing/renaming files: %w", err))
 	}
 
 	return true, nil
