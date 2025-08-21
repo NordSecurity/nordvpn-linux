02fc56d768dda87889f3efeedebfa0986f8f36cd
Stanisław Kubiak <stanislaw.kubiak@nordsec.com>
2025-08-21 13:20:35 +0200

    LVPN-8576: Integrate remote config analytics (#998)
    Signed-off-by: keliramu <ramunas.keliuotis@nordsec.com>
    Signed-off-by: Stanislaw Kubiak <stanislaw.kubiak@nordsec.com>
    Co-authored-by: keliramu <ramunas.keliuotis@nordsec.com>

--- config/remote/data_test.go
+++ config/remote/data_test.go
@@ -1,6 +1,7 @@
 package remote
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/NordSecurity/nordvpn-linux/test/category"
@@ -39,5 +40,22 @@ func TestFeaturePaths(t *testing.T) {
 			assert.Equal(t, test.expectedPath, feature.FilePath(test.basePath))
 			assert.Equal(t, test.expectedHashPath, feature.HashFilePath(test.basePath))
 		})
+	}
+}
+
+func TestFeatureOrderFixed(t *testing.T) {
+	category.Set(t, category.Unit)
+
+	const featureCount = 5
+	const featureNamePattern = "feature-%d"
+
+	featureMap := NewFeatureMap()
+	for i := 0; i < featureCount; i++ {
+		featureMap.add(fmt.Sprintf(featureNamePattern, i))
+	}
+	assert.Equal(t, featureCount, len(featureMap.keys()))
+
+	for i, f := range featureMap.keys() {
+		assert.Equal(t, f, fmt.Sprintf(featureNamePattern, i))
 	}
 }
