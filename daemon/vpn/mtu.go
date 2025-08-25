ba9f68fb4e9ea199c9a407902893b292844a33eb
Bartosz Oleaczek <bartosz.oleaczek@nordsec.com>
2025-08-25 07:57:17 +0200

    Set MTU using netlink instead of exec

--- daemon/vpn/mtu.go
+++ daemon/vpn/mtu.go
@@ -1,42 +1,17 @@
 package vpn
 
 import (
-	"errors"
+	"fmt"
 	"log"
 	"net"
-	"os/exec"
-	"strings"
 	"syscall"
 
 	"github.com/NordSecurity/nordvpn-linux/internal"
+	"github.com/vishvananda/netlink"
 	"golang.org/x/sys/unix"
 )
 
-var (
-	errNoDefaultIpRoute          = errors.New("default gateway not found")
-	errUnrecognizedIpRouteOutput = errors.New("unrecognized output of 'ip route show default'")
-)
-
 const defaultMTU = 1500
-
-// getDefaultIpRouteInterface takes output of the `ip route show default` command and returns the
-// interface/device name. If there are multiple default routes in the output, first one will be returned
-func getDefaultIpRouteInterface(ipRouteOutput string) (string, error) {
-	outputRows := strings.Split(ipRouteOutput, "\n")
-
-	if len(outputRows) < 1 || outputRows[0] == "" {
-		return "", errNoDefaultIpRoute
-	}
-
-	outputColumns := strings.Split(strings.Trim(outputRows[0], "\n"), " ")
-
-	if len(outputColumns) < 5 {
-		log.Printf("unexpected output of 'ip route show default': %s, dev value not found", outputRows[0])
-		return "", errUnrecognizedIpRouteOutput
-	}
-
-	return outputColumns[4], nil
-}
 
 // SetMTU for an interface.
 func SetMTU(iface net.Interface, headerSize int) error {
@@ -57,44 +32,32 @@ func SetMTU(iface net.Interface, headerSize int) error {
 	return unix.IoctlIfreq(fd, unix.SIOCSIFMTU, req)
 }
 
-func retrieveAndCalculateMTU(headerSize int) int {
-	c1 := exec.Command("ip", "route", "show", "default")
-	out, err := c1.Output()
-
+func getDefaultGatewayMTU() (int, error) {
+	routes, err := netlink.RouteList(nil, netlink.FAMILY_V4)
 	if err != nil {
-		log.Println(internal.ErrorPrefix, "ip route show default failed: ", err)
-		out = nil
+		return 0, fmt.Errorf("listing routes: %w", err)
 	}
 
-	return calculateMTU(string(out), headerSize)
+	for _, route := range routes {
+		if route.Dst == nil {
+			link, err := netlink.LinkByIndex(route.LinkIndex)
+			if err != nil {
+				return 0, fmt.Errorf("getting link attributes: %w", err)
+			}
+			return link.Attrs().MTU, nil
+		}
+	}
+
+	return 0, fmt.Errorf("failed to find default route")
 }
 
-func calculateMTU(ipRouteOutput string, headerSize int) int {
-	defaultGatewayMTU := func() (int, error) {
-		defaultGatewayName, err := getDefaultIpRouteInterface(ipRouteOutput)
-
-		if err != nil {
-			return 0, err
-		}
-
-		defaultGateway, err := net.InterfaceByName(defaultGatewayName)
-		if err != nil {
-			return 0, err
-		}
-
-		// wireguard-quick does this
-		mtu := defaultGateway.MTU - headerSize
-		return mtu, nil
+func retrieveAndCalculateMTU(headerSize int) int {
+	defaultRouteMTU, err := getDefaultGatewayMTU()
+	if err != nil {
+		log.Println(internal.ErrorPrefix, "failed to retrieve default interface, will use default value of: %w",
+			defaultMTU, err)
+		return defaultMTU
 	}
 
-	if ipRouteOutput != "" {
-		mtu, err := defaultGatewayMTU()
-		if err == nil {
-			return mtu
-		}
-
-		log.Println(internal.WarningPrefix, "using default MTU, failed to get default gateway MTU:", err)
-	}
-
-	return defaultMTU - headerSize
+	return defaultRouteMTU - headerSize
 }
