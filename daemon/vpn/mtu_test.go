ce59c5f88c25ff66be5bcca9d85f98158264c140
Bartosz Oleaczek <bartosz.oleaczek@nordsec.com>
2025-08-25 07:57:17 +0200

    Set MTU using netlink instead of exec

--- daemon/vpn/mtu_test.go
+++ daemon/vpn/mtu_test.go
@@ -3,114 +3,58 @@ package vpn
 import (
 	"net"
 	"os/exec"
-	"strings"
+	"strconv"
 	"testing"
 
+	"github.com/NordSecurity/nordvpn-linux/daemon/device"
 	"github.com/NordSecurity/nordvpn-linux/test/category"
-	"github.com/stretchr/testify/assert"
+	"gotest.tools/v3/assert"
 )
 
 const testHeaderSize = 30
+const testInterfaceName = "testif"
 
-func TestCalculateMTU(t *testing.T) {
-	category.Set(t, category.Unit)
+func TestSetMTU(t *testing.T) {
+	category.Set(t, category.Link)
 
 	tests := []struct {
-		name          string
-		ipRouteOutput string
-		expectedMTU   int
+		name        string
+		currentMTU  int
+		expectedMTU int
 	}{
+		// {
+		// 	name:        "set MTU underlying MTU is 1500",
+		// 	currentMTU:  1500,
+		// 	expectedMTU: 1500 - testHeaderSize,
+		// },
 		{
-			name:        "no default route exist",
-			expectedMTU: defaultMTU - testHeaderSize,
-		},
-		{
-			name:          "incorrect ip route output",
-			ipRouteOutput: "default via interface_name proto dhcp metric 600",
-			expectedMTU:   defaultMTU - testHeaderSize,
+			name:        "set MTU underlying MTU is 1000",
+			currentMTU:  1450,
+			expectedMTU: 1450 - testHeaderSize,
 		},
 	}
 
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			mtu := calculateMTU(test.ipRouteOutput, testHeaderSize)
-			assert.Equal(t, mtu, test.expectedMTU)
+			defaultGateway, _ := device.DefaultGateway()
+			exec.Command("ip", "link", "set", "dev", defaultGateway.Name, "mtu", strconv.Itoa(test.currentMTU)).Run()
+			defer exec.Command("ip",
+				"link",
+				"set",
+				"dev",
+				defaultGateway.Name,
+				"mtu",
+				strconv.Itoa(defaultGateway.MTU)).Run()
+
+			exec.Command("ip", "link", "add", testInterfaceName, "type", "dummy").Run()
+			exec.Command("sudo", "ip", "addr", "add", "192.168.100.1/24", "dev", "mydummy0").Run()
+			exec.Command("sudo", "ip", "link", "set", "mydummy0", "up").Run()
+
+			iface, _ := net.InterfaceByName(testInterfaceName)
+			SetMTU(*iface, testHeaderSize)
+
+			iface, _ = net.InterfaceByName(testInterfaceName)
+			assert.Equal(t, test.expectedMTU, iface.MTU)
 		})
 	}
-}
-
-func TestRetrieveAndCalculateMTU(t *testing.T) {
-	category.Set(t, category.Link)
-
-	out, err := exec.Command("ip", "route", "show", "default").Output()
-	assert.NoError(t, err)
-
-	defaultGatewayName, err := getDefaultIpRouteInterface(string(out))
-	assert.NoError(t, err)
-
-	defaultGateway, err := net.InterfaceByName(defaultGatewayName)
-	assert.NoError(t, err)
-
-	mtu := retrieveAndCalculateMTU(testHeaderSize)
-	assert.Equal(t, defaultGateway.MTU-testHeaderSize, mtu)
-}
-
-func TestGetDefaultIpRouteInterface(t *testing.T) {
-	category.Set(t, category.Unit)
-
-	tests := []struct {
-		testName              string
-		ipRoutes              string
-		expectedInterfaceName string
-		expectedError         error
-	}{
-		{
-			testName:              "one route",
-			ipRoutes:              "default via 192.168.0.4 dev wlp0s2f4 proto dhcp metric 600\n",
-			expectedInterfaceName: "wlp0s2f4",
-			expectedError:         nil,
-		},
-		{
-			testName: "two routes",
-			ipRoutes: `default via 192.168.0.4 dev wlp0s2f4 proto dhcp metric 600\n
-					   default via 192.168.0.5 dev enp0s2f6 proto dhcp metric 600\n`,
-			expectedInterfaceName: "wlp0s2f4",
-			expectedError:         nil,
-		},
-		{
-			testName:              "no routes",
-			ipRoutes:              "",
-			expectedInterfaceName: "",
-			expectedError:         errNoDefaultIpRoute,
-		},
-		{
-			testName:              "unrecognized output",
-			ipRoutes:              "default via bad output",
-			expectedInterfaceName: "",
-			expectedError:         errUnrecognizedIpRouteOutput,
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.testName, func(t *testing.T) {
-			ifName, err := getDefaultIpRouteInterface(test.ipRoutes)
-
-			assert.Equal(t, ifName, test.expectedInterfaceName)
-			assert.Equal(t, err, test.expectedError)
-		})
-	}
-}
-
-func TestGetDefaultIpRouteInterfaceFromCommandOutput(t *testing.T) {
-	category.Set(t, category.Route)
-
-	out, _ := exec.Command("ip", "route", "show", "default").Output()
-	outString := string(out)
-
-	// assume one default route
-	expectedIfName := strings.Split(outString, " ")[4]
-
-	ifName, _ := getDefaultIpRouteInterface(string(out))
-
-	assert.Equal(t, expectedIfName, ifName)
 }
