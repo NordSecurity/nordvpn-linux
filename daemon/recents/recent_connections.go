cd92e6b66c04a47b5895296c9b2b0af7365a275f
Gražvidas Balčius <grazvidas.balcius@nordsec.com>
2025-08-26 12:22:35 +0300

    Changes responding to comments
    Signed-off-by: Gražvidas Balčius <grazvidas.balcius@nordsec.com>

--- daemon/recents/recent_connections.go
+++ daemon/recents/recent_connections.go
@@ -110,7 +110,6 @@ func (r *RecentConnectionsStore) save(values []Model) error {
 	if err := r.fsHandle.WriteFile(r.path, data, internal.PermUserRW); err != nil {
 		return fmt.Errorf("writing vpn connections store: %w", err)
 	}
-
 	return nil
 }
 
--- tray/tray.go
+++ tray/tray.go
@@ -103,10 +103,12 @@ func sortedConnections(sgs []*pb.ServerGroup) []string {
 			set[c] = struct{}{}
 		}
 	}
+
 	list := make([]string, 0, len(set))
 	for k := range set {
 		list = append(list, k)
 	}
+
 	sort.Strings(list)
 	return list
 }
