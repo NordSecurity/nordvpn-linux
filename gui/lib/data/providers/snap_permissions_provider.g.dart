899bad547a8c970438c16811926daf80db4c3560
Daniel Fetti <39974476+dfetti@users.noreply.github.com>
2025-09-29 16:23:15 +0300

    LVPN-9058: Add snap permissions screen (#1088)
    Signed-off-by: Daniel Fetti <daniel.fetti@nordsec.com>

--- gui/lib/data/providers/snap_permissions_provider.g.dart
+++ gui/lib/data/providers/snap_permissions_provider.g.dart
@@ -1,26 +1,26 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
 
-part of 'consent_status_provider.dart';
+part of 'snap_permissions_provider.dart';
 
 // **************************************************************************
 // RiverpodGenerator
 // **************************************************************************
 
-String _$consentStatusHash() => r'38fbe4b40cf9b9b876b3bf0b6f7aebe82fb36e5d';
+String _$snapPermissionsHash() => r'c6d2c22713b7f593f7ef3efd39916d468febb370';
 
-/// See also [ConsentStatus].
-@ProviderFor(ConsentStatus)
-final consentStatusProvider =
-    AutoDisposeAsyncNotifierProvider<ConsentStatus, ConsentLevel>.internal(
-      ConsentStatus.new,
-      name: r'consentStatusProvider',
+/// See also [SnapPermissions].
+@ProviderFor(SnapPermissions)
+final snapPermissionsProvider =
+    AutoDisposeAsyncNotifierProvider<SnapPermissions, List<String>>.internal(
+      SnapPermissions.new,
+      name: r'snapPermissionsProvider',
       debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
           ? null
-          : _$consentStatusHash,
+          : _$snapPermissionsHash,
       dependencies: null,
       allTransitiveDependencies: null,
     );
 
-typedef _$ConsentStatus = AutoDisposeAsyncNotifier<ConsentLevel>;
+typedef _$SnapPermissions = AutoDisposeAsyncNotifier<List<String>>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member, deprecated_member_use_from_same_package
