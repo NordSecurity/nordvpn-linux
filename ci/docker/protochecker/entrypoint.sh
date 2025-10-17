96d73c991025b0717066950dd3d64e26de1e3318
Gražvidas Balčius <grazvidas.balcius@nordsec.com>
2025-10-17 17:50:12 +0300

    Fix shellcheck issue
    Signed-off-by: Gražvidas Balčius <grazvidas.balcius@nordsec.com>

--- ci/docker/protochecker/entrypoint.sh
+++ ci/docker/protochecker/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 # take ownership
-chown "${UID:-1000}:${GID:-1000}" /repo
+chown "$(id -u):$(id -g)" /repo
 
 # run the clone as the specified user.
-su-exec "${UID:-1000}:${GID:-1000}" git clone /src /repo
+su-exec "$(id -u):$(id -g)" git clone /src /repo
