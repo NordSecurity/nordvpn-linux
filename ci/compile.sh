7be38dd2432a59b1ad669af3aeda358e13547271
Edvinas Gaubas <edvinas.gaubas@nordsec.com>
2025-10-20 11:15:51 +0300

    Update moose to v18

--- ci/compile.sh
+++ ci/compile.sh
@@ -74,7 +74,7 @@ if [[ $tags == *"moose"* ]]; then
 		-X 'main.EventsDomain=${events_domain:-""}' \
 		-X 'main.EventsSubdomain=${EVENTS_SUBDOMAIN:-""}'"
 
-	source "${WORKDIR}"/ci/add_private_bindings.sh moose/events ./third-party/moose-events/moosenordvpnappgo/v17
+	source "${WORKDIR}"/ci/add_private_bindings.sh moose/events ./third-party/moose-events/moosenordvpnappgo/v18
 	source "${WORKDIR}"/ci/add_private_bindings.sh moose/worker ./third-party/moose-worker/mooseworkergo/v17
 fi
 
