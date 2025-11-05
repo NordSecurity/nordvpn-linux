04e883e1ef63cd5fb28ebceacd72bca0208386c0
Gražvidas Balčius <grazvidas.balcius@nordsec.com>
2025-11-05 09:39:51 +0200

    Add generated code
    Signed-off-by: Gražvidas Balčius <grazvidas.balcius@nordsec.com>

--- gui/lib/i18n/strings_en.g.dart
+++ gui/lib/i18n/strings_en.g.dart
@@ -553,15 +553,6 @@ class TranslationsCitiesEn {
 
 	/// en: 'Ashburn'
 	String get ashburn => 'Ashburn';
-
-	/// en: 'Port Louis'
-	String get port_louis => 'Port Louis';
-
-	/// en: 'Dushanbe'
-	String get dushanbe => 'Dushanbe';
-
-	/// en: 'Lewiston'
-	String get lewiston => 'Lewiston';
 }
 
 // Path: countries
@@ -1460,6 +1451,18 @@ class TranslationsUiEn {
 	/// en: 'P2P'
 	String get p2p => 'P2P';
 
+	/// en: 'Europe'
+	String get europe => 'Europe';
+
+	/// en: 'The Americas'
+	String get theAmericas => 'The Americas';
+
+	/// en: 'Asia Pacific'
+	String get asiaPacific => 'Asia Pacific';
+
+	/// en: 'Africa, the Middle East, and India'
+	String get africaTheMiddleEastAndIndia => 'Africa, the Middle East, and India';
+
 	/// en: 'Obfuscated'
 	String get obfuscated => 'Obfuscated';
 
@@ -2113,9 +2116,6 @@ extension on Translations {
 			case 'cities.mogadishu': return 'Mogadishu';
 			case 'cities.nouakchott': return 'Nouakchott';
 			case 'cities.ashburn': return 'Ashburn';
-			case 'cities.port_louis': return 'Port Louis';
-			case 'cities.dushanbe': return 'Dushanbe';
-			case 'cities.lewiston': return 'Lewiston';
 			case 'countries.AL': return 'Albania';
 			case 'countries.DZ': return 'Algeria';
 			case 'countries.AD': return 'Andorra';
@@ -2408,6 +2408,10 @@ extension on Translations {
 			case 'ui.doubleVpn': return 'Double VPN';
 			case 'ui.onionOverVpn': return 'Onion over VPN';
 			case 'ui.p2p': return 'P2P';
+			case 'ui.europe': return 'Europe';
+			case 'ui.theAmericas': return 'The Americas';
+			case 'ui.asiaPacific': return 'Asia Pacific';
+			case 'ui.africaTheMiddleEastAndIndia': return 'Africa, the Middle East, and India';
 			case 'ui.obfuscated': return 'Obfuscated';
 			case 'ui.selectServerForDip': return 'Pick a location for your IP';
 			case 'ui.selectLocation': return 'Select location';
