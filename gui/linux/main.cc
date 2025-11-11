d8759cef95281cab3e7a46add2a033ecb6529d8c
Marius Sincovici <marius.sincovici@nordsec.com>
2025-11-11 12:33:54 +0100

    Link with X11

--- gui/linux/main.cc
+++ gui/linux/main.cc
@@ -1,7 +1,10 @@
 #include "nordvpn.h"
+#include <X11/Xlib.h>
 
 int main(int argc, char** argv) {
   gtk_init(&argc, &argv);
+
+  XInitThreads();
   
   g_autoptr(NordVPNApplication) app = nordvpn_application_new();
   return g_application_run(G_APPLICATION(app), argc, argv);
