14e1e6feb012ad4a48f78be49d580e054b33ccab
Gražvidas Balčius <grazvidas.balcius@nordsec.com>
2025-11-20 11:25:58 +0200

    Remove unused object
    Signed-off-by: Gražvidas Balčius <grazvidas.balcius@nordsec.com>

--- gui/lib/widgets/link_types.dart
+++ gui/lib/widgets/link_types.dart
@@ -6,11 +6,3 @@ import 'package:nordvpn/widgets/link.dart';
 final class FirstPartyLink<T> extends Link<T> {
   FirstPartyLink({super.key, required super.title, required super.uri});
 }
-
-/// A clickable link for third-party websites.
-///
-/// Use for links to pages outside the company's ecosystem.
-final class ThirdPartyLink<T> extends IconLink<T> {
-  ThirdPartyLink({super.key, required super.title, required super.uri})
-      : super(iconName: "external_link.svg");
-}
