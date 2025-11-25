d0dc206ce82a369a8af7645c914f09d5b4fa5bda
Stanislaw Kubiak <stanislaw.kubiak@nordsec.com>
2025-11-25 10:29:30 +0100

    Unify repeated bits of the firstopen_event tests
    Signed-off-by: Stanislaw Kubiak <stanislaw.kubiak@nordsec.com>

--- events/firstopen/firstopen_event_test.go
+++ events/firstopen/firstopen_event_test.go
@@ -6,123 +6,113 @@ import (
 	"testing"
 
 	"github.com/NordSecurity/nordvpn-linux/config"
-	"github.com/NordSecurity/nordvpn-linux/core"
 	"github.com/NordSecurity/nordvpn-linux/events"
 	"github.com/NordSecurity/nordvpn-linux/test/category"
 	"gotest.tools/v3/assert"
 )
 
+type testFixture struct {
+	count             int
+	fileConfigManager *config.FilesystemConfigManager
+	notifier          func() error
+	subscriber        *stubSubscriber
+}
+
+func newTestFixture(t *testing.T) *testFixture {
+	t.Helper()
+	f := testFixture{
+		count:             0,
+		fileConfigManager: &config.FilesystemConfigManager{NewInstallation: true},
+		subscriber:        &stubSubscriber{},
+	}
+	f.notifier = func() error {
+		f.count++
+		return nil
+	}
+	return &f
+}
+
 func Test_NoPublish_WhenNotNewInstallation(t *testing.T) {
 	category.Set(t, category.Unit)
 	resetGlobals()
 
-	cm := &config.FilesystemConfigManager{NewInstallation: false}
-	sub := &stubSubscriber{}
-	count := 0
-	testNotifier := func() error {
-		count++
-		return nil
-	}
+	tf := newTestFixture(t)
+	tf.fileConfigManager.NewInstallation = false
+	RegisterNotifier(tf.fileConfigManager, tf.subscriber, tf.notifier)
 
-	RegisterNotifier(cm, sub, testNotifier)
+	tf.subscriber.Publish(struct{}{})
 
-	sub.Publish(core.Insights{})
-
-	assert.Equal(t, count, 0)
+	assert.Equal(t, tf.count, 0)
 }
 
 func Test_PublishesExactlyOnce_OnFirstEvent(t *testing.T) {
 	category.Set(t, category.Unit)
 	resetGlobals()
 
-	cm := &config.FilesystemConfigManager{NewInstallation: true}
-	sub := &stubSubscriber{}
-	count := 0
-	testNotifier := func() error {
-		count++
-		return nil
-	}
+	tf := newTestFixture(t)
+	RegisterNotifier(tf.fileConfigManager, tf.subscriber, tf.notifier)
 
-	RegisterNotifier(cm, sub, testNotifier)
+	RegisterNotifier(tf.fileConfigManager, tf.subscriber, tf.notifier)
 
-	sub.Publish(core.Insights{})
-	assert.Equal(t, count, 1)
+	tf.subscriber.Publish(struct{}{})
+	assert.Equal(t, tf.count, 1)
 
 	// second install event - no new publish
-	sub.Publish(core.Insights{})
-	assert.Equal(t, count, 1)
+	tf.subscriber.Publish(struct{}{})
+	assert.Equal(t, tf.count, 1)
 }
 
 func Test_NoResubscribe_AfterPublished(t *testing.T) {
 	category.Set(t, category.Unit)
 	resetGlobals()
 
-	cm := &config.FilesystemConfigManager{NewInstallation: true}
-	sub := &stubSubscriber{}
-	count := 0
-	testNotifier := func() error {
-		count++
-		return nil
-	}
+	tf := newTestFixture(t)
 
-	RegisterNotifier(cm, sub, testNotifier)
-	sub.Publish(core.Insights{})
-	assert.Equal(t, count, 1)
-
+	RegisterNotifier(tf.fileConfigManager, tf.subscriber, tf.notifier)
+	tf.subscriber.Publish(struct{}{})
+	assert.Equal(t, tf.count, 1)
 	// a second registration attempt should not add another handler
-	RegisterNotifier(cm, sub, testNotifier)
-	assert.Equal(t, len(sub.handlers), 1)
+	RegisterNotifier(tf.fileConfigManager, tf.subscriber, tf.notifier)
+	assert.Equal(t, len(tf.subscriber.handlers), 1)
 
 	// even if we fire again, still only one publish
-	sub.Publish(core.Insights{})
-	assert.Equal(t, count, 1)
+	tf.subscriber.Publish(struct{}{})
+	assert.Equal(t, tf.count, 1)
 }
 
 func Test_WithMultipleRegistrations_EventIsEmittedOnlyOnce(t *testing.T) {
 	category.Set(t, category.Unit)
 	resetGlobals()
 
-	cm := &config.FilesystemConfigManager{NewInstallation: true}
-	sub := &stubSubscriber{}
-	count := 0
-	testNotifier := func() error {
-		count++
-		return nil
-	}
+	tf := newTestFixture(t)
 
-	RegisterNotifier(cm, sub, testNotifier)
-	RegisterNotifier(cm, sub, testNotifier)
-	RegisterNotifier(cm, sub, testNotifier)
+	RegisterNotifier(tf.fileConfigManager, tf.subscriber, tf.notifier)
+	RegisterNotifier(tf.fileConfigManager, tf.subscriber, tf.notifier)
+	RegisterNotifier(tf.fileConfigManager, tf.subscriber, tf.notifier)
 
-	sub.Publish(core.Insights{})
-	assert.Equal(t, count, 1)
+	tf.subscriber.Publish(struct{}{})
+	assert.Equal(t, tf.count, 1)
 }
 
 func Test_ConcurrentInstallEvents_OnlyOnePublish(t *testing.T) {
 	category.Set(t, category.Unit)
 	resetGlobals()
 
-	cm := &config.FilesystemConfigManager{NewInstallation: true}
-	sub := &stubSubscriber{}
-	count := 0
-	testNotifier := func() error {
-		count++
-		return nil
-	}
+	tf := newTestFixture(t)
 
-	RegisterNotifier(cm, sub, testNotifier)
+	RegisterNotifier(tf.fileConfigManager, tf.subscriber, tf.notifier)
 	var wg sync.WaitGroup
 	const n = 20
 	wg.Add(n)
 	for range n {
 		go func() {
 			defer wg.Done()
-			sub.Publish(core.Insights{})
+			tf.subscriber.Publish(struct{}{})
 		}()
 	}
 	wg.Wait()
 
-	assert.Equal(t, count, 1, "expected exactly 1 publish")
+	assert.Equal(t, tf.count, 1, "expected exactly 1 publish")
 }
 
 type stubSubscriber struct {
