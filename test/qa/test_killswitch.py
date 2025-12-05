f1d626a06f9a7bfda23682654bf909095921a4ab
Edvinas Gaubas <edvinas.gaubas@nordsec.com>
2025-12-05 14:18:01 +0200

    try with full test suite again to see if repro is due late timing of the test

--- test/qa/test_killswitch.py
+++ test/qa/test_killswitch.py
@@ -19,158 +19,158 @@ MSG_KILLSWITCH_ON = "Kill Switch has been successfully set to 'enabled'."
 MSG_KILLSWITCH_OFF = "Kill Switch has been successfully set to 'disabled'."
 
 
-# @pytest.mark.parametrize(("tech", "proto", "obfuscated"), lib.TECHNOLOGIES)
-# def test_killswitch_on_disconnected(tech, proto, obfuscated):
-#     """Manual TC: LVPN-419"""
+@pytest.mark.parametrize(("tech", "proto", "obfuscated"), lib.TECHNOLOGIES)
+def test_killswitch_on_disconnected(tech, proto, obfuscated):
+    """Manual TC: LVPN-419"""
 
-#     lib.set_technology_and_protocol(tech, proto, obfuscated)
-#     assert network.is_available()
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+    assert network.is_available()
 
-#     assert MSG_KILLSWITCH_ON in sh.nordvpn.set.killswitch("on")
-#     assert daemon.is_killswitch_on()
+    assert MSG_KILLSWITCH_ON in sh.nordvpn.set.killswitch("on")
+    assert daemon.is_killswitch_on()
 
-#     with lib.ErrorDefer(sh.nordvpn.set.killswitch.off):
-#         assert network.is_not_available(2)
+    with lib.ErrorDefer(sh.nordvpn.set.killswitch.off):
+        assert network.is_not_available(2)
 
-#     assert MSG_KILLSWITCH_OFF in sh.nordvpn.set.killswitch("off")
-#     assert not daemon.is_killswitch_on()
-#     assert network.is_available()
-
-
-# @pytest.mark.parametrize(("tech", "proto", "obfuscated"), lib.TECHNOLOGIES)
-# def test_killswitch_on_connect(tech, proto, obfuscated):
-#     """Manual TC: LVPN-8707"""
-
-#     lib.set_technology_and_protocol(tech, proto, obfuscated)
-#     assert network.is_available()
-
-#     assert MSG_KILLSWITCH_ON in sh.nordvpn.set.killswitch("on")
-#     assert daemon.is_killswitch_on()
-
-#     with lib.ErrorDefer(sh.nordvpn.set.killswitch.off):
-#         assert network.is_not_available(2)
-
-#         with lib.ErrorDefer(sh.nordvpn.disconnect):
-#             output = sh.nordvpn.connect()
-#             print(output)
-#             assert network.is_connected()
-
-#     output = sh.nordvpn.disconnect()
-#     print(output)
-
-#     with lib.ErrorDefer(sh.nordvpn.set.killswitch.off):
-#         assert network.is_not_available(2)
-
-#     assert MSG_KILLSWITCH_OFF in sh.nordvpn.set.killswitch("off")
-#     assert not daemon.is_killswitch_on()
-#     assert network.is_available()
+    assert MSG_KILLSWITCH_OFF in sh.nordvpn.set.killswitch("off")
+    assert not daemon.is_killswitch_on()
+    assert network.is_available()
 
 
-# @pytest.mark.parametrize(("tech", "proto", "obfuscated"), lib.TECHNOLOGIES)
-# def test_killswitch_on_connected(tech, proto, obfuscated):
-#     """Manual TC: LVPN-1394"""
+@pytest.mark.parametrize(("tech", "proto", "obfuscated"), lib.TECHNOLOGIES)
+def test_killswitch_on_connect(tech, proto, obfuscated):
+    """Manual TC: LVPN-8707"""
 
-#     lib.set_technology_and_protocol(tech, proto, obfuscated)
-#     assert network.is_available()
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+    assert network.is_available()
 
-#     with lib.Defer(sh.nordvpn.disconnect):
-#         output = sh.nordvpn.connect()
-#         print(output)
-#         assert network.is_connected()
+    assert MSG_KILLSWITCH_ON in sh.nordvpn.set.killswitch("on")
+    assert daemon.is_killswitch_on()
 
-#         assert MSG_KILLSWITCH_ON in sh.nordvpn.set.killswitch("on")
-#         assert daemon.is_killswitch_on()
+    with lib.ErrorDefer(sh.nordvpn.set.killswitch.off):
+        assert network.is_not_available(2)
 
-#     with lib.ErrorDefer(sh.nordvpn.set.killswitch.off):
-#         assert network.is_not_available(2)
+        with lib.ErrorDefer(sh.nordvpn.disconnect):
+            output = sh.nordvpn.connect()
+            print(output)
+            assert network.is_connected()
 
-#     assert MSG_KILLSWITCH_OFF in sh.nordvpn.set.killswitch("off")
-#     assert not daemon.is_killswitch_on()
-#     assert network.is_available()
+    output = sh.nordvpn.disconnect()
+    print(output)
 
+    with lib.ErrorDefer(sh.nordvpn.set.killswitch.off):
+        assert network.is_not_available(2)
 
-# @pytest.mark.parametrize(("tech", "proto", "obfuscated"), lib.TECHNOLOGIES)
-# def test_killswitch_off_connected(tech, proto, obfuscated):
-#     """Manual TC: LVPN-2195"""
-
-#     lib.set_technology_and_protocol(tech, proto, obfuscated)
-#     assert network.is_available()
-
-#     assert MSG_KILLSWITCH_ON in sh.nordvpn.set.killswitch("on")
-#     assert daemon.is_killswitch_on()
-
-#     with lib.ErrorDefer(sh.nordvpn.set.killswitch.off):
-#         assert network.is_not_available(2)
-
-#         with lib.Defer(sh.nordvpn.disconnect):
-#             output = sh.nordvpn.connect()
-#             print(output)
-#             assert network.is_connected()
-
-#             assert MSG_KILLSWITCH_OFF in sh.nordvpn.set.killswitch("off")
-#             assert not daemon.is_killswitch_on()
-#             assert network.is_available()
-
-#     assert network.is_available()
+    assert MSG_KILLSWITCH_OFF in sh.nordvpn.set.killswitch("off")
+    assert not daemon.is_killswitch_on()
+    assert network.is_available()
 
 
-# @pytest.mark.parametrize(("tech_from", "proto_from", "obfuscated_from"), lib.TECHNOLOGIES)
-# @pytest.mark.parametrize(("tech_to", "proto_to", "obfuscated_to"), lib.TECHNOLOGIES)
-# def test_killswitch_reconnect(tech_from, proto_from, obfuscated_from, tech_to, proto_to, obfuscated_to):
-#     """Manual TC: LVPN-8716"""
+@pytest.mark.parametrize(("tech", "proto", "obfuscated"), lib.TECHNOLOGIES)
+def test_killswitch_on_connected(tech, proto, obfuscated):
+    """Manual TC: LVPN-1394"""
 
-#     lib.set_technology_and_protocol(tech_from, proto_from, obfuscated_from)
-#     assert network.is_available()
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+    assert network.is_available()
 
-#     assert MSG_KILLSWITCH_ON in sh.nordvpn.set.killswitch("on")
-#     assert daemon.is_killswitch_on()
+    with lib.Defer(sh.nordvpn.disconnect):
+        output = sh.nordvpn.connect()
+        print(output)
+        assert network.is_connected()
 
-#     with lib.ErrorDefer(sh.nordvpn.set.killswitch.off):
-#         assert network.is_not_available(2)
+        assert MSG_KILLSWITCH_ON in sh.nordvpn.set.killswitch("on")
+        assert daemon.is_killswitch_on()
 
-#         with lib.ErrorDefer(sh.nordvpn.disconnect):
-#             output = sh.nordvpn.connect()
-#             print(output)
-#             assert network.is_connected()
+    with lib.ErrorDefer(sh.nordvpn.set.killswitch.off):
+        assert network.is_not_available(2)
 
-#             lib.set_technology_and_protocol(tech_to, proto_to, obfuscated_to)
-#             assert network.is_connected()
-#             output = sh.nordvpn.connect()
-#             print(output)
-#             assert network.is_connected()
-
-#     output = sh.nordvpn.disconnect()
-#     print(output)
-#     assert daemon.is_disconnected()
-
-#     with lib.ErrorDefer(sh.nordvpn.set.killswitch.off):
-#         assert network.is_not_available(2)
-
-#     assert MSG_KILLSWITCH_OFF in sh.nordvpn.set.killswitch("off")
-#     assert not daemon.is_killswitch_on()
-#     assert network.is_available()
+    assert MSG_KILLSWITCH_OFF in sh.nordvpn.set.killswitch("off")
+    assert not daemon.is_killswitch_on()
+    assert network.is_available()
 
 
-# # Test for 3.8.7 hotfix. Account and login commands would not work when killswitch is on
-# # Issue 441
-# def test_fancy_transport():
-#     """Manual TC: LVPN-8717"""
+@pytest.mark.parametrize(("tech", "proto", "obfuscated"), lib.TECHNOLOGIES)
+def test_killswitch_off_connected(tech, proto, obfuscated):
+    """Manual TC: LVPN-2195"""
 
-#     sh.nordvpn.logout("--persist-token")
-#     output = sh.nordvpn.set.killswitch("on")
-#     assert MSG_KILLSWITCH_ON in output
+    lib.set_technology_and_protocol(tech, proto, obfuscated)
+    assert network.is_available()
 
-#     output = login.login_as("default")
-#     print(output)
-#     assert "Welcome to NordVPN!" in output
+    assert MSG_KILLSWITCH_ON in sh.nordvpn.set.killswitch("on")
+    assert daemon.is_killswitch_on()
 
-#     with lib.ErrorDefer(sh.nordvpn.set.killswitch.off):
-#         output = sh.nordvpn.account()
-#         print(output)
-#         assert "Account Information:" in output
+    with lib.ErrorDefer(sh.nordvpn.set.killswitch.off):
+        assert network.is_not_available(2)
 
-#     sh.nordvpn.set.killswitch("off")
-#     assert network.is_available()
+        with lib.Defer(sh.nordvpn.disconnect):
+            output = sh.nordvpn.connect()
+            print(output)
+            assert network.is_connected()
+
+            assert MSG_KILLSWITCH_OFF in sh.nordvpn.set.killswitch("off")
+            assert not daemon.is_killswitch_on()
+            assert network.is_available()
+
+    assert network.is_available()
+
+
+@pytest.mark.parametrize(("tech_from", "proto_from", "obfuscated_from"), lib.TECHNOLOGIES)
+@pytest.mark.parametrize(("tech_to", "proto_to", "obfuscated_to"), lib.TECHNOLOGIES)
+def test_killswitch_reconnect(tech_from, proto_from, obfuscated_from, tech_to, proto_to, obfuscated_to):
+    """Manual TC: LVPN-8716"""
+
+    lib.set_technology_and_protocol(tech_from, proto_from, obfuscated_from)
+    assert network.is_available()
+
+    assert MSG_KILLSWITCH_ON in sh.nordvpn.set.killswitch("on")
+    assert daemon.is_killswitch_on()
+
+    with lib.ErrorDefer(sh.nordvpn.set.killswitch.off):
+        assert network.is_not_available(2)
+
+        with lib.ErrorDefer(sh.nordvpn.disconnect):
+            output = sh.nordvpn.connect()
+            print(output)
+            assert network.is_connected()
+
+            lib.set_technology_and_protocol(tech_to, proto_to, obfuscated_to)
+            assert network.is_connected()
+            output = sh.nordvpn.connect()
+            print(output)
+            assert network.is_connected()
+
+    output = sh.nordvpn.disconnect()
+    print(output)
+    assert daemon.is_disconnected()
+
+    with lib.ErrorDefer(sh.nordvpn.set.killswitch.off):
+        assert network.is_not_available(2)
+
+    assert MSG_KILLSWITCH_OFF in sh.nordvpn.set.killswitch("off")
+    assert not daemon.is_killswitch_on()
+    assert network.is_available()
+
+
+# Test for 3.8.7 hotfix. Account and login commands would not work when killswitch is on
+# Issue 441
+def test_fancy_transport():
+    """Manual TC: LVPN-8717"""
+
+    sh.nordvpn.logout("--persist-token")
+    output = sh.nordvpn.set.killswitch("on")
+    assert MSG_KILLSWITCH_ON in output
+
+    output = login.login_as("default")
+    print(output)
+    assert "Welcome to NordVPN!" in output
+
+    with lib.ErrorDefer(sh.nordvpn.set.killswitch.off):
+        output = sh.nordvpn.account()
+        print(output)
+        assert "Account Information:" in output
+
+    sh.nordvpn.set.killswitch("off")
+    assert network.is_available()
 
 
 # This test assumes being run on docker
