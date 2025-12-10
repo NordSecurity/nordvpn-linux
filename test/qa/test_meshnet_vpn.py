7efc6bfeb0f8cc1d5d923bd2c747bf208dd20627
Bartosz Oleaczek <bartosz.oleaczek@nordsec.com>
2025-12-10 07:19:20 +0100

    LVPN-9736: set app to defaults in meshnet test setup
    When a test fails in setup, the teardown will not be performed. In such
    cases we need to perform the necessary teardown in the setup function,
    otherwise next test will be started in invalid state. Primarily, this
    concerns setting the app to default/logging out as otherwise the next test will fail
    at the login attempt.
    
    Signed-off-by: Bartosz Oleaczek <bartosz.oleaczek@nordsec.com>

--- test/qa/test_meshnet_vpn.py
+++ test/qa/test_meshnet_vpn.py
@@ -66,7 +66,7 @@ def test_lan_discovery_exitnode(lan_discovery: bool, local: bool):
         assert result, message
 
 
-@pytest.mark.xfail
+# @pytest.mark.xfail
 @pytest.mark.parametrize("lan_discovery", [True, False])
 @pytest.mark.parametrize("local", [True, False])
 def test_killswitch_exitnode_vpn(lan_discovery: bool, local: bool):
