bbe899fc5de5caf49db51d803457552c4aac7528
Vadym Hnatenko <vadym.hnatenko@nordsec.com>
2026-01-07 16:34:06 +0100

    LVPN-9733:Optimize pipeline test job execution times by removing diminishing returns cases  - Improved dynamic_parametrize.py to support different input parameters  - Applied dynamic_parametrize decorator to the combintations tests

--- test/qa/lib/dynamic_parametrize.py
+++ test/qa/lib/dynamic_parametrize.py
@@ -2,9 +2,33 @@ import random
 from functools import wraps
 
 import pytest
+from itertools import product
 
+def _is_list_of_lists(obj):
+    """
+    Check if the object is a list/tuple of lists/tuples.
 
-def _generate_params_with_ids(randomized_source, ordered_source, id_pattern=None, always_pair=None):
+    :param obj: Object to check.
+    :return: True if obj is a list/tuple of lists/tuples, else False.
+    """
+    return isinstance(obj, list | tuple) and obj and all(isinstance(x, list | tuple) for x in obj)
+
+def _flatten_param_tuple(combo):
+    """
+    Flatten any nested tuples/lists inside a combo into a single tuple.
+
+    :param combo: An iterable of parameters.
+    :return: A flattened tuple of values.
+    """
+    flat = []
+    for item in combo:
+        if isinstance(item, tuple | list):
+            flat.extend(item)
+        else:
+            flat.append(item)
+    return tuple(flat)
+
+def _generate_params_with_ids(argnames, randomized_source, ordered_source, cartesian_mode=False, id_pattern=None, always_pair=None, sample_size=None):
     """
     Generate test parameters and their associated unique identifiers (IDs).
 
@@ -12,22 +36,42 @@ def _generate_params_with_ids(randomized_source, ordered_source, id_pattern=None
     randomized data and ordered data. Additionally, test case IDs
     are constructed to provide human-readable descriptions for test output.
 
+    :param argnames: A list of argument names for the test function.
     :param randomized_source:
         A list of items that will be used as randomized data.
     :param ordered_source:
         A list of ordered items.
+    :param cartesian_mode: If True, generate the full cross product (cartesian product) of all sources.
     :param id_pattern:
         A format string for generating unique test IDs.
-        - `{randomized}` is replaced by the corresponding value from the randomized source.
-        - `{ordered}` is replaced by the corresponding value from the ordered source.
+        - {randomized} is replaced by the corresponding value from the randomized source.
+        - {ordered} is replaced by the corresponding value from the ordered source.
     :param always_pair: item from randomized_source to always be paired with every ordered value
+    :param sample_size: Randomly select this many cases from all cartesian combinations (cartesian mode only).
 
     :return:
-        - `params`: A list of parameter combining randomized and ordered data.
-        - `ids`: A list of human-readable strings with names for each test case.
+        - params: A list of parameter combining randomized and ordered data.
+        - ids: A list of human-readable strings with names for each test case.
     """
     params = []
     ids = []
+
+    if cartesian_mode:
+        rnds = randomized_source if _is_list_of_lists(randomized_source) else [randomized_source]
+        ords = ordered_source if _is_list_of_lists(ordered_source) else [ordered_source]
+        sources = list(rnds) + list(ords)
+        all_combos = [_flatten_param_tuple(combo) for combo in product(*sources)]
+        if sample_size is not None:
+            rng = random.Random()
+            all_combos = rng.sample(all_combos, min(sample_size, len(all_combos)))
+        for flat_combo in all_combos:
+            params.append(flat_combo)
+            if id_pattern:
+                arg_dict = dict(zip(argnames, flat_combo, strict=False))
+                ids.append(id_pattern.format(**arg_dict))
+            else:
+                ids.append("".join(str(v) for v in flat_combo))
+        return params, ids
 
     def add_params_and_ids(randomized, ordered):
         param_tuple = (*randomized, ordered)
@@ -53,11 +97,11 @@ def _generate_params_with_ids(randomized_source, ordered_source, id_pattern=None
     return params, ids
 
 
-def dynamic_parametrize(argnames, randomized_source, ordered_source, id_pattern=None, always_pair=None):
+def dynamic_parametrize(argnames, randomized_source, ordered_source, cartesian_mode=False, id_pattern=None, always_pair=None, sample_size=None):
     """
     A pytest decorator for dynamic parameterization of test functions.
 
-    This decorator applies dynamic parameters (`randomized_source` + `ordered_source`) to pytest test functions,
+    This decorator applies dynamic parameters ({randomized_source} + {ordered_source}) to pytest test functions,
     automating the generation of parameterized test cases while supporting human-readable test IDs.
 
     :param argnames:
@@ -66,18 +110,20 @@ def dynamic_parametrize(argnames, randomized_source, ordered_source, id_pattern=
         A list of items that will be used as randomized data.
     :param ordered_source:
         A list of ordered items.
+    :param cartesian_mode: If True, generate the full cross product (cartesian product) of all sources.
     :param id_pattern:
         A format string for generating unique test IDs.
-        - `{randomized}` is replaced by the corresponding value from the randomized source.
-        - `{ordered}` is replaced by the corresponding value from the ordered source.
+        - {randomized_source} is replaced by the corresponding value from the randomized source.
+        - {ordered_source} is replaced by the corresponding value from the ordered source.
     :param always_pair: item from randomized_source to always be paired with every ordered value
+    :param sample_size: Randomly select this many cases from all cartesian combinations (cartesian mode only).
 
     :return:
-        The dynamically parameterized test function wrapped with pytest's `pytest.mark.parametrize`.
+        The dynamically parameterized test function wrapped with pytest's pytest.mark.parametrize.
     """
     def decorator(func):
-
-        params, ids = _generate_params_with_ids(randomized_source, ordered_source, id_pattern, always_pair)
+        params, ids = _generate_params_with_ids(argnames, randomized_source, ordered_source, cartesian_mode,
+                                                id_pattern, always_pair, sample_size)
 
         @pytest.mark.parametrize(argnames, params, ids=ids)
         @wraps(func)
