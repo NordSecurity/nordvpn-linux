b90d5c0d6a07fbb5ab3aeca10933fb0922516731
Bartosz Oleaczek <bartosz.oleaczek@nordsec.com>
2026-01-12 08:48:30 +0100

    fix uts

--- daemon/dns/dns.go
+++ daemon/dns/dns.go
@@ -216,8 +216,6 @@ func (d *DNSServiceSetter) Unset(iface string) error {
 type DNSMethodSetter struct {
 	publisher events.Publisher[string]
 	methods   []Method
-	// unsetMethod is the method previously used to configure DNS
-	unsetMethod Method
 }
 
 func NewSetter(publisher events.Publisher[string], methods ...Method) *DNSMethodSetter {
@@ -257,12 +255,14 @@ func (d *DNSMethodSetter) Set(iface string, nameservers []string) error {
 // is available, and remove the backup on success.
 func (d *DNSMethodSetter) Unset(iface string) error {
 	d.publisher.Publish("unsetting DNS")
-	if d.unsetMethod == nil {
-		return fmt.Errorf("unset method was not set")
-	}
 
-	if err := d.unsetMethod.Unset(iface); err != nil {
-		return fmt.Errorf("unsetting DNS: %w", err)
+	for _, method := range d.methods {
+		d.publisher.Publish("unset dns for interface [" + iface + "] using: " + method.Name())
+		if err := method.Unset(iface); err != nil {
+			log.Println(internal.ErrorPrefix, fmt.Errorf("unsetting dns with %s: %w", method.Name(), err))
+			continue
+		}
+		return nil
 	}
 
 	return nil
