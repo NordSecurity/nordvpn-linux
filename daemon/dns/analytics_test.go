3a60287cc2009011f7ad84353f3ed88001b89e88
Bartosz Oleaczek <122802123+bartoszWojciechO@users.noreply.github.com>
2026-02-25 09:46:57 +0100

    LVPN-9757: add DNS configuration error events
    Signed-off-by: Bartosz Oleaczek <122802123+bartoszWojciechO@users.noreply.github.com>
    Co-authored-by: Stanisław Kubiak <stanislaw.kubiak@nordsec.com>

--- daemon/dns/analytics_test.go
+++ daemon/dns/analytics_test.go
@@ -2,17 +2,21 @@ package dns
 
 import (
 	"fmt"
+	"strconv"
 	"testing"
 
 	"github.com/NordSecurity/nordvpn-linux/events"
+	"github.com/NordSecurity/nordvpn-linux/test/category"
 	mockevents "github.com/NordSecurity/nordvpn-linux/test/mock/events"
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_emitResolvConfOverwrittenEvent(t *testing.T) {
+	category.Set(t, category.Unit)
+
 	mockPublisher := mockevents.MockPublisher[events.DebuggerEvent]{}
 	analytics := newDNSAnalytics(&mockPublisher)
-	analytics.emitResolvConfOverwrittenEvent()
+	analytics.emitResolvConfOverwrittenEvent(unknown)
 
 	event, n, stackIsEmpty := mockPublisher.PopEvent()
 
@@ -20,7 +24,7 @@ func Test_emitResolvConfOverwrittenEvent(t *testing.T) {
 	assert.Equal(t, 0, n, "Unexpected number of events emitted.")
 	assert.Contains(t, event.KeyBasedContextPaths, events.ContextValue{
 		Path:  debuggerEventBaseKey + "." + debuggerEventTypeKey,
-		Value: resolvConfOverwritten.String()})
+		Value: resolvConfOverwrittenEventType.String()})
 	assert.Contains(t, event.KeyBasedContextPaths, events.ContextValue{
 		Path:  debuggerEventBaseKey + "." + debuggerEventManagementServiceKey,
 		Value: unknown.String()})
@@ -30,6 +34,8 @@ func Test_emitResolvConfOverwrittenEvent(t *testing.T) {
 }
 
 func Test_emitDNSConfiguredEvent(t *testing.T) {
+	category.Set(t, category.Unit)
+
 	tests := []struct {
 		name              string
 		managementService dnsManagementService
@@ -48,8 +54,7 @@ func Test_emitDNSConfiguredEvent(t *testing.T) {
 		t.Run(test.name, func(t *testing.T) {
 			mockPublisher := mockevents.MockPublisher[events.DebuggerEvent]{}
 			analytics := newDNSAnalytics(&mockPublisher)
-			analytics.setManagementService(test.managementService)
-			analytics.emitDNSConfiguredEvent()
+			analytics.emitDNSConfiguredEvent(test.managementService)
 
 			event, n, stackIsEmpty := mockPublisher.PopEvent()
 
@@ -57,7 +62,7 @@ func Test_emitDNSConfiguredEvent(t *testing.T) {
 			assert.Equal(t, 0, n, "Unexpected number of events emitted.")
 			assert.Contains(t, event.KeyBasedContextPaths, events.ContextValue{
 				Path:  debuggerEventBaseKey + "." + debuggerEventTypeKey,
-				Value: dnsConfigured.String()})
+				Value: dnsConfiguredEventType.String()})
 			assert.Contains(t, event.KeyBasedContextPaths, events.ContextValue{
 				Path:  debuggerEventBaseKey + "." + debuggerEventManagementServiceKey,
 				Value: test.managementService.String()})
@@ -65,6 +70,106 @@ func Test_emitDNSConfiguredEvent(t *testing.T) {
 			expectedJson :=
 				fmt.Sprintf("{\"event\":\"dns_configured\",\"namespace\":\"nordvpn-linux\",\"management_service\":\"%s\"}",
 					test.managementService.String())
+			assert.Equal(t,
+				expectedJson,
+				event.JsonData)
+		})
+	}
+}
+
+func Test_emitDNSConfigurationErrorEvent(t *testing.T) {
+	category.Set(t, category.Unit)
+
+	tests := []struct {
+		name              string
+		managementService dnsManagementService
+		errorType         errorType
+		critical          bool
+	}{
+		{
+			name:              "set failed for unmanaged, not critical",
+			managementService: unmanaged,
+			errorType:         setFailedErrorType,
+			critical:          false,
+		},
+		{
+			name:              "failed to read for unmanaged, not critical",
+			managementService: unmanaged,
+			errorType:         resolvConfReadFailedErrorType,
+			critical:          false,
+		},
+		{
+			name:              "set failed for unmanaged, critical",
+			managementService: unmanaged,
+			errorType:         setFailedErrorType,
+			critical:          true,
+		},
+		{
+			name:              "failed to read for unmanaged, critical",
+			managementService: unmanaged,
+			errorType:         resolvConfReadFailedErrorType,
+			critical:          true,
+		},
+		{
+			name:              "set failed for systemd-resolved, not critical",
+			managementService: systemdResolved,
+			errorType:         setFailedErrorType,
+			critical:          false,
+		},
+		{
+			name:              "failed to read for systemd-resolved, not critical",
+			managementService: systemdResolved,
+			errorType:         resolvConfReadFailedErrorType,
+			critical:          false,
+		},
+		{
+			name:              "set failed for systemd-resolved, critical",
+			managementService: systemdResolved,
+			errorType:         setFailedErrorType,
+			critical:          true,
+		},
+		{
+			name:              "failed to read for systemd-resolved, critical",
+			managementService: systemdResolved,
+			errorType:         resolvConfReadFailedErrorType,
+			critical:          true,
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			mockPublisher := mockevents.MockPublisher[events.DebuggerEvent]{}
+			analytics := newDNSAnalytics(&mockPublisher)
+			if test.critical {
+				analytics.emitDNSConfigurationCriticalErrorEvent(test.managementService, test.errorType)
+			} else {
+				analytics.emitDNSConfigurationErrorEvent(test.managementService, test.errorType)
+			}
+
+			event, n, stackIsEmpty := mockPublisher.PopEvent()
+
+			assert.True(t, stackIsEmpty, "Event not emitted.")
+			assert.Equal(t, 0, n, "Unexpected number of events emitted.")
+			assert.Contains(t, event.KeyBasedContextPaths, events.ContextValue{
+				Path:  debuggerEventBaseKey + "." + debuggerEventTypeKey,
+				Value: dnsConfigurationErrorEventType.String()})
+			assert.Contains(t, event.KeyBasedContextPaths, events.ContextValue{
+				Path:  debuggerEventBaseKey + "." + debuggerEventManagementServiceKey,
+				Value: test.managementService.String()})
+			assert.Contains(t, event.KeyBasedContextPaths, events.ContextValue{
+				Path:  debuggerEventBaseKey + "." + debuggerEventErrorTypeKey,
+				Value: test.errorType.String(),
+			})
+			assert.Contains(t, event.KeyBasedContextPaths, events.ContextValue{
+				Path:  debuggerEventBaseKey + "." + debuggerEventCriticalKey,
+				Value: test.critical,
+			})
+
+			expectedJson :=
+				fmt.Sprintf("{\"event\":\"dns_configuration_error\",\"namespace\":\"nordvpn-linux\",\"management_service\":\"%s\",\"error_type\":\"%s\",\"critical\":%s}",
+					test.managementService.String(),
+					test.errorType.String(),
+					strconv.FormatBool(test.critical))
 			assert.Equal(t,
 				expectedJson,
 				event.JsonData)
