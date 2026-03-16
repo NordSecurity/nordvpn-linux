711940da38703273d9f2380691bbe1f14f680c75
Gražvidas Balčius <grazvidas.balcius@nordsec.com>
2026-03-16 09:47:51 +0200

    Update test
    Signed-off-by: Gražvidas Balčius <grazvidas.balcius@nordsec.com>

--- test/qa/test_login.py
+++ test/qa/test_login.py
@@ -2,6 +2,7 @@ import pytest
 import pexpect
 import sh
 
+
 import lib
 from lib import (
     daemon,
@@ -10,6 +11,7 @@ from lib import (
     settings,
     selenium,
 )
+
 
 pytestmark = pytest.mark.usefixtures("collect_logs")
 
@@ -202,10 +204,15 @@ def test_logout_disconnects():
 def test_token_login_requires_tty():
     """Manual TC: LVPN-485"""
 
+    sh.nordvpn.set.analytics("enabled")
     with pytest.raises(sh.ErrorReturnCode_1) as ex:
-        sh.nordvpn.login("--token")
+        # simulate non-tty input by piping a token via stdin
+        sh.nordvpn.login("--token", _in="fake_token\n")
 
-    expected = "We couldn't prompt for the token because the input is not a terminal."
+    expected = (
+        "We couldn't prompt for a login token, "
+        "because the command is not running in a terminal."
+    )
     assert expected in ex.value.stdout.decode("utf-8")
 
 
