e739116e3777c96b8c403a3b50c50f4eeb08e12f
Daniel Fetti <39974476+dfetti@users.noreply.github.com>
2026-03-26 10:15:12 +0200

    LVPN-10155: Add connection card connecting state (#1395)
    Signed-off-by: Daniel Fetti <daniel.fetti@nordsec.com>

--- gui/lib/vpn/connection_card_server_info.dart
+++ gui/lib/vpn/connection_card_server_info.dart
@@ -14,26 +14,32 @@ final class ConnectionCardServerInfo extends StatelessWidget {
     final statusCardTheme = context.vpnStatusCardTheme;
     var label = t.ui.connectToVpn;
 
-    if (vpnStatus.isConnected()) {
-      assert(
-        vpnStatus.isMeshnetRouting ||
-            (vpnStatus.country != null && vpnStatus.city != null),
-      );
+    if (vpnStatus.isDisconnected()) {
+      label = t.ui.fastestServer;
+    } else {
+      if (vpnStatus.isConnected()) {
+        assert(
+          vpnStatus.isMeshnetRouting ||
+              (vpnStatus.country != null && vpnStatus.city != null),
+          "Status is connected, but we don't know to what we connected",
+        );
+      }
 
       if (vpnStatus.isMeshnetRouting) {
         label = vpnStatus.hostname ?? vpnStatus.ip ?? "";
       } else if (vpnStatus.country != null) {
         final countryName = vpnStatus.country!.localizedName;
-        label = "$countryName - ${vpnStatus.city ?? ""}";
+        if (vpnStatus.city != null) {
+          label = "${vpnStatus.city!}, $countryName";
+        } else {
+          label = countryName;
+        }
         label += vpnStatus.isVirtualLocation ? " - ${t.ui.virtual}" : "";
       } else {
         label = t.ui.fastestServer;
       }
-    } else if (vpnStatus.isConnecting()) {
-      label = t.ui.findingServer;
-    } else if (vpnStatus.isDisconnected()) {
-      label = t.ui.fastestServer;
     }
+
     return Text(
       label,
       key: ConnectionCardServerInfo.textKey,
