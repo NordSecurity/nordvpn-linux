dea35c56e27fa147103e5a7fb3f8e4cbac52f585
Stanislaw Kubiak <stanislaw.kubiak@nordsec.com>
2026-04-02 09:04:29 +0200

    Convert toast listener to a stateless consumer
    Signed-off-by: Stanislaw Kubiak <stanislaw.kubiak@nordsec.com>

--- gui/lib/widgets/toasts_listener.dart
+++ gui/lib/widgets/toasts_listener.dart
@@ -4,23 +4,18 @@ import 'package:nordvpn/data/providers/toasts_provider.dart';
 import 'package:nordvpn/theme/toast_theme.dart';
 import 'package:nordvpn/widgets/toast.dart';
 
-final class ToastsListener extends ConsumerStatefulWidget {
+final class ToastsListener extends ConsumerWidget {
   final Widget child;
 
   const ToastsListener({super.key, required this.child});
 
   @override
-  ConsumerState<ToastsListener> createState() => _ToastsListenerState();
-}
-
-final class _ToastsListenerState extends ConsumerState<ToastsListener> {
-  @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context, WidgetRef ref) {
     Duration? timeout = ref.watch(toastsProvider);
     final theme = context.toastTheme;
     return Stack(
       children: [
-        widget.child,
+        child,
         if (timeout != null)
           Positioned(
             right: theme.widgetPositionRight,
