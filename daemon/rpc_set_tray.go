8d48fe460ca1fdac9f768f2a14a4ed9482f4784d
Gražvidas Balčius <grazvidas.balcius@nordsec.com>
2026-04-10 09:13:02 +0300

    Eliminate uuid proto field and extract from ctx
    Signed-off-by: Gražvidas Balčius <grazvidas.balcius@nordsec.com>

--- daemon/rpc_set_tray.go
+++ daemon/rpc_set_tray.go
@@ -7,15 +7,34 @@ import (
 	"github.com/NordSecurity/nordvpn-linux/daemon/pb"
 	"github.com/NordSecurity/nordvpn-linux/internal"
 	"github.com/NordSecurity/nordvpn-linux/log"
+	"google.golang.org/grpc/peer"
 )
 
 func (r *RPC) SetTray(ctx context.Context, in *pb.SetTrayRequest) (*pb.Payload, error) {
+	peer, ok := peer.FromContext(ctx)
+	if !ok {
+		log.Println(internal.ErrorPrefix, "failed to retrieve gRPC peer information from the context")
+		return &pb.Payload{
+			Type: internal.CodeInternalError,
+		}, nil
+	}
+
+	cred, ok := peer.AuthInfo.(internal.UcredAuth)
+	if !ok {
+		log.Println(internal.ErrorPrefix, "failed to extract ucred out of gRPC peer info")
+		return &pb.Payload{
+			Type: internal.CodeInternalError,
+		}, nil
+	}
+
+	uid := int64(cred.Uid)
+
 	var cfg config.Config
 	if err := r.cm.Load(&cfg); err != nil {
 		log.Println(internal.ErrorPrefix, err)
 	}
 
-	trayStatus := !cfg.UsersData.TrayOff[in.GetUid()]
+	trayStatus := !cfg.UsersData.TrayOff[uid]
 
 	if in.GetTray() == trayStatus {
 		getBool := func(label bool) string {
@@ -32,7 +51,7 @@ func (r *RPC) SetTray(ctx context.Context, in *pb.SetTrayRequest) (*pb.Payload, 
 
 	if !in.GetTray() {
 		if err := r.cm.SaveWith(func(c config.Config) config.Config {
-			c.UsersData.TrayOff[in.GetUid()] = true
+			c.UsersData.TrayOff[uid] = true
 			return c
 		}); err != nil {
 			log.Println(internal.ErrorPrefix, err)
@@ -42,7 +61,7 @@ func (r *RPC) SetTray(ctx context.Context, in *pb.SetTrayRequest) (*pb.Payload, 
 		}
 	} else {
 		if err := r.cm.SaveWith(func(c config.Config) config.Config {
-			delete(c.UsersData.TrayOff, in.GetUid())
+			delete(c.UsersData.TrayOff, uid)
 			return c
 		}); err != nil {
 			log.Println(internal.ErrorPrefix, err)
@@ -52,8 +71,7 @@ func (r *RPC) SetTray(ctx context.Context, in *pb.SetTrayRequest) (*pb.Payload, 
 		}
 	}
 
-	// #nosec G115
-	if err := r.norduser.Restart(uint32(in.GetUid())); err != nil {
+	if err := r.norduser.Restart(cred.Uid); err != nil {
 		log.Println(internal.ErrorPrefix, "Cannot restart norduserd", err)
 	}
 
