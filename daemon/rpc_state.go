20f1109ae04d73ae0d85f7f2e31d0bf960eeab13
Gražvidas Balčius <grazvidas.balcius@nordsec.com>
2026-04-15 12:25:37 +0300

    LVPN-7836: Set tray allows change tray status for any arbitrary os users (#1425)

--- daemon/rpc_state.go
+++ daemon/rpc_state.go
@@ -7,7 +7,6 @@ import (
 	"github.com/NordSecurity/nordvpn-linux/events"
 	"github.com/NordSecurity/nordvpn-linux/internal"
 	"github.com/NordSecurity/nordvpn-linux/log"
-	"google.golang.org/grpc/peer"
 )
 
 func configToProtobuf(cfg *config.Config, uid int64) *pb.Settings {
@@ -164,19 +163,16 @@ func statusStream(stateChan <-chan any,
 func (r *RPC) SubscribeToStateChanges(_ *pb.Empty, srv pb.Daemon_SubscribeToStateChangesServer) error {
 	log.Println(internal.InfoPrefix, "Received new subscription request")
 
-	peer, ok := peer.FromContext(srv.Context())
-	var uid int64
-	if ok {
-		cred, ok := peer.AuthInfo.(internal.UcredAuth)
-		if !ok {
-			return srv.Send(&pb.AppState{
-				State: &pb.AppState_Error{
-					Error: pb.AppStateError_FAILED_TO_GET_UID,
-				},
-			})
-		}
-		uid = int64(cred.Uid)
+	cred, err := getCallerCred(srv.Context())
+	if err != nil {
+		log.Println(internal.ErrorPrefix, "SubscribeToStateChanges:", err)
+		return srv.Send(&pb.AppState{
+			State: &pb.AppState_Error{
+				Error: pb.AppStateError_FAILED_TO_GET_UID,
+			},
+		})
 	}
+	uid := int64(cred.Uid)
 
 	stateChan, stopChan := r.statePublisher.AddSubscriber()
 	statusStream(stateChan, stopChan, uid, srv, &r.RequestedConnParams)
