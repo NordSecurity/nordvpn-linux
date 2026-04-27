aa6d6b64d201964297ac0702127b30b515cf3f7a
Gražvidas Balčius <grazvidas.balcius@nordsec.com>
2026-04-27 14:15:04 +0300

    Remove regions from 'groups' response
    Signed-off-by: Gražvidas Balčius <grazvidas.balcius@nordsec.com>

--- config/server_groups.go
+++ config/server_groups.go
@@ -2,16 +2,21 @@ package config
 
 // GroupMap maps group titles to IDs
 var GroupMap = map[string]ServerGroup{
-	"double_vpn":                       ServerGroup_DOUBLE_VPN,
-	"onion_over_vpn":                   ServerGroup_ONION_OVER_VPN,
-	"dedicated_ip":                     ServerGroup_DEDICATED_IP,
-	"standard_vpn_servers":             ServerGroup_STANDARD_VPN_SERVERS,
-	"p2p":                              ServerGroup_P2P,
-	"europe":                           ServerGroup_EUROPE,
-	"the_americas":                     ServerGroup_THE_AMERICAS,
-	"asia_pacific":                     ServerGroup_ASIA_PACIFIC,
-	"africa_the_middle_east_and_india": ServerGroup_AFRICA_THE_MIDDLE_EAST_AND_INDIA,
-	"obfuscated_servers":               ServerGroup_OBFUSCATED,
+	"double_vpn":           ServerGroup_DOUBLE_VPN,
+	"onion_over_vpn":       ServerGroup_ONION_OVER_VPN,
+	"dedicated_ip":         ServerGroup_DEDICATED_IP,
+	"standard_vpn_servers": ServerGroup_STANDARD_VPN_SERVERS,
+	"p2p":                  ServerGroup_P2P,
+	"obfuscated_servers":   ServerGroup_OBFUSCATED,
+}
+
+// IsRegionalGroup reports whether g is a deprecated regional group; uses raw ints since the named constants are removed.
+func IsRegionalGroup(g ServerGroup) bool {
+	switch g {
+	case 19, 21, 23, 25: // EUROPE, THE_AMERICAS, ASIA_PACIFIC, AFRICA_THE_MIDDLE_EAST_AND_INDIA
+		return true
+	}
+	return false
 }
 
 // GroupTitleForId converts group ID to group lowercase title
