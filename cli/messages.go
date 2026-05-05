658a4b254e6bda3ceb646cdf86a392ef586f8a00
Stanislaw Kubiak <stanislaw.kubiak@nordsec.com>
2026-05-05 23:11:35 +0200

    Add copy texts for dedicated servers
    Signed-off-by: Stanislaw Kubiak <stanislaw.kubiak@nordsec.com>

--- cli/messages.go
+++ cli/messages.go
@@ -395,8 +395,8 @@ Your browsing activities remain private, regardless of your choice.
 	SetARPIgnoreWarning      = "You’ve turned off arp-ignore. This is an advanced privacy setting and should only be off if your network setup requires ARP responses."
 
 	// TODO: change those messages once we have proper texts
-	DedicatedServersNoServiceMssage       = "no dedicated servers service, purchase it at www.test.com"
-	DedicatedServersNoServersAvailable    = "pick a location at www.test1.com"
-	DedicatedServersServerNotReadyMessage = "activating your server, please wait"
-	DedicatedServersNoNordlynxMessage     = "Nordlynx technology must be enabled"
+	DedicatedServersNoServiceMssage       = "Get a server that's exclusively yours. Enjoy your own IP with resources and speed reserved entirely for you. Use it with port forwarding to host services and access devices remotely.\n%s"
+	DedicatedServersNoServersAvailable    = "Setup your dedicated server. One last step - pick a server location.\n%s"
+	DedicatedServersServerNotReadyMessage = "Activating your dedicated server. This may take a few minutes."
+	DedicatedServersNoNordlynxMessage     = "NordLynx technology must be set to use this feature."
 )
