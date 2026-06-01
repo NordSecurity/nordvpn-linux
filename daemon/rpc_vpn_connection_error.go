c116e475a3c3b30efa5816d1d661a3ccaeb62060
Gražvidas Balčius <grazvidas.balcius@nordsec.com>
2026-06-01 15:28:00 +0300

    Adding protocol-agnostic VPN connection errors
    Signed-off-by: Gražvidas Balčius <grazvidas.balcius@nordsec.com>

---
+++ daemon/rpc_vpn_connection_error.go
@@ -0,0 +1,13 @@
+package daemon
+
+import (
+	"github.com/NordSecurity/nordvpn-linux/events"
+	"github.com/NordSecurity/nordvpn-linux/log"
+)
+
+// HandleVPNConnectionError is the placeholder (for now) listener for generic VPN connection
+// error events published on the internal VPN events bus
+func (r *RPC) HandleVPNConnectionError(e events.VPNConnectionErrorEvent) error {
+	log.Debug("received VPN connection error event, code:", e.Code)
+	return nil
+}
