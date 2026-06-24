e85ba81fb71a8bb148a92ee4c3b97d6cd1f608e7
Bartosz Zbytniewski <bartosz.zbytniewski@nordsec.com>
2026-06-24 10:13:09 +0200

    LVPN-10730: Add bindings import change on major bump
    Signed-off-by: Bartosz Zbytniewski <bartosz.zbytniewski@nordsec.com>

--- ci/set_bindings_version.sh
+++ ci/set_bindings_version.sh
@@ -7,27 +7,37 @@ declare -A LIB_NAME_TO_PACKAGE=(
 )
 
 declare -A LIB_NAME_TO_VERSION
-[[ -n "${LIBTELIO_VERSION:-}" ]] && LIB_NAME_TO_VERSION[libtelio]=$LIBTELIO_VERSION
-[[ -n "${LIBDROP_VERSION:-}" ]] && LIB_NAME_TO_VERSION[libdrop]=$LIBDROP_VERSION
+[[ -n "${LIBTELIO_VERSION:-}" ]] && LIB_NAME_TO_VERSION[libtelio]=${LIBTELIO_VERSION}
+[[ -n "${LIBDROP_VERSION:-}" ]] && LIB_NAME_TO_VERSION[libdrop]=${LIBDROP_VERSION}
 
 declare -A LIB_NAME_TO_BINDINGS_VERSION
-[[ -n "${LIBTELIO_BINDINGS_VERSION:-}" ]] && LIB_NAME_TO_BINDINGS_VERSION[libtelio]=$LIBTELIO_BINDINGS_VERSION
-[[ -n "${LIBDROP_BINDINGS_VERSION:-}" ]] && LIB_NAME_TO_BINDINGS_VERSION[libdrop]=$LIBDROP_BINDINGS_VERSION
+[[ -n "${LIBTELIO_BINDINGS_VERSION:-}" ]] && LIB_NAME_TO_BINDINGS_VERSION[libtelio]=${LIBTELIO_BINDINGS_VERSION}
+[[ -n "${LIBDROP_BINDINGS_VERSION:-}" ]] && LIB_NAME_TO_BINDINGS_VERSION[libdrop]=${LIBDROP_BINDINGS_VERSION}
 
-lib_name=$1
-repo_path="${LIB_NAME_TO_PACKAGE[$lib_name]}"
-lib_version="${LIB_NAME_TO_VERSION[$lib_name]:-}"
-if [[ -z "${lib_version}" ]]; then
-  return 0
-fi
+for lib_name in "$@"; do
+  repo_path="${LIB_NAME_TO_PACKAGE[${lib_name}]}"
+  lib_version="${LIB_NAME_TO_VERSION[${lib_name}]:-}"
+  if [[ -z "${lib_version}" ]]; then
+    continue
+  fi
 
-bindings_version="${LIB_NAME_TO_BINDINGS_VERSION[$lib_name]:-}"
+  bindings_version="${LIB_NAME_TO_BINDINGS_VERSION[${lib_name}]:-}"
 
-major_version=$(echo "${lib_version}" | cut -d'.' -f1)
+  new_version="${bindings_version:-${lib_version}}"
+  major_version=$(echo "${new_version}" | cut -d'.' -f1)
 
-full_package_path=$repo_path/$major_version@$lib_version
-if [[ -n "${bindings_version}" ]]; then
-  full_package_path=$repo_path/$major_version@$bindings_version
-fi
+  current_version=$(grep "^\s*${repo_path}/" go.mod | awk '{print $2}' | head -1) || true
+  current_major=$(echo "${current_version}" | cut -d'.' -f1)
+  new_module="${repo_path}/${major_version}"
 
-go get "$full_package_path"
+  if [[ -n "${current_major}" && -n "${major_version}" && "${current_major}" != "${major_version}" ]]; then
+    # NOTE: GONOSUMDB allows ignoring module checksum check made by go. This is
+    # not needed for regular builds, because we commit changed go.sum which is
+    # trusted by go tooling, but when we are changing module on the fly, go
+    # needs to recompute checksum database and we don't allow to do this in
+    # docker, so this temporary override here allows module change on the fly
+    # without compilation errors.
+    GONOSUMDB="${repo_path}/*" go get "${new_module}@${new_version}"
+    find . -name "*.go" -exec sed -i "s|\"${repo_path}/${current_major}|\"${repo_path}/${major_version}|g" {} +
+  fi
+done
