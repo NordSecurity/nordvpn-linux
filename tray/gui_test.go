7ad7875230c656456789fbee7120336130a66448
Gražvidas Balčius <grazvidas.balcius@nordsec.com>
2026-07-28 10:49:08 +0300

    LVPN-10766: Add button to invoke GUI (#1625)
    Signed-off-by: Gražvidas Balčius <grazvidas.balcius@nordsec.com>

---
+++ tray/gui_test.go
@@ -0,0 +1,22 @@
+package tray
+
+import (
+	"testing"
+
+	"github.com/NordSecurity/nordvpn-linux/snapconf"
+	"github.com/NordSecurity/nordvpn-linux/test/category"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestIsGUIAvailableUnderSnap(t *testing.T) {
+	category.Set(t, category.Unit)
+
+	t.Setenv("PATH", t.TempDir())
+
+	t.Setenv(snapconf.EnvSnapName, "")
+	assert.False(t, isGUIAvailable())
+
+	t.Setenv(snapconf.EnvSnapName, "nordvpn")
+	assert.True(t, isGUIAvailable())
+}
