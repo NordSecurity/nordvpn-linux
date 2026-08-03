96af4fe1f8aa400b0c1368b8dff219745e138c1d
Bartosz Zbytniewski <bartosz.zbytniewski@nordsec.com>
2026-08-03 11:52:29 +0200

    LVPN-10917: Fix server tag calculation (#1663)
    Signed-off-by: Bartosz Zbytniewski <bartosz.zbytniewski@nordsec.com>

--- daemon/serverpicker/server_selection.go
+++ daemon/serverpicker/server_selection.go
@@ -8,6 +8,7 @@ package serverpicker
 import (
 	"errors"
 	"math/rand"
+	"strings"
 	"time"
 
 	"github.com/NordSecurity/nordvpn-linux/config"
@@ -38,9 +39,9 @@ type SearchParams struct {
 
 func NewSearchParams(tag, group string, excludedServer string) SearchParams {
 	return SearchParams{
-		Tag:            tag,
-		Group:          group,
-		ExcludedServer: excludedServer,
+		Tag:            strings.TrimSpace(tag),
+		Group:          strings.TrimSpace(group),
+		ExcludedServer: strings.TrimSpace(excludedServer),
 	}
 }
 
@@ -99,7 +100,7 @@ func PickServer(
 	serverTag, err := serverTagFromString(input.Tag, serverGroup, countries, servers)
 
 	if err != nil {
-		log.ServerSel.Debug("unable to detect server tag", err)
+		log.ServerSel.Debug("unable to detect server tag:", err)
 		if errors.Is(err, internal.ErrTagDoesNotExist) {
 			return ServerSelection{}, err
 		}
