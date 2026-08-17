492f54641d6f611f383794a66187044f21743d24
ggediminass <gediminas.alsauskas@nordsec.com>
2026-08-17 15:37:35 +0300

    LVPN-10994: Test if peer is unreachable, after removing it

--- test/qa/test_meshnet.py
+++ test/qa/test_meshnet.py
@@ -1,4 +1,3 @@
-import os
 from datetime import datetime
 
 import pytest
@@ -163,27 +162,15 @@ def test_exitnode_permissions():
     assert "This peer does not allow file transfers from you." in str(ex.value), "qa-peer should not be able to send file to tester"
 
 
-@pytest.mark.xfail(condition=meshnet.is_meshnet_test_disabled_from_run(), reason="Run only in nightly")
-def test_remove_peer_firewall_update():
-    peer_ip = meshnet.PeerList.from_str(sh_no_tty.nordvpn.mesh.peer.list()).get_external_peer().ip
-    meshnet.set_permissions(peer_ip, True, True, True, True)
+@pytest.mark.core_meshnet
+def test_peer_unreachable_after_remove():
+    peer = meshnet.PeerList.from_str(sh_no_tty.nordvpn.mesh.peer.list()).get_external_peer()
+    meshnet.set_permissions(peer.ip, True, True, True, True)
 
-    sh_no_tty.nordvpn.mesh.peer.remove(peer_ip)
+    sh_no_tty.nordvpn.mesh.peer.remove(peer.ip)
     sh_no_tty.nordvpn.mesh.peer.refresh()
 
-    def all_peer_permissions_removed() -> (bool, str):
-        #rules = sh.sudo.iptables("-S")
-        rules = os.popen("sudo iptables -S").read()
-        if peer_ip not in rules:
-            return True, ""
-        return False, f"Rules for peer were not removed from firewall\nPeer IP: {peer_ip}\nrules:\n{rules}"
-
-    result, message = None, None
-    for (result, message) in lib.poll(all_peer_permissions_removed):  # noqa: B007
-        if result:
-            break
-
-    assert result, message
+    assert not meshnet.is_peer_reachable(peer, meshnet.PeerName.Ip)
 
 
 @pytest.mark.xfail(condition=meshnet.is_meshnet_test_disabled_from_run(), reason="Run only in nightly")
