b79a7a716fd7eaa6c54599a9b9ef83a1750e2432
Edvinas Gaubas <edvinas.gaubas@nordsec.com>
2026-09-04 15:48:42 +0300

    Do not destroy token on logout if logged in via --token flag

--- daemon/rpc_logout.go
+++ daemon/rpc_logout.go
@@ -20,7 +20,6 @@ func (r *RPC) Logout(ctx context.Context, in *pb.LogoutRequest) (*pb.Payload, er
 		ConfigManager:                r.cm,
 		UserLogoutEventPublisherFunc: r.events.User.Logout.Publish,
 		DebugPublisherFunc:           r.publisher.Publish,
-		PersistToken:                 in.GetPersistToken(),
 		DisconnectFunc:               r.DoDisconnect,
 		DeviceKeyInvalidator:         r.dedicatedServerKeyManager,
 	})
