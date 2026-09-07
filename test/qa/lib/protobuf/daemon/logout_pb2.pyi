8608036e7f51aeb28618bf452d315ad7d6544bca
Edvinas Gaubas <edvinas.gaubas@nordsec.com>
2026-09-07 16:16:11 +0300

    Regenerate protobuf

--- test/qa/lib/protobuf/daemon/logout_pb2.pyi
+++ test/qa/lib/protobuf/daemon/logout_pb2.pyi
@@ -1,11 +1,9 @@
 from google.protobuf import descriptor as _descriptor
 from google.protobuf import message as _message
-from typing import ClassVar as _ClassVar, Optional as _Optional
+from typing import ClassVar as _ClassVar
 
 DESCRIPTOR: _descriptor.FileDescriptor
 
 class LogoutRequest(_message.Message):
-    __slots__ = ("persist_token",)
-    PERSIST_TOKEN_FIELD_NUMBER: _ClassVar[int]
-    persist_token: bool
-    def __init__(self, persist_token: bool = ...) -> None: ...
+    __slots__ = ()
+    def __init__(self) -> None: ...
