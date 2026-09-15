ae76d3056ffef55b6e2ac7f1155829daf167ada6
Vadym Hnatenko <vadym.hnatenko@nordsec.com>
2026-09-15 14:28:04 +0200

    LVPN-11134: Fix empty input test data for test_status_change_technology_and_protocol - improved 'dynamic_parametrize' functionality by adding 'ordered_coverage' option

--- test/qa/lib/dynamic_parametrize.py
+++ test/qa/lib/dynamic_parametrize.py
@@ -44,7 +44,7 @@ def as_tuple(obj):
     return tuple(obj) if isinstance(obj, list | tuple) else (obj,)
 
 
-def _generate_params_with_ids(argnames, randomized_source, ordered_source, generate_all=False, id_pattern=None, always_pair=None, sample_size=None):
+def _generate_params_with_ids(argnames, randomized_source, ordered_source, generate_all=False, id_pattern=None, always_pair=None, sample_size=None, ordered_coverage=True):
     """
     Generate test parameters and their associated unique identifiers (IDs).
 
@@ -64,6 +64,8 @@ def _generate_params_with_ids(argnames, randomized_source, ordered_source, gener
         - {ordered} is replaced by the corresponding value from the ordered source.
     :param always_pair: item from randomized_source to always be paired with every ordered value
     :param sample_size: Randomly select this many cases from all cartesian combinations (cartesian mode only).
+    :param ordered_coverage: If True, guarantees that every item in ordered_source is paired exactly once with an
+        item from randomized_source
 
     :return:
         - params: A list of parameter combining randomized and ordered data.
@@ -90,10 +92,26 @@ def _generate_params_with_ids(argnames, randomized_source, ordered_source, gener
                 all_combos.append(as_tuple(ord_elem) + as_tuple(always_pair))
             rnds = [item for item in rnds if item != always_pair]
 
-        for ord_elem in ords:
-            rand_elem = random.Random().choice(rnds)
-            combo = as_tuple(ord_elem) + as_tuple(rand_elem)
-            all_combos.append(combo)
+        if ordered_coverage:
+            ords_raw = ordered_source if _is_list_of_lists(ordered_source) else [ordered_source]
+            rnds_raw = randomized_source if _is_list_of_lists(randomized_source) else [randomized_source]
+            ords_items = list(ords_raw[0]) if len(ords_raw) == 1 and isinstance(ords_raw[0],
+                                                                                list | tuple) else ords_raw
+            rnds_items = list(rnds_raw[0]) if len(rnds_raw) == 1 and isinstance(rnds_raw[0],
+                                                                                list | tuple) else rnds_raw
+
+            if always_pair:
+                rnds_items = [item for item in rnds_items if item != always_pair]
+
+            rnd_pool = (rnds_items * (len(ords_items) // len(rnds_items) + 1))[:len(ords_items)]
+            random.Random().shuffle(rnd_pool)
+            for ord_elem, rnd_elem in zip(ords_items, rnd_pool, strict=False):
+                all_combos.append(as_tuple(ord_elem) + as_tuple(rnd_elem))
+        else:
+            for ord_elem in ords:
+                rand_elem = random.Random().choice(rnds)
+                combo = as_tuple(ord_elem) + as_tuple(rand_elem)
+                all_combos.append(combo)
 
     for flat_combo in all_combos:
         params.append(flat_combo)
@@ -105,7 +123,7 @@ def _generate_params_with_ids(argnames, randomized_source, ordered_source, gener
     return params, ids
 
 
-def dynamic_parametrize(argnames, randomized_source, ordered_source, generate_all=False, id_pattern=None, always_pair=None, sample_size=None):
+def dynamic_parametrize(argnames, randomized_source, ordered_source, generate_all=False, id_pattern=None, always_pair=None, sample_size=None, ordered_coverage=True):
     """
     A pytest decorator for dynamic parameterization of test functions.
 
@@ -125,13 +143,15 @@ def dynamic_parametrize(argnames, randomized_source, ordered_source, generate_al
         - {ordered_source} is replaced by the corresponding value from the ordered source.
     :param always_pair: item from randomized_source to always be paired with every ordered value
     :param sample_size: Randomly select this many cases from all cartesian combinations (cartesian mode only).
+    :param ordered_coverage: If True, guarantees that every item in ordered_source is paired exactly once with an
+        item from randomized_source
 
     :return:
         The dynamically parameterized test function wrapped with pytest's pytest.mark.parametrize.
     """
     def decorator(func):
         params, ids = _generate_params_with_ids(argnames, randomized_source, ordered_source, generate_all,
-                                                id_pattern, always_pair, sample_size)
+                                                id_pattern, always_pair, sample_size, ordered_coverage)
 
         @pytest.mark.parametrize(argnames, params, ids=ids)
         @wraps(func)
