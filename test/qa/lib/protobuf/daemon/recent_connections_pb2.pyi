14eb2ac63f05843f52170cf391f6b1aa935bb654
Marius Sincovici <marius.sincovici@nordsec.com>
2026-09-17 09:20:50 +0200

    Delete import in protobuf

--- test/qa/lib/protobuf/daemon/recent_connections_pb2.pyi
+++ test/qa/lib/protobuf/daemon/recent_connections_pb2.pyi
@@ -1,5 +1,4 @@
 from config import group_pb2 as _group_pb2
-from config import technology_pb2 as _technology_pb2
 import server_selection_rule_pb2 as _server_selection_rule_pb2
 from google.protobuf.internal import containers as _containers
 from google.protobuf import descriptor as _descriptor
